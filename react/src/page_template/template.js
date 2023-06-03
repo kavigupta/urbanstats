42a2851ced8b00d0ca2822ff298ea3fb2d17a094
Kavi Gupta <kavi@berkeley.edu>
2023-06-03 17:16:50 -0400

    bump version number

--- react/src/page_template/template.js
+++ react/src/page_template/template.js
@@ -19,7 +19,7 @@ function create_page(props, main_content) {
                 <div className="right_panel">
                     {main_content}
                     <div className="gap"></div>
-                    <div className="centered_text">Density Database Version 1.3.4 by Kavi Gupta. Last updated 2023-05-28.</div>
+                    <div className="centered_text">Density Database Version 2.0.0 by Kavi Gupta. Last updated 2023-06-03.</div>
                 </div>
             </div>
         </div>
