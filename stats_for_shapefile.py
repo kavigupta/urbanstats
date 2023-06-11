b51fc78b1bb17ade4890f9218c19e490295c97fc
Kavi Gupta <kavi@berkeley.edu>
2023-06-11 19:11:52 -0400

    handle more kinds of shapefile

--- stats_for_shapefile.py
+++ stats_for_shapefile.py
@@ -1,7 +1,9 @@
 from collections import Counter
+import pickle
 
 import attr
 import pandas as pd
+import tqdm
 from census_blocks import RADII, racial_demographics, housing_units
 from election_data import with_election_results, election_column_names
 
@@ -34,13 +36,18 @@ class Shapefile:
     filter = attr.ib()
     meta = attr.ib()
     drop_dup = attr.ib(default=False)
+    chunk_size = attr.ib(default=None)
 
     def load_file(self):
         if isinstance(self.path, list):
             s = gpd.GeoDataFrame(pd.concat([gpd.read_file(p) for p in self.path]))
             s = s.reset_index(drop=True)
         else:
-            s = gpd.read_file(self.path)
+            if self.path.endswith(".pkl"):
+                with open(self.path, "rb") as f:
+                    s = pickle.load(f).reset_index(drop=True)
+            else:
+                s = gpd.read_file(self.path)
         s = s[s.apply(self.filter, axis=1)]
         s = gpd.GeoDataFrame(
             dict(
@@ -52,7 +59,8 @@ class Shapefile:
         if self.drop_dup:
             duplicates = {k: v for k, v in Counter(s.longname).items() if v > 1}
             s = s[s.longname.apply(lambda x: x not in duplicates)]
-
+        if s.crs is None:
+            s.crs = "EPSG:4326"
         s = s.to_crs("EPSG:4326")
         return s
 
@@ -74,8 +82,15 @@ def compute_statistics_for_shapefile(sf):
         *density_metrics,
         *election_column_names,
     ]
-    joined = s.sjoin(blocks_gdf, how="inner", predicate="intersects")
-    grouped_stats = pd.DataFrame(joined[sum_keys]).groupby(joined.index).sum()
+    if sf.chunk_size is None:
+        grouped_stats = compute_grouped_stats(blocks_gdf, s, sum_keys)
+    else:
+        grouped_stats = []
+        for i in tqdm.trange(0, s.shape[0], sf.chunk_size):
+            grouped_stats.append(
+                compute_grouped_stats(blocks_gdf, s.iloc[i : i + sf.chunk_size], sum_keys)
+            )
+        grouped_stats = pd.concat(grouped_stats)
     for k in density_metrics:
         grouped_stats[k] /= grouped_stats["population"]
     result = pd.concat(
@@ -98,3 +113,9 @@ def compute_statistics_for_shapefile(sf):
     del result["occupied"]
     del result["population_18"]
     return result
+
+
+def compute_grouped_stats(blocks_gdf, s, sum_keys):
+    joined = s.sjoin(blocks_gdf, how="inner", predicate="intersects")
+    grouped_stats = pd.DataFrame(joined[sum_keys]).groupby(joined.index).sum()
+    return grouped_stats
