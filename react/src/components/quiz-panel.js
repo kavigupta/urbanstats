975125122cbbbdb4d86a66adf058d7d52fd5c479
Kavi Gupta <kavi@berkeley.edu>
2023-09-02 11:32:40 -0700

    several updates to the quiz

--- react/src/components/quiz-panel.js
+++ react/src/components/quiz-panel.js
@@ -15,7 +15,8 @@ class QuizPanel extends PageTemplate {
         super(props);
         this.state = {
             ...this.state,
-            quiz_history: JSON.parse(localStorage.getItem("quiz_history")) || {}
+            quiz_history: JSON.parse(localStorage.getItem("quiz_history")) || {},
+            waiting: false
         }
         this.todays_quiz = this.get_todays_quiz();
         this.today = this.get_today();
@@ -26,7 +27,10 @@ class QuizPanel extends PageTemplate {
         const quiz = this.todays_quiz;
         const history = this.get_todays_quiz_history();
 
-        const index = history["choices"].length;
+        let index = history["choices"].length;
+        if (this.state.waiting) {
+            index -= 1;
+        }
 
         if (index == quiz.length) {
             return (
@@ -37,7 +41,10 @@ class QuizPanel extends PageTemplate {
         return (
             <QuizQuestion
                 {...quiz[index]}
+                history={history}
+                length={quiz.length}
                 on_select={x => this.on_select(x)}
+                waiting={this.state.waiting}
             />
         );
     }
@@ -46,44 +53,44 @@ class QuizPanel extends PageTemplate {
     get_todays_quiz() {
         // TODO
         const result = [{
-            'question': 'higher % of people who are boomers',
-            'stat_column': 'Boomer %',
-            'longname_a': 'Canton [Urban Area], OH, USA',
-            'longname_b': 'Ogden-Layton [Urban Area], UT, USA',
-            'stat_a': 0.2568660249764331,
-            'stat_b': 0.16268518260446413
+            'question': 'higher % of people who have a graduate degree',
+            'stat_column': 'Grad %',
+            'longname_a': 'New Jersey, USA',
+            'longname_b': 'Oklahoma, USA',
+            'stat_a': 0.15965999170325224,
+            'stat_b': 0.08869466243023064
+        },
+        {
+            'question': 'higher % of people who are born outside the us',
+            'stat_column': 'Born outside US %',
+            'longname_a': 'Honolulu [Urban Area], HI, USA',
+            'longname_b': 'Knoxville [Urban Area], TN, USA',
+            'stat_a': 0.2138420706524398,
+            'stat_b': 0.054303262244923545
+        },
+        {
+            'question': 'higher % of units with 2br rent < $750',
+            'stat_column': '2BR Rent < $750 %',
+            'longname_a': 'Rhode Island, USA',
+            'longname_b': 'South Carolina, USA',
+            'stat_a': 0.14491238740041276,
+            'stat_b': 0.2860413033584326
+        },
+        {
+            'question': 'higher % units built in 2010s+',
+            'stat_column': '% units built in 2010s+',
+            'longname_a': 'Louisville/Jefferson County [Urban Area], KY-IN, USA',
+            'longname_b': 'Lincoln [Urban Area], NE, USA',
+            'stat_a': 0.06178147244298866,
+            'stat_b': 0.10145396738223135
         },
         {
             'question': 'higher % of people who are asian',
             'stat_column': 'Asian %',
-            'longname_a': 'Durham city, North Carolina, USA',
-            'longname_b': 'Anaheim city, California, USA',
-            'stat_a': 0.05605239910321793,
-            'stat_b': 0.17483509386964344
-        },
-        {
-            'question': 'higher % of people who are black',
-            'stat_column': 'Black %',
-            'longname_a': 'Lynchburg MSA, VA, USA',
-            'longname_b': 'San Juan-Carolina-Caguas MSA, PR, USA',
-            'stat_a': 0.16084528255725497,
-            'stat_b': 0.0015971823473871606
-        },
-        {
-            'question': 'higher % of people who are hawaiian / pi',
-            'stat_column': 'Hawaiian / PI %',
-            'longname_a': 'Tucson city, Arizona, USA',
-            'longname_b': 'Minneapolis city, Minnesota, USA',
-            'stat_a': 0.002444803089258342,
-            'stat_b': 0.000397716965070682
-        },
-        {
-            'question': 'higher % of people who are on public insurance',
-            'stat_column': 'Public Insurance %',
-            'longname_a': 'Allentown-Bethlehem [Urban Area], PA-NJ, USA',
-            'longname_b': 'Antioch [Urban Area], CA, USA',
-            'stat_a': 0.1858512397095249,
-            'stat_b': 0.2404909478581838
+            'longname_a': 'Atlanta city, Georgia, USA',
+            'longname_b': 'Henderson city, Nevada, USA',
+            'stat_a': 0.04455592448229954,
+            'stat_b': 0.09109140307961604
         }];
 
         return result;
@@ -91,7 +98,7 @@ class QuizPanel extends PageTemplate {
 
     get_today() {
         // TODO actually get the day
-        return "D1";
+        return "D2";
     }
 
     get_todays_quiz_history() {
@@ -101,17 +108,23 @@ class QuizPanel extends PageTemplate {
     set_todays_quiz_history(history_today) {
         const history = this.state.quiz_history;
         history[this.today] = history_today;
-        this.setState({ history: history });
+        this.setState({ history: history, waiting: true });
         localStorage.setItem("quiz_history", JSON.stringify(history));
     }
 
     on_select(selected) {
+        if (this.state.waiting) {
+            return;
+        }
         const history = this.get_todays_quiz_history();
         const idx = history.correct_pattern.length;
         const quiz = this.todays_quiz[idx];
         history.choices.push(selected);
         history.correct_pattern.push((selected == "A") == (quiz.stat_a > quiz.stat_b));
         this.set_todays_quiz_history(history);
+        setTimeout(function () { //Start the timer
+            this.setState({ waiting: false }) //After 1 second, set render to true
+        }.bind(this), 500)
     }
 }
 
@@ -119,14 +132,46 @@ function Header() {
     return (<div className={"centered_text " + (isMobile ? "headertext_mobile" : "headertext")}>Juxtastat</div>);
 }
 
+// function Overlay({ correct }) {
+//     let cn = "quiz_overlay_text serif";
+//     const text = correct ? "Correct" : "Incorrect";
+//     if (correct) {
+//         cn += " quiz_overlay_correct";
+//     } else {
+//         cn += " quiz_overlay_incorrect";
+//     }
+//     return (<div className={cn}>
+//         <div className="quiz_overlay_text_content">
+//             {text}!
+//         </div>
+//     </div>)
+// }
+
 class QuizQuestion extends PageTemplate {
     constructor(props) {
         super(props);
     }
 
     render() {
+        let button_a = "quiz_clickable";
+        let button_b = "quiz_clickable";
+        if (this.props.waiting) {
+            const choices = this.props.history.choices;
+            const pattern = this.props.history.correct_pattern;
+            const choice = choices[choices.length - 1];
+            const correct = pattern[pattern.length - 1];
+            const css_class = correct ? " quiz_correct" : " quiz_incorrect";
+            if (choice == "A") {
+                button_a += css_class;
+            } else {
+                button_b += css_class;
+            }
+        }
+        console.log(button_a);
+        console.log(button_b);
         return (
             <div>
+                {/* {this.props.waiting ? <Overlay correct={pattern[pattern.length - 1]} /> : undefined} */}
                 <Header />
                 <div className={"centered_text " + (isMobile ? "subheadertext_mobile" : "subheadertext")}>
                     Which has a {this.props.question}?
@@ -136,14 +181,14 @@ class QuizQuestion extends PageTemplate {
                     <tbody>
                         <tr>
                             <td className="quiz_option">
-                                <button className="quiz_clickable" onClick={() => this.props.on_select("A")}>
+                                <button className={button_a} onClick={() => this.props.on_select("A")}>
                                     <div className={"centered_text " + (isMobile ? "subheadertext_mobile" : "subheadertext")}>
                                         {this.props.longname_a}
                                     </div>
                                 </button>
                             </td>
                             <td className="quiz_option">
-                                <button className="quiz_clickable" onClick={() => this.props.on_select("B")}>
+                                <button className={button_b} onClick={() => this.props.on_select("B")}>
                                     <div className={"centered_text " + (isMobile ? "subheadertext_mobile" : "subheadertext")}>
                                         {this.props.longname_b}
                                     </div>
@@ -166,8 +211,48 @@ class QuizQuestion extends PageTemplate {
                         </tr>
                     </tbody>
                 </table>
+                <Footer history={this.props.history} length={this.props.length} />
+                <Help />
             </div>
         )
+    }
+}
+
+class Footer extends PageTemplate {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log(this.props.history);
+        const choices = this.props.history.correct_pattern.map(
+            (correct, _) => correct ? "quiz_green" : "quiz_red"
+        );
+        while (choices.length < this.props.length) {
+            choices.push("quiz_blank");
+        }
+        return <table className="quiz_footer">
+            <tbody>
+                <tr>
+                    {choices.map(x =>
+                        <td className={x}></td>
+                    )}
+                </tr>
+            </tbody>
+        </table >
+    }
+}
+
+class Help extends PageTemplate {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className="centered_text serif">
+            Select the geographical region answering the question. The questions
+            get harder as you go on.
+        </div>
     }
 }
 
