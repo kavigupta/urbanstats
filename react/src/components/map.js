bb3e2c1b3f5e9209e801dc87592803ac84df6c78
Kavi Gupta <kavi@berkeley.edu>
2023-09-30 15:26:42 -0700

    generic version of the map box

--- react/src/components/map.js
+++ react/src/components/map.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export { Map };
+export { Map, MapGeneric };
 
 import { shape_link, article_link } from "../navigation/links.js";
 import { relationship_key } from "./related-button.js";
@@ -10,17 +10,32 @@ import "./map.css";
 import { is_historical_cd } from '../utils/is_historical.js';
 import { loadProtobuf } from '../load_json.js';
 
-class Map extends React.Component {
+class MapGeneric extends React.Component {
     constructor(props) {
         super(props);
         this.polygon_by_name = {};
-        this.already_fit_bounds_for = false;
     }
 
     render() {
         return (
             <div id={this.props.id} className="map"></div>
         );
+    }
+
+    async compute_polygons() {
+        /**
+         * Should return [names, styles, zoom_index]
+         * names: list of names of polygons to draw
+         * styles: list of styles for each polygon
+         * zoom_index: index of polygon to zoom to, or -1 if none 
+         */
+        throw "compute_polygons not implemented";
+    }
+
+    async mapDidRender() {
+        /**
+         * Called after the map is rendered
+         */
     }
 
     async componentDidMount() {
@@ -35,14 +50,12 @@ class Map extends React.Component {
     async componentDidUpdate() {
         const map = this.map;
         this.exist_this_time = [];
-        await this.add_polygon(map, this.props.longname, this.already_fit_bounds_for != this.props.longname, { "interactive": false },
-            true,
-            false);
-        this.already_fit_bounds = this.props.longname;
-        await this.add_related_polygons(map, this.get_related("contained_by"));
-        await this.add_related_polygons(map, this.get_related("intersects"));
-        await this.add_related_polygons(map, this.get_related("borders"));
-        await this.add_related_polygons(map, this.get_related("contains"));
+
+        const [names, styles, zoom_index] = await this.compute_polygons();
+
+        await this.add_polygons(map, names, styles, zoom_index);
+
+        await this.mapDidRender();
 
         // Remove polygons that no longer exist
         for (let name in this.polygon_by_name) {
@@ -53,36 +66,13 @@ class Map extends React.Component {
         }
     }
 
-    get_related(key) {
-        if (this.props.related === undefined) {
-            return [];
-        }
-        const element = this.props.related.filter(
-            (x) => x.relationshipType == key)
-            .map((x) => x.buttons)[0];
-        return element;
-    }
-
-    async add_related_polygons(map, related) {
-        for (let i = related.length - 1; i >= 0; i--) {
-            if (!this.props.settings.show_historical_cds && is_historical_cd(related[i].rowType)) {
-                continue;
-            }
-            let key = relationship_key(this.props.article_type, related[i].rowType);
-            if (!this.props.settings[key]) {
-                continue;
-            }
-
-            const color = random_color(related[i].longname);
-            await this.add_polygon(map,
-                related[i].longname,
-                false,
-                { color: color, weight: 1, fillColor: color, fillOpacity: 0.1 },
-                true);
+    async add_polygons(map, names, styles, zoom_to) {
+        for (let i = 0; i < names.length; i++) {
+            await this.add_polygon(map, names[i], i == zoom_to, styles[i]);
         }
     }
 
-    async add_polygon(map, name, fit_bounds, style, add_callback = false, add_to_bottom = true) {
+    async add_polygon(map, name, fit_bounds, style, add_callback = true, add_to_bottom = false) {
         this.exist_this_time.push(name);
         if (name in this.polygon_by_name) {
             return;
@@ -126,6 +116,71 @@ class Map extends React.Component {
         map.addLayer(group);
         this.polygon_by_name[name] = group;
     }
+}
 
+class Map extends MapGeneric {
+    constructor(props) {
+        super(props);
+
+        this.already_fit_bounds = false;
+    }
+
+    async compute_polygons() {
+        const relateds = [];
+        relateds.push(...this.get_related("contained_by"));
+        relateds.push(...this.get_related("intersects"));
+        relateds.push(...this.get_related("borders"));
+        relateds.push(...this.get_related("contains"));
+
+        const names = [];
+        const styles = [];
+
+        names.push(this.props.longname);
+        styles.push({ "interactive": false });
+
+        const [related_names, related_styles] = this.related_polygons(relateds);
+        names.push(...related_names);
+        styles.push(...related_styles);
+
+        const zoom_index = this.already_fit_bounds != this.props.longname ? 0 : -1;
+
+        return [names, styles, zoom_index];
+    }
+
+    async mapDidRender() {
+        this.already_fit_bounds = this.props.longname;
+    }
+
+    get_related(key) {
+        if (this.props.related === undefined) {
+            return [];
+        }
+        const element = this.props.related.filter(
+            (x) => x.relationshipType == key)
+            .map((x) => x.buttons)[0];
+        return element;
+    }
+
+    related_polygons(related) {
+        const names = [];
+        const styles = [];
+        for (let i = related.length - 1; i >= 0; i--) {
+            if (!this.props.settings.show_historical_cds && is_historical_cd(related[i].rowType)) {
+                continue;
+            }
+            let key = relationship_key(this.props.article_type, related[i].rowType);
+            if (!this.props.settings[key]) {
+                continue;
+            }
+
+            // const current_time_millis = new Date().getTime();
+            // console.log(related[i].longname + current_time_millis)
+            const color = random_color(related[i].longname);
+            const style = { color: color, weight: 1, fillColor: color, fillOpacity: 0.1 };
+            names.push(related[i].longname);
+            styles.push(style);
+        }
+        return [names, styles];
+    }
 
 }
