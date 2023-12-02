a444482c6dedfa7d9891f4b45e7d8d16ecfe9473
Kavi Gupta <kavi@berkeley.edu>
2023-12-02 00:27:32 -0800

    update

--- create_website.py
+++ create_website.py
@@ -246,7 +246,7 @@ def main(no_geo=False, no_data=False, no_data_jsons=False):
     with open(f"{folder}/.nojekyll", "w") as f:
         f.write("")
 
-    os.system("cd react; npm run dev")
+    os.system("cd react; npm run prod")
     shutil.copy("dist/article.js", f"{folder}/scripts/")
     shutil.copy("dist/index.js", f"{folder}/scripts/")
     shutil.copy("dist/about.js", f"{folder}/scripts/")
