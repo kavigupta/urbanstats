07990428291931d06df9bfee76f5307f5e962d94
Kavi Gupta <kavi@berkeley.edu>
2023-12-26 22:48:02 -0500

    load article

--- react/src/components/article-panel.js
+++ react/src/components/article-panel.js
@@ -9,6 +9,7 @@ import { PageTemplate } from "../page_template/template.js";
 import "../common.css";
 import "./article.css";
 import { isMobile } from 'react-device-detect';
+import { load_article } from './load-article.js';
 
 class ArticlePanel extends PageTemplate {
     constructor(props) {
@@ -17,49 +18,7 @@ class ArticlePanel extends PageTemplate {
 
     main_content() {
         const self = this;
-        let article_type = this.props.articleType;
-
-        const categories = require("../data/statistic_category_list.json");
-        const names = require("../data/statistic_name_list.json");
-        const paths = require("../data/statistic_path_list.json");
-        const stats = require("../data/statistic_list.json");
-        const counts_by_article_type = require("../data/counts_by_article_type.json");
-        const explanation_page = require("../data/explanation_page.json");
-
-        const indices = require("../data/indices_by_type.json")[article_type];
-
-        let modified_rows = [];
-        for (let i in this.props.rows) {
-            let row_original = this.props.rows[i];
-            i = indices[i];
-            // fresh row object
-            let row = {};
-            row.statval = row_original.statval;
-            row.ordinal = row_original.ordinal;
-            row.overallOrdinal = row_original.overallOrdinal;
-            row.percentile_by_population = row_original.percentileByPopulation;
-            row.statistic_category = categories[i];
-            row.statcol = stats[i];
-            row.statname = names[i];
-            row.statpath = paths[i];
-            row.explanation_page = explanation_page[i];
-            row.article_type = article_type;
-
-            function for_type(typ) {
-                return counts_by_article_type.filter(
-                    (x) =>
-                        x[0][1] == typ
-                        && JSON.stringify(x[0][0]) == JSON.stringify(row.statcol)
-                    )[0][1];
-            }
-
-            let count_articles = for_type(article_type);
-            let count_articles_overall = for_type("overall");
-
-            row.total_count_in_class = count_articles;
-            row.total_count_overall = count_articles_overall;
-            modified_rows.push(row);
-        }
+        const modified_rows = load_article(this.props);
         const filtered_rows = modified_rows.filter((row) => {
             const key = "show_statistic_" + row.statistic_category;
             return self.state.settings[key];
@@ -84,7 +43,7 @@ class ArticlePanel extends PageTemplate {
                     longname={this.props.longname}
                     related={this.props.related}
                     settings={this.state.settings}
-                    article_type={article_type}
+                    article_type={this.props.article_type}
                     basemap={{type: "osm"}} />
 
                 <script src="/scripts/map.js"></script>
@@ -93,7 +52,7 @@ class ArticlePanel extends PageTemplate {
                     related={this.props.related}
                     settings={this.state.settings}
                     set_setting={(key, value) => self.set_setting(key, value)}
-                    article_type={article_type} />
+                    article_type={this.props.article_type} />
             </div>
         );
     }
