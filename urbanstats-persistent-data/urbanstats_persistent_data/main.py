f134a683497438a2c80e288533e3af5a2156abc7
Kavi Gupta <kavi@berkeley.edu>
2024-01-04 04:03:35 -0500

    allow cerritos to log on

--- urbanstats-persistent-data/urbanstats_persistent_data/main.py
+++ urbanstats-persistent-data/urbanstats_persistent_data/main.py
@@ -29,10 +29,11 @@ def flask_form():
 
 
 def valid_token(tok):
-    token_must_hash_to = (
-        "d02a2a95256315523dd5740e1a4d1c00b5a69752f26f891170e8078fd37aa224"
-    )
-    return hashlib.sha256(tok.encode("utf-8")).hexdigest() == token_must_hash_to
+    token_must_hash_to = {
+        "d02a2a95256315523dd5740e1a4d1c00b5a69752f26f891170e8078fd37aa224",
+        "5f5cf96b428467624d144bb4297ae317f564af0ef0df8e6b70d2b706823add1b",
+    }
+    return hashlib.sha256(tok.encode("utf-8")).hexdigest() in token_must_hash_to
 
 
 @app.route("/shorten", methods=["POST"])
