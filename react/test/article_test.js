4bfbc103a0b65f89757c0f54f239f4b42421251c
Kavi Gupta <kavi@berkeley.edu>
2024-02-07 02:06:58 -0500

    update

--- react/test/article_test.js
+++ react/test/article_test.js
@@ -331,14 +331,29 @@ test('comparison-3-editable-number-third', async t => {
 })
 
 fixture('quiz result test')
-    .page(TARGET + '/quiz.html')
+    .page(TARGET + '/quiz.html?date=100')
     // no local storage
     .beforeEach(async t => {
-        await t.eval(() => localStorage.clear());
+        await t.eval(() => {
+            localStorage.clear()
+            const quiz_history = {};
+            for (var i = 2; i <= 100; i++) {
+                quiz_history[i] = {
+                    "choices": ["A", "A", "A", "A", "A"],
+                    "correct_pattern": [true, true, true, i % 3 == 1, i % 4 == 1]
+                }
+            }
+            quiz_history[62] = {
+                "choices": ["A", "A", "A", "A", "A"],
+                "correct_pattern": [false, false, false, false, false]
+            }
+            localStorage.setItem("quiz_history", JSON.stringify(quiz_history));
+        });
     });
 
-test('california-article-test', async t => {
-    // screenshot path: images/first_test.png
-    await screencap(t, "article/california");
+test('quiz-results-test', async t => {
+    await t.eval(() => location.reload(true));
+    await t.eval(() => document.getElementById("quiz-timer").remove());
+    await screencap(t, "quiz/results-page");
 });
 
