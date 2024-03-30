f8a0e914999d350c14ea9b7437b8754178a64322
Kavi Gupta <kavi@berkeley.edu>
2024-03-29 23:19:45 -0700

    update

--- create_website.py
+++ create_website.py
@@ -324,7 +324,7 @@ def main(
     with open(f"react/src/data/indices_by_type.json", "w") as f:
         json.dump(get_idxs_by_type(), f)
 
-    os.system("cd react; npm run dev")
+    os.system("cd react; npm run prod")
     shutil.copy("dist/article.js", f"{site_folder}/scripts/")
     shutil.copy("dist/comparison.js", f"{site_folder}/scripts/")
     shutil.copy("dist/statistic.js", f"{site_folder}/scripts/")
