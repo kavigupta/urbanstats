3a7045d741dce447e151f08b0ac4de6aad29962e
Kavi Gupta <kavi@berkeley.edu>
2024-04-28 01:13:34 -0400

    update

--- urbanstats/utils.py
+++ urbanstats/utils.py
@@ -7,6 +7,8 @@ def hash_full_table(sh):
     non_float_columns = [
         x for x in sh if sh[x].dtype != np.float64 and sh[x].dtype != np.float32
     ]
+    # for c in sh:
+    #     print(c, stable_hash(sh[c]))
     return stable_hash(
         (
             stable_hash([sh[x] for x in non_float_columns]),
