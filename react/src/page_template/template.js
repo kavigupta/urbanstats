7a24aa517388575b495c8be718091f3aef8da9e9
Kavi Gupta <kavi@berkeley.edu>
2024-05-12 11:44:48 -0700

    update

--- react/src/page_template/template.js
+++ react/src/page_template/template.js
@@ -144,11 +144,11 @@ function TemplateFooter() {
 }
 
 function Version() {
-    return <span id="current-version">12.1.0</span>
+    return <span id="current-version">13.0.0</span>
 }
 
 function LastUpdated() {
-    return <span id="last-updated">2024-05-11</span>
+    return <span id="last-updated">2024-05-12</span>
 }
 
 function MainCredits() {
