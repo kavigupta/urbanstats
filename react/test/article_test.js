1bffbda0bc0bab700da6748993a27df783fc67ea
Kavi Gupta <kavi@berkeley.edu>
2024-05-12 00:07:07 -0400

    fix ability to click around a map (#202)

--- react/test/article_test.js
+++ react/test/article_test.js
@@ -64,6 +64,23 @@ test('california-article-test', async t => {
     await screencap(t, "article/california");
 });
 
+test('neighboring-state-test', async t => {
+    const arizona = Selector('li').withAttribute('class', 'list_of_lists')
+        .withText('Arizona')
+        // find a checkbox inside it
+        .find('input');
+    await t
+        .click(arizona);
+    await t.wait(1000);
+    await screencap(t, "article/california-with-neighbors");
+    await t
+        .click(Selector('path').withAttribute('class', /tag-Arizona,_USA/));
+    await t.expect(getLocation())
+        .eql(TARGET + '/article.html?longname=Arizona%2C+USA');
+});
+
+
+
 fixture('shorter article test')
     .page(TARGET + '/article.html?longname=San+Marino+city%2C+California%2C+USA')
     .beforeEach(async t => {
