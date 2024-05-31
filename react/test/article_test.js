e89a91e97fb498ed3196b3c5c670fa85440e1c7e
Kavi Gupta <kavi@berkeley.edu>
2024-05-31 17:52:05 -0400

    correct universe in random selector (#213)

--- react/test/article_test.js
+++ react/test/article_test.js
@@ -684,3 +684,21 @@ fixture('mapping')
 test("state-map", async t => {
     await screencap(t, "state-map");
 })
+
+fixture('random')
+    .page(TARGET + "/random.html?sampleby=population&us_only=true")
+    // no local storage
+    .beforeEach(async t => {
+        await t.eval(() => localStorage.clear());
+    });
+
+test("random-usa", async t => {
+    // wait for load
+    await t.wait(1000);
+    // contains article
+    await t.expect(getLocation())
+        .contains('/article.html?longname=');
+    // location should not include &universe=
+    await t.expect(getLocation())
+        .notContains('&universe=');
+})
