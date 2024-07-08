348ff825a05b9140ec97481da12263f9cdf2a82d
Kavi Gupta <kavi@berkeley.edu>
2024-07-08 01:29:36 -0400

    convert to a functional component

--- react/src/components/search.js
+++ react/src/components/search.js
@@ -6,174 +6,165 @@ import { loadProtobuf } from '../load_json.js';
 import { is_historical_cd } from '../utils/is_historical';
 import "../common.css";
 
-class SearchBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            matches: [], matches_stale: false,
-            index_cache: undefined, index_cache_uninitialized: true,
-            first_character: undefined,
-            focused: 0,
+const SearchBox = (props) => {
+
+    const [matches, setMatches] = React.useState([]);
+    const [matchesStale, setMatchesStale] = React.useState(false);
+    const [indexCache, setIndexCache] = React.useState(undefined);
+    const [indexCacheUninitialized, setIndexCacheUninitialized] = React.useState(true);
+    const [firstCharacter, setFirstCharacter] = React.useState(undefined);
+    const [focused, setFocused] = React.useState(0);
+
+    const form = React.useRef();
+    const textbox = React.useRef();
+    const dropdown = React.useRef();
+
+
+    const searchbox_dropdown_item_style = idx => {
+        return {
+            padding: "0.5em",
+            cursor: "pointer",
+            backgroundColor: (focused === idx) ? "#ffe0e0" : undefined
         };
-        this.form = React.createRef();
-        this.textbox = React.createRef();
-        this.dropdown = React.createRef();
     }
 
-    render() {
-
-        const self = this;
-
-        const setFocused = (fn) => {
-            self.setState({ focused: fn(self.state.focused) })
+    const onFormSubmit = event => {
+        event.preventDefault();
+        let terms = matches;
+        if (terms.length > 0) {
+            props.on_change(terms[focused])
         }
-
-        const searchbox_dropdown_item_style = idx => {
-            return {
-                padding: "0.5em",
-                cursor: "pointer",
-                backgroundColor: (this.state.focused === idx) ? "#ffe0e0" : undefined
-            };
-        }
-
-        const onFormSubmit = event => {
-            event.preventDefault();
-            let terms = self.state.matches;
-            if (terms.length > 0) {
-                self.props.on_change(terms[self.state.focused])
-            }
-            return false;
-        }
-
-        const get_input = () => {
-            var input = this.textbox.current.value;
-            input = normalize(input);
-            return input;
-        }
-
-        const reload_cache = () => {
-            const input = get_input();
-            console.log("RELOADING CACHE")
-            if (input == '') {
-                console.log("empty input; setting matches to empty array")
-                this.setState({ index_cache_uninitialized: false, matches_stale: false, matches: [] })
-                return;
-            }
-            const first_character = input[0];
-            if (this.state.first_character != first_character) {
-                this.setState({ index_cache_uninitialized: true });
-                (async () => {
-                    this.setState({ first_character: first_character })
-                    this.setState({ index_cache: await loadProtobuf(`/index/pages_${first_character}.gz`, "SearchIndex") });
-                    this.setState({ index_cache_uninitialized: false, matches_stale: true });
-                })();
-                return;
-            }
-            if (this.state.index_cache_uninitialized) {
-                return;
-            }
-        }
-
-        const onTextBoxKeyUp = (event) => {
-
-            reload_cache();
-            this.setState({ matches_stale: true });
-
-            // if down arrow, then go to the next one
-            let dropdowns = document.getElementsByClassName("searchbox-dropdown-item");
-            if (dropdowns.length > 0) {
-                if (event.key == "ArrowDown") {
-                    setFocused(focused => (focused + 1) % dropdowns.length)
-                }
-                if (event.key == "ArrowUp") {
-                    setFocused(focused => (focused - 1) % dropdowns.length)
-                }
-            }
-        }
-
-        const update_matches = async () => {
-            const input = get_input();
-            if (input == '') {
-                if (this.state.matches.length > 0) {
-                    this.setState({ matches: [] });
-                }
-                return;
-            }
-            const values = this.state.index_cache.elements;
-            const priorities = this.state.index_cache.priorities;
-            let matches = [];
-            for (let i = 0; i < values.length; i++) {
-                let match_count = is_a_match(input, normalize(values[i]));
-                if (match_count == 0) {
-                    continue;
-                }
-                if (!this.props.settings.show_historical_cds) {
-                    if (is_historical_cd(values[i])) {
-                        continue;
-                    }
-                }
-                if (is_international_duplicate(values[i])) {
-                    continue;
-                }
-                matches.push([match_count, i, match_count - priorities[i] / 10]);
-            }
-            matches = top_10(matches);
-            matches = matches.map(idx => values[idx]);
-            console.log("setting matches because of input", input, "matches", matches)
-            this.setState({ matches: matches });
-            this.setState({ matches_stale: false });
-        }
-
-        if (this.state.matches_stale && !this.state.index_cache_uninitialized) {
-            update_matches();
-        }
-
-        console.log("render: matches", this.state.matches);
-
-        return (
-            <form
-                autoComplete="off" ref={this.form}
-                style={{ marginBlockEnd: "0em", position: "relative", width: "100%" }}
-                onSubmit={onFormSubmit}
-            >
-                <input
-                    autoFocus={this.props.autoFocus}
-                    ref={this.textbox}
-                    id="searchbox"
-                    type="text"
-                    className="serif"
-                    style={{ backgroundColor: "#fff8f0", borderWidth: "0.1em", ...this.props.style }}
-                    placeholder={this.props.placeholder}
-                    onKeyUp={onTextBoxKeyUp}
-                />
-
-                <div ref={this.dropdown} style={
-                    {
-                        position: "absolute",
-                        width: "100%",
-                        maxHeight: "20em",
-                        overflowY: "auto",
-                        backgroundColor: "#f7f1e8",
-                        borderRadius: "0.25em",
-                        zIndex: "1"
-                    }
-                }>
-                    {
-                        this.state.matches.map((location, idx) =>
-                            <div
-                                key={location}
-                                className="serif searchbox-dropdown-item"
-                                style={searchbox_dropdown_item_style(idx)}
-                                onClick={() => this.props.on_change(this.state.matches[idx])}
-                                onMouseOver={() => this.setState({ focused: idx })}
-                            >{location}</div>
-                        )
-                    }
-                </div>
-            </form>
-        );
-
+        return false;
     }
+
+    const get_input = () => {
+        var input = textbox.current.value;
+        input = normalize(input);
+        return input;
+    }
+
+    const reload_cache = () => {
+        const input = get_input();
+        if (input == '') {
+            setIndexCacheUninitialized(false);
+            setMatchesStale(false);
+            setMatches([]);
+            return;
+        }
+        const currentFirstCharacter = input[0];
+        if (firstCharacter != currentFirstCharacter) {
+            setFirstCharacter(currentFirstCharacter);
+            (async () => {
+                setFirstCharacter(currentFirstCharacter);
+                setIndexCache(await loadProtobuf(`/index/pages_${currentFirstCharacter}.gz`, "SearchIndex"));
+                setIndexCacheUninitialized(false);
+                setMatchesStale(true);
+            })();
+            return;
+        }
+        if (indexCacheUninitialized) {
+            return;
+        }
+    }
+
+    const onTextBoxKeyUp = (event) => {
+
+        reload_cache();
+        // this.setState({ matches_stale: true });
+        setMatchesStale(true);
+
+        // if down arrow, then go to the next one
+        let dropdowns = document.getElementsByClassName("searchbox-dropdown-item");
+        if (dropdowns.length > 0) {
+            if (event.key == "ArrowDown") {
+                setFocused((focused + 1) % dropdowns.length)
+            }
+            if (event.key == "ArrowUp") {
+                setFocused((focused - 1) % dropdowns.length)
+            }
+        }
+    }
+
+    const update_matches = async () => {
+        const input = get_input();
+        if (input == '') {
+            if (matches.length > 0) {
+                setMatches([]);
+            }
+            return;
+        }
+        const values = indexCache.elements;
+        const priorities = indexCache.priorities;
+        let matches_new = [];
+        for (let i = 0; i < values.length; i++) {
+            let match_count = is_a_match(input, normalize(values[i]));
+            if (match_count == 0) {
+                continue;
+            }
+            if (!props.settings.show_historical_cds) {
+                if (is_historical_cd(values[i])) {
+                    continue;
+                }
+            }
+            if (is_international_duplicate(values[i])) {
+                continue;
+            }
+            matches_new.push([match_count, i, match_count - priorities[i] / 10]);
+        }
+        matches_new = top_10(matches_new);
+        matches_new = matches_new.map(idx => values[idx]);
+        setMatches(matches_new);
+        setMatchesStale(false);
+    }
+
+    if (matchesStale && !indexCacheUninitialized) {
+        update_matches();
+    }
+
+    return (
+        <form
+            autoComplete="off" ref={form}
+            style={{ marginBlockEnd: "0em", position: "relative", width: "100%" }}
+            onSubmit={onFormSubmit}
+        >
+            <input
+                autoFocus={props.autoFocus}
+                ref={textbox}
+                id="searchbox"
+                type="text"
+                className="serif"
+                style={{ backgroundColor: "#fff8f0", borderWidth: "0.1em", ...props.style }}
+                placeholder={props.placeholder}
+                onKeyUp={onTextBoxKeyUp}
+            />
+
+            <div ref={dropdown} style={
+                {
+                    position: "absolute",
+                    width: "100%",
+                    maxHeight: "20em",
+                    overflowY: "auto",
+                    backgroundColor: "#f7f1e8",
+                    borderRadius: "0.25em",
+                    zIndex: "1"
+                }
+            }>
+                {
+                    matches.map((location, idx) =>
+                        <div
+                            key={location}
+                            className="serif searchbox-dropdown-item"
+                            style={searchbox_dropdown_item_style(idx)}
+                            onClick={() => props.on_change(matches[idx])}
+                            onMouseOver={() =>
+                                setFocused(idx)
+                            }
+                        >{location}</div>
+                    )
+                }
+            </div>
+        </form>
+    );
 }
 
 function top_10(matches) {
