fd066e66713f9c57a78506881300811fa1e8b756
Kavi Gupta <kavi@berkeley.edu>
2024-07-17 02:05:29 -0400

    update load_json

--- react/src/load_json.ts
+++ react/src/load_json.ts
@@ -1,10 +1,12 @@
-export { loadJSON, loadProtobuf };
+export { loadJSON, loadProtobuf, load_ordering_protobuf, load_ordering };
 
 import { gunzipSync } from 'zlib';
 import {
     Article, Feature, StringList, ConsolidatedShapes,
-    ConsolidatedStatistics
+    ConsolidatedStatistics, DataLists, OrderLists, SearchIndex,
+    OrderList
 } from "./utils/protos";
+import { index_link, ordering_data_link, ordering_link } from './navigation/links';
 
 // from https://stackoverflow.com/a/4117299/1549476
 
@@ -12,6 +14,10 @@ import {
 function loadJSON(filePath: string) {
     // Load json file;
     const json = loadTextFileAjaxSync(filePath, "application/json");
+    // assert json is a string
+    if (typeof json !== "string") {
+        throw "file not found: " + filePath;
+    }
     // Parse json
     return JSON.parse(json);
 }
@@ -30,16 +36,21 @@ function loadTextFileAjaxSync(filePath: string, mimeType: string) {
         return xmlhttp.responseText;
     }
     else {
-        throw new Error(`Failed loading ${filePath} status ${xmlhttp.status} readystate ${xmlhttp.readyState}`)
+        // TODO Throw exception
+        return null;
     }
 }
+
 
 // Load a protobuf file from the server
 async function loadProtobuf(filePath: string, name: "Article"): Promise<Article>
 async function loadProtobuf(filePath: string, name: "Feature"): Promise<Feature>
 async function loadProtobuf(filePath: string, name: "StringList"): Promise<StringList>
+async function loadProtobuf(filePath: string, name: "OrderLists"): Promise<OrderLists>
+async function loadProtobuf(filePath: string, name: "DataLists"): Promise<DataLists>
 async function loadProtobuf(filePath: string, name: "ConsolidatedShapes"): Promise<ConsolidatedShapes>
 async function loadProtobuf(filePath: string, name: "ConsolidatedStatistics"): Promise<ConsolidatedStatistics>
+async function loadProtobuf(filePath: string, name: "SearchIndex"): Promise<SearchIndex>
 async function loadProtobuf(filePath: string, name: string) {
     const response = await fetch(filePath);
     const compressed_buffer = await response.arrayBuffer();
@@ -51,11 +62,45 @@ async function loadProtobuf(filePath: string, name: string) {
         return Feature.decode(arr);
     } else if (name == "StringList") {
         return StringList.decode(arr);
+    } else if (name == "OrderLists") {
+        return OrderLists.decode(arr);
+    } else if (name == "DataLists") {
+        return DataLists.decode(arr);
     } else if (name == "ConsolidatedShapes") {
         return ConsolidatedShapes.decode(arr);
     } else if (name == "ConsolidatedStatistics") {
         return ConsolidatedStatistics.decode(arr);
+    } else if (name == "SearchIndex") {
+        return SearchIndex.decode(arr);
     } else {
-        throw "protobuf type not recognized (see load_json.js)";
+        throw "protobuf type not recognized (see load_json.ts)";
     }
+}
+
+const order_links = require("./data/order_links.json");
+const data_links = require("./data/data_links.json");
+
+async function load_ordering_protobuf(universe: string, statpath: string, type: string, is_data: boolean) {
+    const links = is_data ? data_links : order_links;
+    const key = `${universe}__${type}__${statpath}`;
+    const idx = key in links ? links[key] : 0;
+    const order_link = is_data ? ordering_data_link(universe, type, idx) : ordering_link(universe, type, idx);
+    if (is_data) {
+        const dataLists = await loadProtobuf(order_link, "DataLists");
+        const index = dataLists.statnames.indexOf(statpath);
+        return dataLists.dataLists[index];
+    } else {
+        const orderLists = await loadProtobuf(order_link, "OrderLists");
+        const index = orderLists.statnames.indexOf(statpath);
+        return orderLists.orderLists[index];
+    }
+}
+
+async function load_ordering(universe: string, statpath: string, type: string) {
+    const idx_link = index_link(universe, type);
+    const data_promise = loadProtobuf(idx_link, "StringList");
+    const ordering_promise = load_ordering_protobuf(universe, statpath, type, false);
+    const [data, ordering] = await Promise.all([data_promise, ordering_promise]);
+    const names_in_order = (ordering as OrderList).orderIdxs.map((i: number) => data.elements[i]);
+    return names_in_order;
 }
