e08e40be37a1b41ce5d44457b7416e07dae6c40e
Kavi Gupta <kavi@berkeley.edu>
2024-07-17 02:01:02 -0400

    comment

--- react/src/utils/is_historical.ts
+++ react/src/utils/is_historical.ts
@@ -1,3 +1,5 @@
+
 export function is_historical_cd(name: string) {
+    // if name starts with "Historical Congressional District"
     return name.startsWith("Historical Congressional District");
 }
