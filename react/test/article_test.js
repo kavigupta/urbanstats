b666e3d620e4bab3a0b3a56a1c7c50b26f852cfe
Kavi Gupta <kavi@berkeley.edu>
2024-07-18 15:48:47 -0400

    better mapping test

--- react/test/article_test.js
+++ react/test/article_test.js
@@ -731,6 +731,17 @@ test("state-map", async t => {
     await screencap(t, "state-map");
 })
 
+fixture('mapping-more-complex')
+    .page(TARGET + '/mapper.html?settings=H4sIAAAAAAAAA5WSwW6DMAyGXwV5l3ZqJ8ax10o797DbVCFDDUQLSeSErqjqu88BRpm0wyokhH7bn3%2FbXKEmWzO6ps8%2FlTnBDva2M6GHDVRKB2LYXYEMFpokWKH2JJHOlEFZE2OhdyRVs3S7baC02nLuA4a%2FMjZwRt1F6W0psYpNPOw%2BrmCwjfGLBOjieEHxytSaFoyDdZ3GSEn2DZqaklWWvqbbLM3SdXQz09xjtAETCcexjLwfRgZKtsnqkjwnbi2VE7xR8s1UT2njGLJQciQvE%2F7XFGR5c8nIeMDpcVhjLhbUacgZ8zlXRg5ZkhMXUExaaamqVKmmRtCClH%2BRqpuQF33u7p7Gq9%2BO4o2xdYtJtDKEDNPBW1zG4sIo3Bf0TsyohgmZzsQ%2B%2Fk%2BBOxIhcuR36WVAAQwwqXgq0vjAjy2R0pcsAgr09Lub9a1c6htvmskEzgIAAA%3D%3D')
+    // no local storage
+    .beforeEach(async t => {
+        await t.eval(() => localStorage.clear());
+    });
+
+test("mapping-more-complex", async t => {
+    await screencap(t, "mapping-more-complex");
+})
+
 fixture('random')
     .page(TARGET + "/random.html?sampleby=population&us_only=true")
     // no local storage
