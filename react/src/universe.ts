56c3d66a5735032b47b12d29a4c6a53c3b5f47da
Kavi Gupta <kavi@berkeley.edu>
2024-07-18 15:04:57 -0400

    add universe.js from master

---
+++ react/src/universe.ts
@@ -0,0 +1,59 @@
+
+
+export function get_universe(default_universe: string | undefined) {
+    return new URLSearchParams(window.location.search).get("universe") || default_universe
+}
+
+export function set_universe(universe: string) {
+    const params = new URLSearchParams(window.location.search);
+    params.set("universe", universe);
+    window.location.search = params.toString();
+}
+
+export function remove_universe_if_not_in(universes: string[]) {
+    const universe = get_universe(undefined);
+    if (universe == undefined || !universes.includes(universe)) {
+        // clear universe without actually reloading the page
+        const params = new URLSearchParams(window.location.search);
+        params.delete("universe");
+        window.history.replaceState({}, "", window.location.pathname + "?" + params.toString());
+    }
+}
+
+export function remove_universe_if_default(default_universe: string) {
+    if (get_universe(undefined) === default_universe) {
+        // clear universe without actually reloading the page
+        const params = new URLSearchParams(window.location.search);
+        params.delete("universe");
+        window.history.replaceState({}, "", window.location.pathname + "?" + params.toString());
+    }
+}
+
+export function default_article_universe(longname: string) {
+    console.log(longname);
+    // if longname contains USA, then default to USA
+    if (longname.includes("USA")) {
+        console.log("USA!!")
+        return "USA";
+    }
+    return "world";
+}
+
+export function default_comparison_universe(longnames: string[]) {
+    // if all longnames are the same universe, default to that universe
+    const universes = longnames.map(x => default_article_universe(x));
+    if (universes.every(x => x === universes[0])) {
+        return universes[0];
+    }
+    return "world";
+}
+
+export function universe_is_american(universe: string) {
+    // if universe ends with USA, then it's American
+    return universe.includes("USA");
+}
+
+export function longname_is_exclusively_american(universe: string) {
+    // if longname ends with ", USA", then it's exclusively American
+    return universe.endsWith(", USA");
+}
