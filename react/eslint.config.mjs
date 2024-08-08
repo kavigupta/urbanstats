2bac49d7c05204c78964784814855af7eb3c2f2f
Luke Brody <lukebrody@pavonine.co>
2024-08-07 21:59:35 -0700

    eslint setup

---
+++ react/eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  {
+    files: ['**/*.{ts,tsx,mts,cts}'],
+    rules: {
+      'no-undef': 'off',
+    },
+  },
+  {
+    ignores: ['**/*.js', 'src/utils/protos.d.ts'],
+  }
+);
