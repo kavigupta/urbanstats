e01401f56d44cf7845d912557d8d40226798e994
Kavi Gupta <kavi@berkeley.edu>
2024-08-31 21:30:03 -0400

    add dummy test

--- react/test/string_check_test.js
+++ react/test/string_check_test.js
@@ -1,5 +1,16 @@
 import { IS_TESTING } from "./test_utils";
 
-if (!IS_TESTING) {
-    throw new Error("String tests are in overwrite mode. Set IS_TESTING to true to run tests.");
-}
+
+
+fixture('mapping')
+    .page(TARGET)
+    // no local storage
+    .beforeEach(async t => {
+        await t.eval(() => localStorage.clear());
+    });
+
+test("state-map", async t => {
+    if (!IS_TESTING) {
+        throw new Error("String tests are in overwrite mode. Set IS_TESTING to true to run tests.");
+    }
+})
