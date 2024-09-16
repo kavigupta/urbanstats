6d8a8399798e96c72f5afc3c462c7305c50fd4d6
Kavi Gupta <kavi@berkeley.edu>
2024-09-16 09:39:20 -0700

    fix height

--- test.sh
+++ test.sh
@@ -2,14 +2,11 @@
 
 cd react
 
-for browser in chrome firefox ; do
+for browser in "chrome '--window-size=1400,800'" "firefox -width 1400 -height 800" ; do
     for test_file in test/*_test.ts ; do
         npx testcafe -e $browser $test_file
     done
 done
-
-# npx testcafe -e "chrome '--window-size=1400,800'" test/article_test.js
-# npx testcafe -e "firefox -width 1400 -height 800" test/article_test.js
 
 cd ..
 
