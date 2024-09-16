f1339efeec6b9e190becdd05c7a0e73d052a954a
Kavi Gupta <kavi@berkeley.edu>
2024-09-16 09:49:43 -0700

    update

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 
 cd react
 
-for browser in "chrome '--window-size=1400,800'" "firefox -width 1400 -height 800" ; do
+for browser in "chrome --no-first-run '--window-size=1400,800'" "firefox -width 1400 -height 800" ; do
     for test_file in test/*_test.ts ; do
-        npx testcafe -e $browser $test_file
+        npx testcafe -e "$browser" $test_file
     done
 done
 
