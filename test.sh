b77be71050b5b48a2aeb97665495d08fd29e4ee6
Kavi Gupta <kavi@berkeley.edu>
2024-09-16 17:29:21 -0400

    thumbnails in script

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ cd react
 
 for browser in "chrome --no-first-run '--window-size=1400,800' --hide-scrollbars --disable-gpu"; do
     for test_file in test/*_test.ts ; do
-        npx testcafe -e "$browser" $test_file
+        npx testcafe -e "$browser" $test_file -s thumbnails=false
     done
 done
 
