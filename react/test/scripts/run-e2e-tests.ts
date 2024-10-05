78f2fb96105b6332ae65da5703f0a052382df765
Luke Brody <lukebrody@pavonine.co>
2024-10-04 23:36:02 -0700

    Better options for runner

--- react/test/scripts/run-e2e-tests.ts
+++ react/test/scripts/run-e2e-tests.ts
@@ -8,17 +8,24 @@ import { argumentParser } from 'zodcli'
 
 import { runProxy } from './ci_proxy'
 
+function booleanArgument({ defaultValue }: { defaultValue: boolean }): z.ZodDefault<z.ZodUnion<[z.ZodEffects<z.ZodLiteral<'true'>, boolean, 'true'>, z.ZodEffects<z.ZodLiteral<'false'>, boolean, 'false'>, z.ZodEffects<z.ZodNull, boolean, null>]>> {
+    return z.union([
+        z.literal('true').transform(() => true),
+        z.literal('false').transform(() => false),
+        z.null().transform(() => defaultValue),
+    ]).default(null)
+}
+
 async function main(): Promise<void> {
     const options = argumentParser({
         options: z.object({
-            mode: z.union([z.literal('ci'), z.literal('local')]).default('local'), // Enum doesn't work?
+            proxyToGithub: booleanArgument({ defaultValue: false }),
+            browser: z.union([z.literal('chrome'), z.literal('chromium')]).default('chrome'),
             test: z.array(z.string()).default(['test/*_test.ts']),
             parallel: z.string().transform(string => parseInt(string)).default('1'),
-            headless: z.union([
-                z.literal('true').transform(() => true),
-                z.literal('false').transform(() => false),
-                z.null().transform(() => true),
-            ]).default(null),
+            headless: booleanArgument({ defaultValue: true }),
+            video: booleanArgument({ defaultValue: false }),
+            compareScreenshots: booleanArgument({ defaultValue: false }),
         }).strict(),
     }).parse(process.argv.slice(2))
 
@@ -32,7 +39,7 @@ async function main(): Promise<void> {
         void execa('bash', ['-c', 'fluxbox >/dev/null 2>&1'])
     }
 
-    if (options.mode === 'ci') {
+    if (options.proxyToGithub) {
         runProxy()
     }
 
@@ -51,19 +58,23 @@ async function main(): Promise<void> {
             return Promise.resolve(0)
         }
 
-        const failedTests = await testcafe.createRunner()
+        let runner = testcafe.createRunner()
             .src(`test/${test}.ts`)
-            .browsers([`${options.mode === 'ci' ? 'chromium' : 'chrome'} --window-size=1400,800 --hide-scrollbars --disable-gpu`])
-            .video('videos')
+            .browsers([`${options.browser} --window-size=1400,800 --hide-scrollbars --disable-gpu`])
             .screenshots(`screenshots/${test}`)
-            .run()
+
+        if (options.video) {
+            runner = runner.video('videos')
+        }
+
+        const failedTests = await runner.run()
 
         return failedTests + await runTest()
     }
 
     const testsFailed = (await Promise.all(Array.from({ length: options.parallel }).map(runTest))).reduce((a, n) => a + n, 0)
 
-    if (options.mode === 'local') {
+    if (options.compareScreenshots) {
         await Promise.all(tests.map(test => execa('python', ['tests/check_images.py', `--test=${test}`], {
             cwd: '..',
             stdio: 'inherit',
