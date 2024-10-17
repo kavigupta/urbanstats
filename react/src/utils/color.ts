c5bb06ee2e994a02f5c44f7dc48c9fea286f6d83
Kavi Gupta <kavi@berkeley.edu>
2024-10-17 01:19:22 -0400

    replace lighten with a more generic mixWithBackground function

--- react/src/utils/color.ts
+++ react/src/utils/color.ts
@@ -58,10 +58,10 @@ export function interpolate_color(ramp: Keypoints, item: number): string {
     return `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`
 }
 
-export function lighten(color: string, fraction: number): string {
+export function mixWithBackground(color: string, fraction: number, background: string): string {
     if (!(typeof color === 'string')) {
         throw new Error('color is not a string')
     }
-    const ramp: [number, string][] = [[0, color], [1, '#ffffff']]
+    const ramp: [number, string][] = [[0, color], [1, background]]
     return interpolate_color(ramp, fraction)
 }
