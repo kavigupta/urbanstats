0382eda372d7010ff2fde6ab695f82101cef9d9f
Kavi Gupta <kavi@berkeley.edu>
2024-10-22 22:03:24 -0400

    fix tests

--- react/test/symlink_test.ts
+++ react/test/symlink_test.ts
@@ -14,12 +14,12 @@ function symlink_test(name: string, link: string, expected: string | undefined =
 
 symlink_test('national', '/article.html?longname=Timor-Leste')
 symlink_test('subnational', '/article.html?longname=Haut-Lomami%2C+Congo%2C+The+Democratic+Republic+of+the')
-symlink_test('urban center', '/article.html?longname=Tehrān%2C+Iran%2C+Islamic+Republic+of')
+symlink_test('urban center', '/article.html?longname=Tehr%C4%81n%2C+Iran%2C+Islamic+Republic+of')
 symlink_test('5mpc', '/article.html?longname=Pyongyang+5MPC%2C+Korea%2C+Democratic+People%27s+Republic+of')
 
 symlink_test(
     'comparison',
-    '/comparison.html?longnames=%5B"Pyongyang+5MPC%2C+Korea%2C+Democratic+People%27s+Republic+of"%2C"Tehran+%28Outer%29+5MPC%2C+Iran%2C+Islamic+Republic+of"%5D',
+    '/comparison.html?longnames=%5B%22Pyongyang+5MPC%2C+Korea%2C+Democratic+People%27s+Republic+of%22%2C%22Tehran+%28Outer%29+5MPC%2C+Iran%2C+Islamic+Republic+of%22%5D',
 )
 
 symlink_test(
@@ -28,4 +28,4 @@ symlink_test(
     '/article.html?longname=USA',
 )
 
-symlink_test('usa-comparison', '/comparison.html?longnames=%5B%United+States+of+America%22%2C%22Canada%22%5D', '/comparison.html?longnames=%5B%22USA%22%2C%22Canada%22%5D')
+symlink_test('usa-comparison', '/comparison.html?longnames=%5B%22United+States+of+America%22%2C%22Canada%22%5D', '/comparison.html?longnames=%5B%22USA%22%2C%22Canada%22%5D')
