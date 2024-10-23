c29b13e54d76cc0d514f5d3998e3c18234b99cbd
Kavi Gupta <kavi@berkeley.edu>
2024-10-22 21:28:16 -0400

    ocmparison

--- react/test/symlink_test.ts
+++ react/test/symlink_test.ts
@@ -16,3 +16,8 @@ symlink_test('national', '/article.html?longname=Timor-Leste')
 symlink_test('subnational', '/article.html?longname=Haut-Lomami%2C+Congo%2C+The+Democratic+Republic+of+the')
 symlink_test('urban center', '/article.html?longname=Tehrān%2C+Iran%2C+Islamic+Republic+of')
 symlink_test('5mpc', '/article.html?longname=Pyongyang+5MPC%2C+Korea%2C+Democratic+People%27s+Republic+of')
+
+symlink_test(
+    'comparison',
+    '/comparison.html?longnames=%5B"Pyongyang+5MPC%2C+Korea%2C+Democratic+People%27s+Republic+of"%2C"Tehran+%28Outer%29+5MPC%2C+Iran%2C+Islamic+Republic+of"%5D',
+)
