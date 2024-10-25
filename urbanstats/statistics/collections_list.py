181bd85c753cddfce9c94886e07e7f7d83ea0724
Kavi Gupta <kavi@berkeley.edu>
2024-10-25 15:18:43 -0700

    switch to census 2020 framework

--- urbanstats/statistics/collections_list.py
+++ urbanstats/statistics/collections_list.py
@@ -1,6 +1,5 @@
 from urbanstats.statistics.collections.cdc_statistics import CDCStatistics
-from urbanstats.statistics.collections.census_2010 import Census2000, Census2010, CensusChange2000, CensusChange2010
-from urbanstats.statistics.collections.census_basics import CensusBasics
+from urbanstats.statistics.collections.census_2010 import Census2000, Census2010, Census2020, CensusChange2000, CensusChange2010
 from urbanstats.statistics.collections.education_gender_gap import (
     EducationGenderGapStatistics,
 )
@@ -10,7 +9,6 @@ from urbanstats.statistics.collections.generation import GenerationStatistics
 from urbanstats.statistics.collections.geographic import AreaAndCompactnessStatistics
 from urbanstats.statistics.collections.gpw import GPWStatistics
 from urbanstats.statistics.collections.heating import HouseHeating
-from urbanstats.statistics.collections.housing_census import HousingCensus
 from urbanstats.statistics.collections.housing_rent import HousingRent
 from urbanstats.statistics.collections.housing_rent_burden import HousingRentBurden
 from urbanstats.statistics.collections.housing_rent_or_own import HousingRentOrOwn
@@ -34,7 +32,6 @@ from urbanstats.statistics.collections.national_origin_language import (
     NationalOriginLanguageStatistics,
 )
 from urbanstats.statistics.collections.occupation import OccupationStatistics
-from urbanstats.statistics.collections.race_census import RaceCensus
 from urbanstats.statistics.collections.segregation import SegregationStatistics
 from urbanstats.statistics.collections.sexual_orientation_and_relationship_status import (
     SexualOrientationRelationshipStatusStatistics,
@@ -54,9 +51,7 @@ from urbanstats.statistics.collections.usda_fra_statistics import USDAFRAStatist
 from urbanstats.statistics.collections.weather import USWeatherStatistics
 
 statistic_collections = (
-    CensusBasics(),
-    RaceCensus(),
-    HousingCensus(),
+    Census2020(),
     Census2010(),
     CensusChange2010(),
     Census2000(),
