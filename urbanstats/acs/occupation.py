62d005e0d62ba67d739814910cdf37c58fe2e61b
Kavi Gupta <kavi@berkeley.edu>
2024-10-26 17:30:23 -0400

    more ignore

--- urbanstats/acs/occupation.py
+++ urbanstats/acs/occupation.py
@@ -2,6 +2,8 @@ from urbanstats.acs.every_subcategory import produce_subcategories
 
 from .load import ACSDataEntity
 
+# pylint: disable=line-too-long
+
 list_of_occupations_female = [
     "Estimate!!Total:",
     "Estimate!!Total:!!Female:",
