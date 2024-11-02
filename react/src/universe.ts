8516b5726c6ccf9284c103c89f0bc47ff8fd8d97
Kavi Gupta <kavi@berkeley.edu>
2024-11-01 17:33:43 -0700

    fix bug in universe detection

--- react/src/universe.ts
+++ react/src/universe.ts
@@ -54,7 +54,7 @@ export function default_comparison_universe(longnames: string[]): 'USA' | 'world
 
 export function universe_is_american(universe: string): boolean {
     // if universe ends with USA, then it's American
-    return universe.endsWith(', USA')
+    return universe.endsWith(', USA') || universe === 'USA'
 }
 
 export function longname_is_exclusively_american(universe: string): boolean {
