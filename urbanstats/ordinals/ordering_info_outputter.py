7df37b75d37ee1e3a45cdadb0a586aa406c46ed9
Kavi Gupta <kavi@berkeley.edu>
2024-11-02 23:50:56 -0400

    typed python statistics tree (#524)
    this doesn't change anything about the frontend

--- urbanstats/ordinals/ordering_info_outputter.py
+++ urbanstats/ordinals/ordering_info_outputter.py
@@ -5,10 +5,8 @@ import tqdm.auto as tqdm
 from urbanstats.ordinals.compress_counts import compress_counts, mapify
 from urbanstats.protobuf import data_files_pb2
 from urbanstats.protobuf.utils import save_string_list, write_gzip
-from urbanstats.statistics.output_statistics_metadata import (
-    get_statistic_column_path,
-    internal_statistic_names,
-)
+from urbanstats.statistics.output_statistics_metadata import internal_statistic_names
+from urbanstats.statistics.stat_path import get_statistic_column_path
 from urbanstats.universe.annotate_universes import all_universes
 
 
