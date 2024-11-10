3d3d3d07cac544192fbacf4cdb6320fae63425fb
Luke Brody <luke@lukebrody.com>
2024-11-09 23:08:40 -0800

    Include whether stats are expanded when sharing links (#559)

--- react/src/components/QuerySettingsConnection.tsx
+++ react/src/components/QuerySettingsConnection.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode, useContext, useEffect } from 'react'
 
-import { Settings, SettingsDictionary } from '../page_template/settings'
+import { Settings, SettingsDictionary, statPathsWithExtra } from '../page_template/settings'
 import { fromVector, useVector, VectorSettingKey } from '../page_template/settings-vector'
-import { useAvailableGroups, useAvailableYears } from '../page_template/statistic-settings'
+import { useAvailableGroups, useAvailableYears, useStatPaths } from '../page_template/statistic-settings'
 
 /**
  * - Query Params -> Settings
@@ -11,8 +11,11 @@ import { useAvailableGroups, useAvailableYears } from '../page_template/statisti
  *
  * - Settings -> Query Params
  *   Watches settings keys and reflects in query params. Must include all settings
+ *
+ * For `stagedSettingsKeys`, changes will launch the interface into staging mode.
+ * For `applySettingsKeys`, these setting values will be automatically applied without entering staging mode.
  */
-export function QuerySettingsConnection({ settingsKeys }: { settingsKeys: VectorSettingKey[] }): null {
+export function QuerySettingsConnection({ stagedSettingsKeys, applySettingsKeys }: { stagedSettingsKeys: VectorSettingKey[], applySettingsKeys: VectorSettingKey[] }): null {
     const settings = useContext(Settings.Context)
 
     // Query Params -> Settings
@@ -23,8 +26,11 @@ export function QuerySettingsConnection({ settingsKeys }: { settingsKeys: Vector
             return
         }
         const settingsFromQueryParams = fromVector(settingsVector, settings)
-        if (settingsKeys.some(key => JSON.stringify(settingsFromQueryParams[key]) !== JSON.stringify(settings.get(key)))) {
-            settings.enterStagedMode(Object.fromEntries(settingsKeys.map(key => [key, settingsFromQueryParams[key]])) as unknown as Partial<SettingsDictionary>)
+        if (stagedSettingsKeys.some(key => JSON.stringify(settingsFromQueryParams[key]) !== JSON.stringify(settings.get(key)))) {
+            settings.enterStagedMode(Object.fromEntries(stagedSettingsKeys.map(key => [key, settingsFromQueryParams[key]])) as unknown as Partial<SettingsDictionary>)
+        }
+        for (const applySettingsKey of applySettingsKeys) {
+            settings.setSetting(applySettingsKey, settingsFromQueryParams[applySettingsKey])
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps -- We should only load params on first load.
     }, [])
@@ -40,19 +46,22 @@ export function QuerySettingsConnection({ settingsKeys }: { settingsKeys: Vector
         }
 
         // If we're in staged mode, our monitored settings are the same as persisted, exit staged mode
-        if (settings.getStagedKeys() !== undefined && settingsKeys.every((key) => {
+        if (settings.getStagedKeys() !== undefined && stagedSettingsKeys.every((key) => {
             const info = settings.getSettingInfo(key)
             return JSON.stringify(info.persistedValue) === JSON.stringify(info.stagedValue)
         })) {
             settings.exitStagedMode('discard')
         }
-    }, [settingsVector, settingsKeys, settings])
+    }, [settingsVector, stagedSettingsKeys, settings])
 
     return null
 }
 
 export function ArticleComparisonQuerySettingsConnection(): ReactNode {
-    const settingsKeys: VectorSettingKey[] = [
+    const availableStatPaths = useStatPaths()
+    const availableStatPathsWithExtra = statPathsWithExtra.filter(path => availableStatPaths.includes(path))
+
+    const stagedSettingsKeys: VectorSettingKey[] = [
         'use_imperial',
         'show_historical_cds',
         'simple_ordinals',
@@ -60,5 +69,9 @@ export function ArticleComparisonQuerySettingsConnection(): ReactNode {
         ...useAvailableGroups().map(group => `show_stat_group_${group.id}` as const),
     ]
 
-    return <QuerySettingsConnection settingsKeys={settingsKeys} />
+    const applySettingsKeys: VectorSettingKey[] = [
+        ...availableStatPathsWithExtra.map(path => `expanded__${path}` as const),
+    ]
+
+    return <QuerySettingsConnection stagedSettingsKeys={stagedSettingsKeys} applySettingsKeys={applySettingsKeys} />
 }
