kavigupta/urbanstats

commit b1e33849bbbcaf0fbcf13e7de6061cfa98d3cb24

require build pass (#545)

urbanstats/website_data/build.py
@@ -98,7 +98,7 @@ def create_react_jsons():
 
 
 def build_react_site(site_folder, dev):
-    os.system(f"cd react; npm {'i' if dev else 'ci'}")
+    subprocess.run(f"cd react; npm {'i' if dev else 'ci'}", shell=True, check=True)
 
     create_react_jsons()
 
@@ -108,7 +108,9 @@ def build_react_site(site_folder, dev):
         cwd="react",
     )
 
-    os.system(f"cd react; npm run {'dev' if dev else 'prod'}")
+    subprocess.run(
+        f"cd react; npm run {'dev' if dev else 'prod'}", shell=True, check=True
+    )
 
     link_scripts_folder(site_folder, dev)
 

commit 6b83cd9d9c0c7255b3113ff6aaf04b26721c593b

Unbreak watch command when running on broken code (#550)
Broken in #545 

Should only fail in prod mode

urbanstats/website_data/build.py
@@ -121,7 +121,7 @@ def build_react_site(site_folder, dev):
     )
 
     subprocess.run(
-        f"cd react; npm run {'dev' if dev else 'prod'}", shell=True, check=True
+        f"cd react; npm run {'dev' if dev else 'prod'}", shell=True, check=not dev
     )
 
     link_scripts_folder(site_folder, dev)