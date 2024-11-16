730a089967a12559769fc67af1f222b7151a11ad
Kavi Gupta <kavi@berkeley.edu>
2024-11-16 11:00:25 -0800

    update symlinks

---
+++ urbanstats/special_cases/symlinks/symlinks_from_subnational_usa_fixes.py
@@ -0,0 +1,5 @@
+symlinks_from_subnational_usa_fixes = {
+    "Ciudad Ojeda 5MPC, Venezuela-USA-France": "Ciudad Ojeda 5MPC, Venezuela-France-USA",
+    "Acapulco 20MPC, Canada-USA-Mexico": "Acapulco 20MPC, Canada-Mexico-USA",
+    "Virginia Beach 20MPC, USA": "Virginia Beach 20MPC, USA-Canada",
+}
