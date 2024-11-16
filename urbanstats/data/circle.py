0fa0d165c53b6fcb5e2df2873771d6e395ac5333
Kavi Gupta <kavi@berkeley.edu>
2024-11-16 03:11:27 -0500

    add prints to circle

--- urbanstats/data/circle.py
+++ urbanstats/data/circle.py
@@ -521,6 +521,7 @@ def specify_duplicates(frame, long_to_short):
 def overlapping_circles_frame(
     country_shapefile, population, suffix, max_radius_in_chunks=20
 ):
+    print(f"population circles being computed for {suffix}")
     ghs = load_full_ghs()
     circles = overlapping_circles_fast(
         ghs, population, limit=10**9, max_radius_in_chunks=max_radius_in_chunks
@@ -536,6 +537,7 @@ def overlapping_circles_frame(
         shapely.geometry.box(-180, -89, 180, 89)
     )
     assert len(frame) == len(set(frame.longname))
+    print(f"done with {suffix}")
     return frame
 
 
