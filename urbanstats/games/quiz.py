e13bc7a1b8f0d69f2fd8858818465788a2b6e4ac
Kavi Gupta <kavi@berkeley.edu>
2024-11-21 12:12:23 -0800

    bump juxtaversion

--- urbanstats/games/quiz.py
+++ urbanstats/games/quiz.py
@@ -34,7 +34,7 @@ from .quiz_custom import get_custom_quizzes
 
 min_pop = 250_000
 min_pop_international = 2_500_000
-version_numeric = 77
+version_numeric = 78
 
 version = str(version_numeric) + stable_hash(statistic_collections)
 
