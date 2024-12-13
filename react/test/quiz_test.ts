9de8fbcddecbf9768301553e3ce98a4902e41427
Kavi Gupta <kavi@berkeley.edu>
2024-12-12 23:48:55 -0500

    extract quiz test utils

--- react/test/quiz_test.ts
+++ react/test/quiz_test.ts
@@ -2,36 +2,10 @@ import { exec } from 'child_process'
 import { writeFileSync, readFileSync } from 'fs'
 import { promisify } from 'util'
 
-import { execa } from 'execa'
-import { ClientFunction, RequestHook, Selector } from 'testcafe'
+import { ClientFunction, Selector } from 'testcafe'
 
-import { TARGET, getLocation, most_recent_download_path, safeReload, screencap, urbanstatsFixture } from './test_utils'
-
-async function quiz_screencap(t: TestController): Promise<void> {
-    await t.eval(() => {
-        const elem = document.getElementById('quiz-timer')
-        if (elem) {
-            elem.remove()
-        }
-    })
-    await t.wait(1000)
-    await screencap(t)
-}
-
-export class ProxyPersistent extends RequestHook {
-    override onRequest(e: { requestOptions: RequestMockOptions }): void {
-        if (e.requestOptions.hostname === 'persistent.urbanstats.org') {
-            e.requestOptions.hostname = 'localhost'
-            e.requestOptions.port = 54579
-            e.requestOptions.protocol = 'http:'
-            e.requestOptions.path = e.requestOptions.path.replace('https://persistent.urbanstats.org', 'localhost:54579')
-            e.requestOptions.host = 'localhost:54579'
-        }
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-empty-function -- TestCafe complains if we don't have this
-    override onResponse(): void { }
-}
+import { click_button, click_buttons, quiz_fixture, quiz_screencap, tempfile_name } from './quiz_test_utils'
+import { TARGET, getLocation, most_recent_download_path, safeReload, screencap } from './test_utils'
 
 async function run_query(query: string): Promise<string> {
     // dump given query to a string
@@ -50,51 +24,6 @@ function retrostat_table(): Promise<string> {
 
 function secure_id_table(): Promise<string> {
     return run_query('SELECT user, secure_id from JuxtaStatUserSecureID')
-}
-
-function tempfile_name(): string {
-    return `/tmp/quiz_test_${Math.floor(Math.random() * 1000000)}`
-}
-
-function quiz_fixture(fix_name: string, url: string, new_localstorage: Record<string, string>, sql_statements: string): void {
-    urbanstatsFixture(fix_name, url, async (t) => {
-        // create a temporary file
-        const tempfile = `${tempfile_name()}.sql`
-        // write the sql statements to the temporary file
-        writeFileSync(tempfile, sql_statements)
-        await promisify(exec)(`rm -f ../urbanstats-persistent-data/db.sqlite3; cd ../urbanstats-persistent-data; cat ${tempfile} | sqlite3 db.sqlite3; cd -`)
-        void execa('bash', ['../urbanstats-persistent-data/run_for_test.sh'], { stdio: 'inherit' })
-        await t.wait(2000)
-        await t.eval(() => {
-            localStorage.clear()
-            for (const k of Object.keys(new_localstorage)) {
-                localStorage.setItem(k, new_localstorage[k])
-            }
-        }, { dependencies: { new_localstorage } })
-        await t.eval(() => {
-            localStorage.setItem('testHostname', 'urbanstats.org')
-        })
-        // Must reload after setting localstorage so page picks it up
-        await safeReload(t)
-    })
-        .afterEach(async (t) => {
-            exec('killall gunicorn')
-            await t.wait(1000)
-        })
-        .requestHooks(new ProxyPersistent())
-}
-
-// click the kth button with id quiz-answer-button-$which
-function click_button(t: TestController, which: string): TestControllerPromise {
-    return t.click(Selector('div').withAttribute('id', `quiz-answer-button-${which}`))
-}
-
-async function click_buttons(t: TestController, whichs: string[]): Promise<void> {
-    for (const which of whichs) {
-        await click_button(t, which)
-        await t.wait(500)
-    }
-    await t.wait(2000)
 }
 
 function example_quiz_history(min_quiz: number, max_quiz: number, min_retro?: number, max_retro?: number): Record<string | number, { choices: ('A' | 'B')[], correct_pattern: [boolean, boolean, boolean, boolean, boolean] }> {
