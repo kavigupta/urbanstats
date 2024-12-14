923c7a130a9ffcc2a52b76f6f3061db91417c286
Kavi Gupta <kavi@berkeley.edu>
2024-12-13 22:03:04 -0500

    make tests generic

--- react/test/quiz_friends_test.ts
+++ react/test/quiz_friends_test.ts
@@ -95,129 +95,153 @@ async function removeFriend(t: TestController, nth: number): Promise<void> {
     await t.click(Selector('button').withText('Remove').nth(nth))
 }
 
-quiz_fixture(
-    'basic friends test',
-    `${TARGET}/quiz.html#date=99`,
-    { },
-    '',
-)
+function testsGeneric(): void {
+    const alicePattern = 'yyyyn'
+    const bobPattern = 'nnnny'
+    const charliePattern = 'ynynn'
+    const charliePatternPrev = 'nnnny'
+    const aliceOtherPattern = 'yyynn'
+    const today = 'quiz.html#date=99'
+    const yesterday = 'quiz.html#date=98'
+    const other = 'quiz.html#date=11&mode=retro'
 
-async function aliceBobFriends(t: TestController, screenshots: boolean): Promise<JuxtastatUserState> {
-    const state = starting_state()
-    // Alice does the quiz
-    await create_user(t, 'Alice', '000000a', state)
-    await click_buttons(t, ['a', 'a', 'a', 'a', 'a'])
-    await t.expect(await friendsText(t)).eql(['Youyyyyn'])
-    await create_user(t, 'Bob', '000000b', state)
-    await click_buttons(t, ['b', 'b', 'b', 'b', 'b'])
-    await t.expect(await friendsText(t)).eql(['Younnnny'])
-    await addFriend(t, 'Alice', '000000a')
-    if (screenshots) {
-        await quiz_screencap(t) // screencap of pending friend request
+    quiz_fixture(
+        'juxtastat friends test',
+        `${TARGET}/${today}`,
+        { },
+        '',
+    )
+
+    async function aliceBobFriends(t: TestController, screenshots: boolean): Promise<JuxtastatUserState> {
+        const state = starting_state()
+        // Alice does the quiz
+        await create_user(t, 'Alice', '000000a', state)
+        await click_buttons(t, ['a', 'a', 'a', 'a', 'a'])
+        await t.expect(await friendsText(t)).eql([`You${alicePattern}`])
+        await create_user(t, 'Bob', '000000b', state)
+        await click_buttons(t, ['b', 'b', 'b', 'b', 'b'])
+        await t.expect(await friendsText(t)).eql([`You${bobPattern}`])
+        await addFriend(t, 'Alice', '000000a')
+        if (screenshots) {
+            await quiz_screencap(t) // screencap of pending friend request
+        }
+        await t.expect(await friendsText(t)).eql([`You${bobPattern}`, 'AlicePending Friend RequestRemove'])
+        await restore_user(t, 'Alice', state)
+        await addFriend(t, 'Bob', '000000b')
+        // Alice and Bob are now friends
+        if (screenshots) {
+            await quiz_screencap(t) // screencap of friends' score
+        }
+        await t.expect(await friendsText(t)).eql([`You${alicePattern}`, `Bob${bobPattern}Remove`])
+        return state
     }
-    await t.expect(await friendsText(t)).eql(['Younnnny', 'AlicePending Friend RequestRemove'])
-    await restore_user(t, 'Alice', state)
-    await addFriend(t, 'Bob', '000000b')
-    // Alice and Bob are now friends
-    if (screenshots) {
-        await quiz_screencap(t) // screencap of friends' score
-    }
-    await t.expect(await friendsText(t)).eql(['Youyyyyn', 'BobnnnnyRemove'])
-    return state
+
+    test('basic-friends-test', async (t) => {
+        const state = await aliceBobFriends(t, true)
+        // Charlie hasn't done the quiz yet (they register on #98 instead)
+        await create_user(t, 'Charlie', '000000c', state)
+        await t.eval(() => {
+            // eslint-disable-next-line no-restricted-syntax -- Localstorage is not reactive
+            window.location.href = `${window.location.origin}/${yesterday}`
+        }, { dependencies: { yesterday } })
+        await click_buttons(t, ['a', 'b', 'a', 'b', 'a'])
+        await addFriend(t, 'Alice', '000000a')
+        await t.expect(await friendsText(t)).eql([`You${charliePatternPrev}`, 'AlicePending Friend RequestRemove'])
+        await restore_user(t, 'Alice', state)
+        await t.eval(() => {
+            // eslint-disable-next-line no-restricted-syntax -- Localstorage is not reactive
+            window.location.href = `${window.location.origin}/${today}`
+        }, { dependencies: { today } })
+        await addFriend(t, 'Charlie', '000000c')
+        // Alice and Charlie are now friends
+        await quiz_screencap(t) // screencap of friend who hasn't done the quiz yet
+        await t.expect(await friendsText(t)).eql([`You${alicePattern}`, `Bob${bobPattern}Remove`, 'CharlieNot Done YetRemove'])
+        // Charlie now does the quiz
+        await restore_user(t, 'Charlie', state)
+        await click_buttons(t, ['a', 'b', 'a', 'b', 'a'])
+        await t.expect(await friendsText(t)).eql([`You${charliePattern}`, `Alice${alicePattern}Remove`])
+        await restore_user(t, 'Alice', state)
+        await quiz_screencap(t) // multiple friends who have done the quiz
+        await t.expect(await friendsText(t)).eql([`You${alicePattern}`, `Bob${bobPattern}Remove`, `Charlie${charliePattern}Remove`])
+        // Alice unfriends Bob
+        await removeFriend(t, 0)
+        // wait until there's only one Remove button. up to 1 second
+        await t.expect(Selector('button').withText('Remove').count).eql(1, { timeout: 1000 })
+        await quiz_screencap(t) // unfriended Bob
+        await t.expect(await friendsText(t)).eql([`You${alicePattern}`, `Charlie${charliePattern}Remove`])
+        // Bob no longer sees Alice's score
+        await restore_user(t, 'Bob', state)
+        await quiz_screencap(t) // Bob no longer sees Alice's score
+        await t.expect(await friendsText(t)).eql([`You${bobPattern}`, 'AlicePending Friend RequestRemove'])
+        // rename Alice to Alice2 by clicking on the Alice name and changing it
+        const aliceName = Selector('span').withAttribute('class', 'editable_content').nth(0)
+        await t.click(aliceName)
+        await t.pressKey('ctrl+a')
+        await t.typeText(aliceName, 'Alice2')
+        await t.pressKey('enter')
+        await t.expect(await friendsText(t)).eql([`You${bobPattern}`, 'Alice2Pending Friend RequestRemove'])
+        // refresh
+        await t.eval(() => {
+            // eslint-disable-next-line no-restricted-syntax -- Make sure changes are saved
+            window.location.reload()
+        })
+        await t.wait(1000)
+        await t.expect(await friendsText(t)).eql([`You${bobPattern}`, 'Alice2Pending Friend RequestRemove'])
+    })
+
+    test('friends-bad-naming-test', async (t) => {
+        const state = await aliceBobFriends(t, false)
+        await restore_user(t, 'Alice', state)
+        // should error because we are attempting to add the same user under a different name
+        await addFriend(t, 'Bob2', '000000b')
+        // Bob2 not added
+        await t.expect(await friendsText(t)).eql([`You${alicePattern}`, `Bob${bobPattern}Remove`])
+        // check that the text 'Friend ID 000000b already exists as Bob' is displayed
+        await t.expect(Selector('div').withText('Friend ID 000000b already exists as Bob').exists).ok()
+        // should error because we are attempting to add a duplicate name
+        await addFriend(t, 'Bob', 'abc')
+        // Bob not added
+        await t.expect(await friendsText(t)).eql([`You${alicePattern}`, `Bob${bobPattern}Remove`])
+        // check that the text 'Friend Name Bob already exists' is displayed
+        await t.expect(Selector('div').withText('Friend name already exists').exists).ok()
+        // should error because we are attempting to add an empty name
+        await addFriend(t, '', 'abc')
+        // empty name not added
+        await t.expect(await friendsText(t)).eql([`You${alicePattern}`, `Bob${bobPattern}Remove`])
+        // check that the text 'Friend name cannot be empty' is displayed
+        await t.expect(Selector('div').withText('Friend name cannot be empty').exists).ok()
+        // should error because we are attempting to add an empty ID
+        await addFriend(t, 'Bob2', '')
+        // empty ID not added
+        await t.expect(await friendsText(t)).eql([`You${alicePattern}`, `Bob${bobPattern}Remove`])
+        // check that the text 'Friend ID cannot be empty' is displayed
+        await t.expect(Selector('div').withText('Friend ID cannot be empty').exists).ok()
+        // add an additional friend
+        await addFriend(t, 'Charlie', '000000c')
+        // Charlie added
+        await t.expect(await friendsText(t)).eql([`You${alicePattern}`, `Bob${bobPattern}Remove`, 'CharliePending Friend RequestRemove'])
+        // attempting to rename Charlie to Bob should error
+        const charlieName = Selector('span').withAttribute('class', 'editable_content').nth(1)
+        await t.click(charlieName)
+        await t.pressKey('ctrl+a')
+        await t.typeText(charlieName, 'Bob')
+        await t.pressKey('enter')
+        // Charlie not renamed
+        await t.expect(await friendsText(t)).eql([`You${alicePattern}`, `Bob${bobPattern}Remove`, 'CharliePending Friend RequestRemove'])
+        // check that the text 'Friend name already exists' is displayed
+        await t.expect(Selector('div').withText('Friend name already exists').exists).ok()
+    })
+
+    test('same-on-juxta-and-retro', async (t) => {
+        await aliceBobFriends(t, false)
+        // same on retrostat
+        await t.eval(() => {
+            // eslint-disable-next-line no-restricted-syntax -- Localstorage is not reactive
+            window.location.href = `${window.location.origin}/${other}`
+        }, { dependencies: { other } })
+        await click_buttons(t, ['a', 'a', 'a', 'a', 'a'])
+        await t.expect(await friendsText(t)).eql([`You${aliceOtherPattern}`, 'BobNot Done YetRemove'])
+    })
 }
 
-test('basic-friends-test', async (t) => {
-    const state = await aliceBobFriends(t, true)
-    // Charlie hasn't done the quiz yet (they register on #98 instead)
-    await create_user(t, 'Charlie', '000000c', state)
-    await t.eval(() => {
-        // eslint-disable-next-line no-restricted-syntax -- Localstorage is not reactive
-        window.location.href = `${window.location.origin}/quiz.html#date=98`
-    })
-    await click_buttons(t, ['a', 'a', 'a', 'a', 'a'])
-    await addFriend(t, 'Alice', '000000a')
-    await t.expect(await friendsText(t)).eql(['Younynyy', 'AlicePending Friend RequestRemove'])
-    await restore_user(t, 'Alice', state)
-    await t.eval(() => {
-        // eslint-disable-next-line no-restricted-syntax -- Localstorage is not reactive
-        window.location.href = `${window.location.origin}/quiz.html#date=99`
-    })
-    await addFriend(t, 'Charlie', '000000c')
-    // Alice and Charlie are now friends
-    await quiz_screencap(t) // screencap of friend who hasn't done the quiz yet
-    await t.expect(await friendsText(t)).eql(['Youyyyyn', 'BobnnnnyRemove', 'CharlieNot Done YetRemove'])
-    // Charlie now does the quiz
-    await restore_user(t, 'Charlie', state)
-    await click_buttons(t, ['a', 'b', 'a', 'b', 'a'])
-    await t.expect(await friendsText(t)).eql(['Youynynn', 'AliceyyyynRemove'])
-    await restore_user(t, 'Alice', state)
-    await quiz_screencap(t) // multiple friends who have done the quiz
-    await t.expect(await friendsText(t)).eql(['Youyyyyn', 'BobnnnnyRemove', 'CharlieynynnRemove'])
-    // Alice unfriends Bob
-    await removeFriend(t, 0)
-    // wait until there's only one Remove button. up to 1 second
-    await t.expect(Selector('button').withText('Remove').count).eql(1, { timeout: 1000 })
-    await quiz_screencap(t) // unfriended Bob
-    await t.expect(await friendsText(t)).eql(['Youyyyyn', 'CharlieynynnRemove'])
-    // Bob no longer sees Alice's score
-    await restore_user(t, 'Bob', state)
-    await quiz_screencap(t) // Bob no longer sees Alice's score
-    await t.expect(await friendsText(t)).eql(['Younnnny', 'AlicePending Friend RequestRemove'])
-    // rename Alice to Alice2 by clicking on the Alice name and changing it
-    const aliceName = Selector('span').withAttribute('class', 'editable_content').nth(0)
-    await t.click(aliceName)
-    await t.pressKey('ctrl+a')
-    await t.typeText(aliceName, 'Alice2')
-    await t.pressKey('enter')
-    await t.expect(await friendsText(t)).eql(['Younnnny', 'Alice2Pending Friend RequestRemove'])
-    // refresh
-    await t.eval(() => {
-        // eslint-disable-next-line no-restricted-syntax -- Make sure changes are saved
-        window.location.reload()
-    })
-    await t.wait(1000)
-    await t.expect(await friendsText(t)).eql(['Younnnny', 'Alice2Pending Friend RequestRemove'])
-})
-
-test('friends-bad-naming-test', async (t) => {
-    const state = await aliceBobFriends(t, false)
-    await restore_user(t, 'Alice', state)
-    // should error because we are attempting to add the same user under a different name
-    await addFriend(t, 'Bob2', '000000b')
-    // Bob2 not added
-    await t.expect(await friendsText(t)).eql(['Youyyyyn', 'BobnnnnyRemove'])
-    // check that the text 'Friend ID 000000b already exists as Bob' is displayed
-    await t.expect(Selector('div').withText('Friend ID 000000b already exists as Bob').exists).ok()
-    // should error because we are attempting to add a duplicate name
-    await addFriend(t, 'Bob', 'abc')
-    // Bob not added
-    await t.expect(await friendsText(t)).eql(['Youyyyyn', 'BobnnnnyRemove'])
-    // check that the text 'Friend Name Bob already exists' is displayed
-    await t.expect(Selector('div').withText('Friend name already exists').exists).ok()
-    // should error because we are attempting to add an empty name
-    await addFriend(t, '', 'abc')
-    // empty name not added
-    await t.expect(await friendsText(t)).eql(['Youyyyyn', 'BobnnnnyRemove'])
-    // check that the text 'Friend name cannot be empty' is displayed
-    await t.expect(Selector('div').withText('Friend name cannot be empty').exists).ok()
-    // should error because we are attempting to add an empty ID
-    await addFriend(t, 'Bob2', '')
-    // empty ID not added
-    await t.expect(await friendsText(t)).eql(['Youyyyyn', 'BobnnnnyRemove'])
-    // check that the text 'Friend ID cannot be empty' is displayed
-    await t.expect(Selector('div').withText('Friend ID cannot be empty').exists).ok()
-    // add an additional friend
-    await addFriend(t, 'Charlie', '000000c')
-    // Charlie added
-    await t.expect(await friendsText(t)).eql(['Youyyyyn', 'BobnnnnyRemove', 'CharliePending Friend RequestRemove'])
-    // attempting to rename Charlie to Bob should error
-    const charlieName = Selector('span').withAttribute('class', 'editable_content').nth(1)
-    await t.click(charlieName)
-    await t.pressKey('ctrl+a')
-    await t.typeText(charlieName, 'Bob')
-    await t.pressKey('enter')
-    // Charlie not renamed
-    await t.expect(await friendsText(t)).eql(['Youyyyyn', 'BobnnnnyRemove', 'CharliePending Friend RequestRemove'])
-    // check that the text 'Friend name already exists' is displayed
-    await t.expect(Selector('div').withText('Friend name already exists').exists).ok()
-})
+testsGeneric()
