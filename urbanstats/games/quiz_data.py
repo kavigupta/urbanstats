6aa94dbf23e202d60b803b68347c26c22269b120
Kavi Gupta <kavi@berkeley.edu>
2024-12-25 17:05:34 -0800

    delete unused

--- urbanstats/games/quiz_data.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-import numpy as np
-import pandas as pd
-from urbanstats.games.quiz_question_metadata import QuizQuestionSkip
-from urbanstats.statistics.statistics_tree import statistics_tree
-
-from .quiz_columns import all_descriptors
