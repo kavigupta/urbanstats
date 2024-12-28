a1661043b054d03a171e050b9f87f78da10b5100
Luke Brody <luke@lukebrody.com>
2024-12-28 01:59:46 -0800

    Improve url encoding

--- urbanstats/website_data/sitemap.py
+++ urbanstats/website_data/sitemap.py
@@ -1,4 +1,4 @@
-from urllib.parse import quote_plus
+from urllib.parse import urlencode
 
 from urbanstats.ordinals.ordering_info_outputter import reorganize_counts
 from urbanstats.statistics.output_statistics_metadata import (
@@ -47,10 +47,11 @@ def top_level_pages():
 
 
 def article_urls(articles):
-    return [
-        f"https://urbanstats.org/article.html?longname={quote_plus(longname)}"
-        for longname in list(articles.longname)
-    ]
+    result = []
+    for longname in list(articles.longname):
+        params = {"longname": longname}
+        result.append(f"https://urbanstats.org/article.html?{urlencode(params)}")
+    return result
 
 
 def statistic_urls(ordinal_info):
@@ -62,7 +63,12 @@ def statistic_urls(ordinal_info):
         for article_type, stat_counts in article_types.items():
             for stat_index, stat_count in enumerate(stat_counts):
                 if stat_count > 0:
+                    params = {
+                        "statname": stat_names[stat_index],
+                        "article_type": article_type,
+                        "universe": universe,
+                    }
                     result.append(
-                        f"https://urbanstats.org/statistic.html?statname={quote_plus(stat_names[stat_index])}&article_type={quote_plus(article_type)}&universe={quote_plus(universe)}"
+                        f"https://urbanstats.org/statistic.html?{urlencode(params)}"
                     )
     return result
