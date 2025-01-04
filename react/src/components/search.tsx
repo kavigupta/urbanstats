dc569a32afaf85585ab803439d0d22b90845c3b3
Luke Brody <luke@lukebrody.com>
2025-01-04 00:26:55 -0800

    Bitap impl!!

--- react/src/components/search.tsx
+++ react/src/components/search.tsx
@@ -6,6 +6,7 @@ import { useColors } from '../page_template/colors'
 import { useSetting } from '../page_template/settings'
 import { isHistoricalCD } from '../utils/is_historical'
 import '../common.css'
+import { SearchIndex } from '../utils/protos'
 
 export function SearchBox(props: {
     onChange?: (inp: string) => void
@@ -26,9 +27,8 @@ export function SearchBox(props: {
     const [focused, setFocused] = React.useState(0)
 
     const searchQuery = normalizedQuery.current
-    const firstCharacter = searchQuery.length === 0 ? undefined : searchQuery[0]
 
-    const indexCache = useMemo(() => firstCharacter === undefined ? undefined : loadProtobuf(`/index/pages_${firstCharacter}.gz`, 'SearchIndex'), [firstCharacter])
+    const fullIndex = useMemo(() => loadProtobuf('/index/pages.gz', 'SearchIndex'), [])
 
     const reset = (): void => {
         setQuery('')
@@ -70,36 +70,29 @@ export function SearchBox(props: {
 
     // Do the search
     useEffect(() => {
-        if (indexCache === undefined) {
+        if (searchQuery === '') {
             // Occurs when query is empty
             setMatches([])
             setFocused(0)
             return
         }
-        void indexCache.then(({ elements, priorities }) => {
+
+        const doSearch = (index: SearchIndex): void => {
             // we can skip searching if the query has changed since we were waiting on the indexCache
             if (normalizedQuery.current !== searchQuery) {
                 return
             }
 
-            let matchesNew = []
-            for (let i = 0; i < elements.length; i++) {
-                const matchCount = isAMatch(searchQuery, normalize(elements[i]))
-                if (matchCount === 0) {
-                    continue
-                }
-                if (!showHistoricalCDs) {
-                    if (isHistoricalCD(elements[i])) {
-                        continue
-                    }
-                }
-                matchesNew.push([matchCount, i, matchCount - priorities[i] / 10])
-            }
-            matchesNew = top10(matchesNew)
-            matchesNew = matchesNew.map(idx => elements[idx])
-            setMatches(matchesNew)
-        })
-    }, [searchQuery, indexCache, showHistoricalCDs])
+            setMatches(bitap(index.elements, searchQuery))
+        }
+
+        void (async () => {
+            const full = await fullIndex
+            const s2 = Date.now()
+            doSearch(full)
+            console.log(`Took ${Date.now() - s2}ms to do full search`)
+        })()
+    }, [searchQuery, showHistoricalCDs, fullIndex])
 
     return (
         <form
@@ -170,59 +163,128 @@ export function SearchBox(props: {
     )
 }
 
-function top10(matches: number[][]): number[] {
-    const numPrioritized = 3
-    const sortKey = (idx: number) => {
-        return (a: number[], b: number[]) => {
-            if (a[idx] !== b[idx]) {
-                return b[idx] - a[idx]
-            }
-            return a[1] - b[1]
-        }
-    }
-    matches.sort(sortKey(2))
-    const overallMatches = []
-    for (let i = 0; i < Math.min(numPrioritized, matches.length); i++) {
-        overallMatches.push(matches[i][1])
-        matches[i][0] = -100
-    }
-    matches.sort(sortKey(0))
-    for (let i = 0; i < Math.min(10 - numPrioritized, matches.length); i++) {
-        if (matches[i][0] === -100) {
-            break
-        }
-        overallMatches.push(matches[i][1])
-    }
-    return overallMatches
+function normalize(a: string): string {
+    return a.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
 }
 
-/*
-    Check whether a is a substring of b (does not have to be contiguous)
+interface BitapMatch {
+    element: string
+    errors: number
+    indexInElement: number
+    whitespaceAfterPattern: boolean
+    distanceFromPatternLength: number
+}
 
-*/
-function isAMatch(a: string, b: string): number {
-    let i = 0
-    let matchCount = 0
-    let prevMatch = true
-    // eslint-disable-next-line @typescript-eslint/prefer-for-of -- b is a string
-    for (let j = 0; j < b.length; j++) {
-        if (a[i] === b[j]) {
-            i++
-            if (prevMatch) {
-                matchCount++
-            }
-            prevMatch = true
-        }
-        else {
-            prevMatch = false
-        }
-        if (i === a.length) {
-            return matchCount + 1
-        }
+function compareBitapMatches(a: BitapMatch, b: BitapMatch): number {
+    if (a.errors !== b.errors) {
+        return a.errors - b.errors
+    }
+    if (a.indexInElement !== b.indexInElement) {
+        return a.indexInElement - b.indexInElement
+    }
+    if (a.whitespaceAfterPattern !== b.whitespaceAfterPattern) {
+        // Note swapped order
+        return Number(b.whitespaceAfterPattern) - Number(a.whitespaceAfterPattern)
+    }
+    if (a.distanceFromPatternLength !== b.distanceFromPatternLength) {
+        return a.distanceFromPatternLength - b.distanceFromPatternLength
     }
     return 0
 }
 
-function normalize(a: string): string {
-    return a.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+function bitap(elements: string[], pattern: string): string[] {
+    if (pattern.length > 53) {
+        throw new Error('pattern too large')
+    }
+
+    const maxErrors = Math.min(2, pattern.length)
+
+    const alphabet = new Array<number>(65535).fill(0)
+    for (let i = 0; i < pattern.length; i++) {
+        const char = pattern.charCodeAt(i)
+        alphabet[char] = alphabet[char] | (1 << (pattern.length - i - 1))
+    }
+
+    const matchMask = 1 << (pattern.length - 1)
+
+    const matches: BitapMatch[] = []
+    const numMatches = 10
+    function addToMatches(newMatch: BitapMatch): void {
+        const indexOfExistingMatchOnElement = matches.findIndex(match => match.element === newMatch.element)
+
+        // if (pattern.toLocaleLowerCase().startsWith('santa') && newMatch.element.toLowerCase().startsWith('santa') && matches.some(match => match.element.toLowerCase().startsWith('santa'))) {
+        //     console.log({ newMatch, matches })
+        // }
+
+        if (indexOfExistingMatchOnElement !== -1) {
+            if (compareBitapMatches(newMatch, matches[indexOfExistingMatchOnElement]) < 0) {
+                matches.splice(indexOfExistingMatchOnElement, 1)
+            }
+            else {
+                return
+            }
+        }
+
+        for (let i = 0; i < numMatches; i++) {
+            if (i >= matches.length || compareBitapMatches(newMatch, matches[i]) < 0) {
+                matches.splice(i, 0, newMatch)
+                break
+            }
+        }
+
+        if (matches.length > numMatches) {
+            matches.pop()
+        }
+    }
+
+    const lengthOfLongestElement = elements.reduce((length, element) => Math.max(length, element.length), 0)
+
+    const longestFinish = lengthOfLongestElement + pattern.length
+
+    let rd = new Array<number>(longestFinish + 2)
+    let lastRd = new Array<number>(longestFinish + 2)
+
+    for (const element of elements) {
+        const normalizedElement = normalize(element)
+        const finish = normalizedElement.length + pattern.length
+
+        for (let errors = 0; errors <= maxErrors; errors++) {
+            rd.fill(0)
+            rd[finish + 1] = (1 << errors) - 1
+
+            for (let j = finish; j >= 1; j--) {
+                let charMatch: number
+                if (j - 1 < normalizedElement.length) {
+                    charMatch = alphabet[normalizedElement.charCodeAt(j - 1)]
+                }
+                else {
+                    charMatch = 0
+                }
+                if (errors === 0) {
+                    rd[j] = ((rd[j + 1] << 1) | 1) & charMatch
+                }
+                else {
+                    // Subsequent passes: fuzzy match.
+                    rd[j] = (((rd[j + 1] << 1) | 1) & charMatch) | (((lastRd[j + 1] | lastRd[j]) << 1) | 1) | lastRd[j + 1]
+                }
+                if ((rd[j] & matchMask) !== 0) {
+                    const indexInElement = j - 1
+                    const indexAfterMatch = indexInElement + pattern.length
+                    addToMatches({
+                        element,
+                        errors,
+                        indexInElement,
+                        whitespaceAfterPattern: indexAfterMatch >= normalizedElement.length || normalizedElement.charAt(indexAfterMatch) === ' ',
+                        distanceFromPatternLength: Math.abs(pattern.length - normalizedElement.length),
+                    })
+                }
+            }
+
+            [lastRd, rd] = [rd, lastRd]
+        }
+    }
+
+    console.log(matches)
+
+    return matches.map(match => match.element)
 }
