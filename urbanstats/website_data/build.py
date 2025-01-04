aa2715c30c265a5e30d263dc6c99e18faffc40e4
Kavi Gupta <kavi@berkeley.edu>
2025-01-04 10:43:17 -0800

    lint

--- urbanstats/website_data/build.py
+++ urbanstats/website_data/build.py
@@ -147,7 +147,7 @@ def build_react_site(site_folder, mode):
     link_scripts_folder(site_folder, mode)
 
 
-# pylint: disable-next=too-many-branches
+# pylint: disable-next=too-many-branches,too-many-arguments
 def build_urbanstats(
     site_folder,
     *,
