f4e3b3b2610049c9e2612f642030fb736b7f1b57
Luke Brody <luke@lukebrody.com>
2025-01-05 12:27:57 -0800

    Add memory tests

---
+++ react/test/memory_test.ts
@@ -0,0 +1,23 @@
+import { urbanstatsFixture } from './test_utils'
+
+urbanstatsFixture('home page', '/')
+
+async function getJSHeapSize(t: TestController): Promise<number> {
+    await t.wait(1000) // Wait for page to load
+    const cdpSession = await t.getCurrentCDPSession()
+    await cdpSession.HeapProfiler.collectGarbage()
+    await t.wait(1000) // Wait for garbage collect
+    const bytesUsed = await t.eval(() => (performance as unknown as { memory: { usedJSHeapSize: number } }).memory.usedJSHeapSize) as number
+    console.warn(`Bytes used: ${bytesUsed}`)
+    return bytesUsed
+}
+
+test('under memory limit home page', async (t) => {
+    await t.expect(await getJSHeapSize(t)).lt(35_000_000)
+})
+
+urbanstatsFixture('california', '/article.html?longname=California%2C+USA&s=GczH23sVhzZkQid')
+
+test('under memory limit california', async (t) => {
+    await t.expect(await getJSHeapSize(t)).lt(57_000_000)
+})
