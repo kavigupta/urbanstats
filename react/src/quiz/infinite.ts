14edaa5b3a305d1eeb94149e0baf945786f9566f
Kavi Gupta <kavi@berkeley.edu>
2025-01-05 00:44:31 -0500

    compute

--- react/src/quiz/infinite.ts
+++ react/src/quiz/infinite.ts
@@ -1,14 +1,18 @@
 import seedrandom from 'seedrandom'
 
 import quiz_infinite from '../data/quiz_infinite'
-
-import { QuizQuestion } from './quiz'
-import { loadProtobuf } from '../load_json'
-import statistic_list from '../data/statistic_list'
+import quiz_names from '../data/quiz_names'
 import statistic_name_list from '../data/statistic_name_list'
 import statistic_path_list from '../data/statistic_path_list'
+import { loadProtobuf } from '../load_json'
+import { QuizFullData } from '../utils/protos'
 
-type QuizQuestionChunks = readonly {path: string, totalP: number}[]
+import { QuizQuestion } from './quiz'
+
+const initialLives = 3
+const correctForBonus = 5
+
+type QuizQuestionChunks = readonly { path: string, totalP: number }[]
 
 function sampleChunk(rng: seedrandom.PRNG, chunks: QuizQuestionChunks): string {
     const probeP = rng()
@@ -22,7 +26,7 @@ function sampleChunk(rng: seedrandom.PRNG, chunks: QuizQuestionChunks): string {
     throw new Error('Invalid chunk distribution')
 }
 
-function sampleTroncheIndex(rng: seedrandom.PRNG, negLogProbX100: number[]) {
+function sampleTroncheIndex(rng: seedrandom.PRNG, negLogProbX100: number[]): number {
     const probeP = rng()
     let totalP = 0
     for (let i = 0; i < negLogProbX100.length; i++) {
@@ -34,6 +38,15 @@ function sampleTroncheIndex(rng: seedrandom.PRNG, negLogProbX100: number[]) {
     throw new Error('Invalid tronche distribution')
 }
 
+let quizSamplingData: Promise<QuizFullData> | null = null
+function loadQuizSamplingData(): Promise<QuizFullData> {
+    // cache the data
+    if (quizSamplingData === null) {
+        quizSamplingData = loadProtobuf('/quiz_sampling_info/data.gz', 'QuizFullData')
+    }
+    return quizSamplingData
+}
+
 export async function sampleRandomQuestion(seed: string, index: number): Promise<QuizQuestion> {
     const seedFull = `${seed}-${index}`
     const rng = seedrandom(seedFull)
@@ -41,31 +54,53 @@ export async function sampleRandomQuestion(seed: string, index: number): Promise
     const chunks = quiz_infinite.questionDistribution[whichQ]
     const chunk = sampleChunk(rng, chunks satisfies QuizQuestionChunks)
     const tronche = await loadProtobuf(chunk, 'QuizQuestionTronche')
+    const data = await loadQuizSamplingData()
     const troncheIdx = sampleTroncheIndex(rng, tronche.negLogProbX100)
     let geoA = tronche.geographyA[troncheIdx]
     let geoB = tronche.geographyB[troncheIdx]
     if (rng() < 0.5) {
         [geoA, geoB] = [geoB, geoA]
     }
-    const stat = quiz_infinite.allStats[tronche.stat[troncheIdx]]
-    const statName = statistic_name_list[statistic_list.indexOf(stat)]
-    const statPath = statistic_path_list[statistic_list.indexOf(stat)]
-    console.log(chunk)
-    console.log(tronche)
+    const stat = tronche.stat[troncheIdx]
+    const statName = statistic_name_list[quiz_infinite.allStats[stat]]
+    const statPath = statistic_path_list[quiz_infinite.allStats[stat]]
+    const question: string = quiz_names[quiz_infinite.allStats[stat]]
+    const sA = data.stats[stat].stats![geoA]
+    const sB = data.stats[stat].stats![geoB]
     return {
-        stat_a: 0,
-        stat_b: 1,
-        question: 'Which region is better?',
+        stat_a: sA,
+        stat_b: sB,
+        question,
         longname_a: quiz_infinite.allGeographies[geoA],
         longname_b: quiz_infinite.allGeographies[geoB],
         kind: 'juxtastat',
         stat_column: statName,
         stat_path: statPath,
     } satisfies QuizQuestion
-    // throw new Error('Not implemented')
 }
 
 export function infiniteQuizIsDone(correctPattern: boolean[]): boolean {
-    // TODO fix
-    return false
+    return numLives(correctPattern) === 0
+}
+
+export function numLives(correctPattern: boolean[]): number {
+    let lives = initialLives
+    let correctRun = 0
+    for (const i of correctPattern.keys()) {
+        if (correctPattern[i]) {
+            correctRun++
+            if (correctRun >= correctForBonus) {
+                lives++
+                correctRun = 0
+            }
+        }
+        else {
+            lives--
+            correctRun = 0
+        }
+        if (lives <= 0) {
+            return 0
+        }
+    }
+    return lives
 }
