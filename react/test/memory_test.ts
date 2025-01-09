2b9628870c11174989f5e3250727bf41b0f58933
Luke Brody <luke@lukebrody.com>
2025-01-08 23:21:21 -0800

    just memory test

--- react/test/memory_test.ts
+++ react/test/memory_test.ts
@@ -10,14 +10,34 @@ async function getJSHeapSize(t: TestController): Promise<number> {
     return bytesUsed
 }
 
+const homePageThreshold = 35_000_000
+const compressedSearchIndexSize = 3_000_000
+const californiaArticleThreshold = 66_000_000
+
 urbanstatsFixture('home page', '/')
 
 test('under memory limit home page', async (t) => {
-    await t.expect(await getJSHeapSize(t)).lt(35_000_000)
+    await t.expect(await getJSHeapSize(t)).lt(homePageThreshold)
+})
+
+test('click away from search box and return to memory limit', async (t) => {
+    await t.expect(await getJSHeapSize(t)).lt(homePageThreshold)
+    await t.click('#searchbox')
+    await t.expect(await getJSHeapSize(t)).gt(homePageThreshold)
+    await t.click('body')
+    await t.expect(await getJSHeapSize(t)).lt(homePageThreshold + compressedSearchIndexSize)
+})
+
+test('go to article and return to under memory limit', async (t) => {
+    await t.expect(await getJSHeapSize(t)).lt(homePageThreshold)
+    await t.click('#searchbox')
+    await t.expect(await getJSHeapSize(t)).gt(homePageThreshold)
+    await t.typeText('#searchbox', 'California').pressKey('enter')
+    await t.expect(await getJSHeapSize(t)).lt(californiaArticleThreshold)
 })
 
 urbanstatsFixture('california', '/article.html?longname=California%2C+USA&s=GczH23sVhzZkQid')
 
 test('under memory limit california', async (t) => {
-    await t.expect(await getJSHeapSize(t)).lt(66_000_000)
+    await t.expect(await getJSHeapSize(t)).lt(californiaArticleThreshold)
 })
