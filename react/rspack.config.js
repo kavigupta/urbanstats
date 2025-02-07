fa19b12b9ea3432994dc6fdb82a021069faae9cd
Kavi Gupta <kavi@berkeley.edu>
2025-02-06 21:55:17 -0500

    fix

--- react/rspack.config.js
+++ react/rspack.config.js
@@ -52,6 +52,6 @@ export default env => ({
     performance: {
         hints: isProduction ? 'error': false,
         maxAssetSize: Number.MAX_SAFE_INTEGER,
-        maxEntrypointSize: 3.5 * Math.pow(2, 20)
+        maxEntrypointSize: 2.4 * Math.pow(2, 20)
     }
 })
