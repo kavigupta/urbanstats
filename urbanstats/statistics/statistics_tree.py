0ce7bf9b74ee8e40b4b4e6ab9a5fb23b6877960f
Kavi Gupta <kavi@berkeley.edu>
2025-02-09 20:18:23 -0500

    rename Climate Change group to Environment (#935)
    We're going to put non-climate change related pollution stuff in this
    category, so the title should be broadened

--- urbanstats/statistics/statistics_tree.py
+++ urbanstats/statistics/statistics_tree.py
@@ -504,7 +504,7 @@ statistics_tree = StatisticTree(
         ),
         **just_2020_category(
             "climate_change",
-            "Climate Change",
+            "Environmment",
             "heating_utility_gas",
             "heating_electricity",
             "heating_bottled_tank_lp_gas",
