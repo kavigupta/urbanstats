e60511d4e0af1960812fe5c716a1cb29e5448db3
Luke Brody <luke@lukebrody.com>
2025-02-11 18:46:03 -0800

    Random unit test

---
+++ react/unit/util/localStorage.ts
@@ -0,0 +1,9 @@
+global.localStorage = {
+    get: () => null,
+    length: 0,
+    clear: () => undefined,
+    getItem: () => null,
+    removeItem: () => undefined,
+    key: () => null,
+    setItem: () => undefined,
+}
