16b28e2866ccc504079d3283656d610f75c78d35
Kavi Gupta <kavi@berkeley.edu>
2025-02-14 02:11:21 -0500

    fix unit random tests (#955)
    Adds: by population test (domestic and full)
    
    Fix unit testing to not assume URL encoding

--- react/src/navigation/random.ts
+++ react/src/navigation/random.ts
@@ -2,39 +2,41 @@ import { loadJSON, loadProtobuf } from '../load_json'
 import { Settings } from '../page_template/settings'
 import { isHistoricalCD } from '../utils/is_historical'
 
-export async function byPopulation(domesticOnly: boolean): Promise<string> {
+export async function byPopulation(domesticOnly: boolean): Promise<() => string> {
     const values = (await loadProtobuf('/index/pages_all.gz', 'SearchIndex')).elements
     const populations = await loadJSON('/index/best_population_estimate.json') as number[]
     const totalWeight = populations.reduce((sum, x) => sum + x)
 
-    while (true) {
+    return () => {
+        while (true) {
         // Generate a random number between 0 and the total weight
-        const randomValue = Math.random() * totalWeight
+            const randomValue = Math.random() * totalWeight
 
-        // Find the destination based on the random value
-        let x: string
-        let cumulativeWeight = 0
+            // Find the destination based on the random value
+            let x: string
+            let cumulativeWeight = 0
 
-        for (let i = 0; i < values.length; i++) {
-            cumulativeWeight += populations[i]
+            for (let i = 0; i < values.length; i++) {
+                cumulativeWeight += populations[i]
 
-            if (randomValue < cumulativeWeight) {
-                x = values[i]
-                break
+                if (randomValue < cumulativeWeight) {
+                    x = values[i]
+                    break
+                }
             }
-        }
 
-        if (!valid(x!)) {
-            continue
-        }
+            if (!valid(x!)) {
+                continue
+            }
 
-        // this is specifically looking for stuff that's only in the US.
-        // so it makes sense.
-        if (domesticOnly && (!(x!.endsWith(', USA') || (x!) === 'USA'))) {
-            continue
-        }
+            // this is specifically looking for stuff that's only in the US.
+            // so it makes sense.
+            if (domesticOnly && (!(x!.endsWith(', USA') || (x!) === 'USA'))) {
+                continue
+            }
 
-        return x!
+            return x!
+        }
     }
 }
 
--- react/unit/random.test.ts
+++ react/unit/random.test.ts
@@ -3,11 +3,11 @@ import * as assert from 'uvu/assert'
 
 import './util/fetch'
 import './util/localStorage'
-import { uniform } from '../src/navigation/random'
+import { byPopulation, uniform } from '../src/navigation/random'
 
 function assertNoSpecials(article: string): void {
-    assert.not.match(article, /.*Historical\+Congressional.*/)
-    assert.not.match(article, /.*PC%2C.*/)
+    assert.not.match(article, /.*Historical Congressional.*/)
+    assert.not.match(article, /.*PC.*/)
 }
 
 const repeats = 100000
@@ -16,6 +16,21 @@ test('uniform', async () => {
     const getArticle = await uniform()
     for (let count = 0; count < repeats; count++) {
         assertNoSpecials(getArticle())
+    }
+})
+
+test('by-pop', async () => {
+    const getArticle = await byPopulation(false)
+    for (let count = 0; count < repeats; count++) {
+        assertNoSpecials(getArticle())
+    }
+})
+
+test('by-pop-usa-only', async () => {
+    const getArticle = await byPopulation(true)
+    for (let count = 0; count < repeats; count++) {
+        assertNoSpecials(getArticle())
+        assert.match(getArticle(), /.*USA.*/)
     }
 })
 
