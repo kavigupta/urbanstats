f91d13c0c4f42a40faf51989391d19b5d5e55798
Kavi Gupta <kavi@berkeley.edu>
2025-02-13 23:19:40 -0800

    delete metadata

--- react/src/navigation/random.ts
+++ react/src/navigation/random.ts
@@ -1,9 +1,10 @@
 import { loadJSON, loadProtobuf } from '../load_json'
 import { Settings } from '../page_template/settings'
 import { isHistoricalCD } from '../utils/is_historical'
+import { SearchIndex } from '../utils/protos'
 
 export async function byPopulation(domesticOnly: boolean): Promise<() => string> {
-    const values = (await loadProtobuf('/index/pages_all.gz', 'SearchIndex')).elements
+    const index = (await loadProtobuf('/index/pages_all.gz', 'SearchIndex'))
     const populations = await loadJSON('/index/best_population_estimate.json') as number[]
     const totalWeight = populations.reduce((sum, x) => sum + x)
 
@@ -13,48 +14,54 @@ export async function byPopulation(domesticOnly: boolean): Promise<() => string>
             const randomValue = Math.random() * totalWeight
 
             // Find the destination based on the random value
-            let x: string
+            let idx: number
             let cumulativeWeight = 0
 
-            for (let i = 0; i < values.length; i++) {
+            for (let i = 0; i < index.elements.length; i++) {
                 cumulativeWeight += populations[i]
 
                 if (randomValue < cumulativeWeight) {
-                    x = values[i]
+                    idx = i
                     break
                 }
             }
 
-            if (!valid(x!)) {
+            if (!valid(index, idx!)) {
                 continue
             }
+
+            const x = index.elements[idx!]
 
             // this is specifically looking for stuff that's only in the US.
             // so it makes sense.
-            if (domesticOnly && (!(x!.endsWith(', USA') || (x!) === 'USA'))) {
+            if (domesticOnly && (!(x.endsWith(', USA') || (x) === 'USA'))) {
                 continue
             }
 
-            return x!
-        }
-    }
-}
-
-export async function uniform(): Promise<() => string> {
-    const values = (await loadProtobuf('/index/pages_all.gz', 'SearchIndex')).elements
-    return () => {
-        while (true) {
-            const randomIndex = Math.floor(Math.random() * values.length)
-            const x = values[randomIndex]
-            if (!valid(x)) {
-                continue
-            }
             return x
         }
     }
 }
 
-function valid(x: string): boolean {
+export async function uniform(): Promise<() => string> {
+    const index = (await loadProtobuf('/index/pages_all.gz', 'SearchIndex'))
+    return () => {
+        while (true) {
+            const randomIndex = Math.floor(Math.random() * index.elements.length)
+            if (!valid(index, randomIndex)) {
+                continue
+            }
+            return index.elements[randomIndex]
+        }
+    }
+}
+
+function valid(index: SearchIndex, idx: number): boolean {
+    const x = index.elements[idx]
+    const metadata = index.metadata[idx]
+    if (metadata.isSymlink) {
+        return false
+    }
     if (!Settings.shared.get('show_historical_cds') && isHistoricalCD(x)) {
         return false
     }
