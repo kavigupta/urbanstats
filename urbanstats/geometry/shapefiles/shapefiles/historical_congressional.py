5a71b1a47e80d3dc0e13fb57fdf9ad84c622d516
Kavi Gupta <kavi@berkeley.edu>
2025-02-14 19:32:26 -0800

    historical congressional rename

--- urbanstats/geometry/shapefiles/shapefiles/historical_congressional.py
+++ urbanstats/geometry/shapefiles/shapefiles/historical_congressional.py
@@ -1,14 +1,23 @@
 from urbanstats.geometry.shapefiles.shapefile import Shapefile
 from urbanstats.geometry.shapefiles.shapefile_subset import SelfSubset
-from urbanstats.geometry.shapefiles.shapefiles.judicial import render_start_and_end
 from urbanstats.universe.universe_provider.constants import us_domestic_provider
+
+
+def to_year(congress_number):
+    congress_number_zero_indexed = congress_number - 1
+    year_congress_start = 1789  # 1st Congress started in 1789
+    return congress_number_zero_indexed * 2 + year_congress_start
+
+
+def historical_shortname(x):
+    return f'{x["state"]}-{int(x["district"]):02d} ({to_year(x.start)})'
+
 
 HISTORICAL_CONGRESSIONAL = Shapefile(
     hash_key="historical_congressional_5",
     path="named_region_shapefiles/congressional_districts/combo/historical.pkl",
-    shortname_extractor=lambda x: f'{x["state"]}-{int(x["district"]):02d} [{render_start_and_end(x)} Congress]',
-    longname_extractor=lambda x: "Historical Congressional District"
-    + f" {x['state']}-{x['district']}, {render_start_and_end(x)} Congress, USA",
+    shortname_extractor=historical_shortname,
+    longname_extractor=lambda x: historical_shortname(x) + ", USA",
     filter=lambda x: True,
     meta=dict(
         type="Historical Congressional District",
