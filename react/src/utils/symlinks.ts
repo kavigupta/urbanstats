2b137b6a66c3c6bc780559b67e047d2140a23f0d
Kavi Gupta <kavi@berkeley.edu>
2025-02-14 19:51:38 -0800

    fix

--- react/src/utils/symlinks.ts
+++ react/src/utils/symlinks.ts
@@ -11,14 +11,12 @@ function followSymlink(name: string): string {
     return name
 }
 
-function followSymlinks(names: string[]): string[] {
-    return names.map(name => followSymlink(name))
-}
-
 export function loadArticleFromPossibleSymlink(longname: string): Promise<Article> {
+    longname = followSymlink(longname)
     return loadProtobuf(dataLink(longname), 'Article')
 }
 
-export function loadArticlesFromPossibleSymlink(names: string[]): Promise<Article[]> {
-    return Promise.all(names.map(loadArticleFromPossibleSymlink))
+export function loadArticlesFromPossibleSymlink(longnames: string[]): Promise<Article[]> {
+    longnames = longnames.map(followSymlink)
+    return Promise.all(longnames.map(loadArticleFromPossibleSymlink))
 }
