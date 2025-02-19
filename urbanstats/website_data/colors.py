d4253f671dba138c9c5a7b83644353b6ddb35812
Kavi Gupta <kavi@berkeley.edu>
2025-02-18 20:13:02 -0500

    add file

---
+++ urbanstats/website_data/colors.py
@@ -0,0 +1,25 @@
+hue_colors = {
+    "blue": "#5a7dc3",
+    "orange": "#f7aa41",
+    "darkOrange": "#af6707",
+    "purple": "#975ac3",
+    "red": "#f96d6d",
+    "grey": "#8e8e8e",
+    "darkGrey": "#4e525a",
+    "pink": "#c767b0",
+    "yellow": "#b8a32f",
+    "green": "#8ac35a",
+    "cyan": "#07a5af",
+}
+
+related_button_colors = {
+    "International": "red",
+    "US Subdivision": "blue",
+    "Census": "cyan",
+    "Political": "purple",
+    "Oddball": "darkGrey",
+    "Kavi": "darkOrange",
+    "School": "yellow",
+    "Small": "pink",
+    "Native": "green",
+}
