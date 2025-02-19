6de2831a08926f52f91381b0920513e6469c4ab9
Kavi Gupta <kavi@berkeley.edu>
2025-02-18 20:10:00 -0500

    hue colors

---
+++ react/src/data/hueColors.ts
@@ -0,0 +1,13 @@
+export default {
+    blue: '#5a7dc3',
+    orange: '#f7aa41',
+    darkOrange: '#af6707',
+    purple: '#975ac3',
+    red: '#f96d6d',
+    grey: '#8e8e8e',
+    darkGrey: '#4e525a',
+    pink: '#c767b0',
+    yellow: '#b8a32f',
+    green: '#8ac35a',
+    cyan: '#07a5af',
+} as const
--- react/src/page_template/color-themes.ts
+++ react/src/page_template/color-themes.ts
@@ -1,3 +1,5 @@
+import hueColors from '../data/hueColors'
+
 export interface HueColors {
     blue: string
     orange: string
@@ -44,19 +46,7 @@ export interface JuxtastatColors {
     lifeLostEmoji: string
 }
 
-const defaultHueColors: HueColors = {
-    blue: '#5a7dc3',
-    orange: '#f7aa41',
-    darkOrange: '#af6707',
-    purple: '#975ac3',
-    red: '#f96d6d',
-    grey: '#8e8e8e',
-    darkGrey: '#4e525a',
-    pink: '#c767b0',
-    yellow: '#b8a32f',
-    green: '#8ac35a',
-    cyan: '#07a5af',
-}
+const defaultHueColors: HueColors = hueColors
 
 export type Theme = 'Light Mode' | 'Dark Mode'
 
