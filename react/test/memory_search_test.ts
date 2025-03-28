b0e51275243edf6e76e3088a293adf5af510b7f7
Luke Brody <luke@lukebrody.com>
2025-03-27 22:24:09 -0700

    Fix memory tests

--- react/test/memory_search_test.ts
+++ react/test/memory_search_test.ts
@@ -1,14 +1,13 @@
-import { memoryMonitor, homePageThreshold, searchSize } from './memory_test_utils'
+import { homePageSize, searchSize, memoryUsage } from './memory_test_utils'
 import { urbanstatsFixture } from './test_utils'
 
 urbanstatsFixture('home page', '/')
 
 test('click away from search box and return to memory limit', async (t) => {
-    const memory = await memoryMonitor(t)
-    await t.expect(await memory()).lt(homePageThreshold)
+    await t.expect(await memoryUsage(t)).lt(homePageSize)
     await t.click('#searchbox')
-    await t.expect(await memory()).gt(homePageThreshold)
-    await t.expect(await memory()).lt(homePageThreshold + searchSize)
+    await t.expect(await memoryUsage(t)).gt(homePageSize)
+    await t.expect(await memoryUsage(t)).lt(homePageSize + searchSize)
     await t.click('body')
-    await t.expect(await memory()).lt(homePageThreshold)
+    await t.expect(await memoryUsage(t)).lt(homePageSize)
 })
