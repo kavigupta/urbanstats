d3d57d0bc0fe4e8ab0cdff1c8a24bdba56e5d222
Kavi Gupta <kavi@berkeley.edu>
2025-03-29 13:53:49 -0700

    Revert "temporarily disable attribution so we don't have a bunch of image changes. will be fixeed in followup"
    This reverts commit ff581c05068fa50b43685863dd3fe7c650d8573c.

--- react/src/components/map.tsx
+++ react/src/components/map.tsx
@@ -111,7 +111,6 @@ export class MapGeneric<P extends MapGenericProps> extends React.Component<P, Ma
             style: 'https://tiles.openfreemap.org/styles/bright',
             container: this.id,
             scrollZoom: true,
-            attributionControl: false,
             dragRotate: false,
         })
         this.map = map
