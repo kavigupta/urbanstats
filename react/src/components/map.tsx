84879538b92f55e84078d7c26be41a6f551eb2da
Luke Brody <luke@lukebrody.com>
2025-05-09 02:11:56 -0700

    Change map pixel ratio

--- react/src/components/map.tsx
+++ react/src/components/map.tsx
@@ -119,6 +119,7 @@ export class MapGeneric<P extends MapGenericProps> extends React.Component<P, Ma
             canvasContextAttributes: {
                 preserveDrawingBuffer: true,
             },
+            pixelRatio: 0.5,
         })
         this.map = map
         this.ensureStyleLoaded = new Promise(resolve => map.on('style.load', resolve))
