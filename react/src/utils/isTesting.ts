4ea3d87f5abbbec93cddbd2ced89a9dc10c66160
Luke Brody <luke@lukebrody.com>
2025-05-09 06:19:46 -0700

    Left in changes

--- react/src/utils/isTesting.ts
+++ react/src/utils/isTesting.ts
@@ -4,7 +4,7 @@
  * Use sparingly! Functionality under testing should diverge minimally.
  */
 export function isTesting(): boolean {
-    return navigator.userAgent !== testingUserAgent
+    return navigator.userAgent === testingUserAgent
 }
 
 export const testingUserAgent = 'urbanstats-e2e'
