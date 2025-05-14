0be2c8ca790f33d5e447a3a8aec5071772d8b181
Luke Brody <luke@lukebrody.com>
2025-05-14 01:05:23 -0700

    Fix watch

--- react/watch.sh
+++ react/watch.sh
@@ -7,7 +7,7 @@ python create_website.py $1 --mode=dev --no-data --no-geo --no-juxta
 cd react
 
 while true; do
-    rspack serve --mode=production --watch --env directory=$1
+    rspack serve --mode=development --watch --env directory=$1
     echo 'Restarting watcher... Press ^C again to stop watching.'
     sleep 1
 done
