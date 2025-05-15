33d42f852d65229d5c2c3482dcdf487737516cdc
Luke Brody <luke@lukebrody.com>
2025-05-15 01:56:27 -0700

    bump version

--- react/src/page_template/template.tsx
+++ react/src/page_template/template.tsx
@@ -138,7 +138,7 @@ function TemplateFooter(): ReactNode {
 }
 
 function Version(): ReactNode {
-    return <span id="current-version">23.13.0</span>
+    return <span id="current-version">23.13.1</span>
 }
 
 function LastUpdated(): ReactNode {
