290192cda43dc4e71dfb7b18427d3829a4c2cda0
Luke Brody <luke@lukebrody.com>
2025-05-17 09:53:22 +0000

    delete repos

--- react/ci_proxy/main.ts
+++ react/ci_proxy/main.ts
@@ -1,30 +1,40 @@
-import { existsSync } from 'fs'
+import { readdir, rm } from 'fs/promises'
 import { join } from 'path'
 
 import { execa } from 'execa'
 import express from 'express'
+import { existsSync } from "fs"
 
 // Git repo must be manually initialized
 // Use command `git clone --mirror https://github.com/densitydb/densitydb.github.io.git densitydb/densitydb.github.io`
 
 const bareRepo = join(import.meta.dirname, 'densitydb', 'densitydb.github.io')
 
-function repoPath(branch: string): string {
-    return join(import.meta.dirname, 'densitydb', 'repos', branch)
+function repoPath(branch?: string): string {
+    return join(import.meta.dirname, 'densitydb', 'repos', ...(branch === undefined ? [] : [branch]))
 }
 
 async function update(): Promise<void> {
-    await execa(`git`, ['remote', 'update', '--prune'], { cwd: bareRepo, stdio: 'inherit', reject: false })
-    const { stdout: branchesOutput } = await execa('git', ['branch'], { cwd: bareRepo })
-    const branches = Array.from(branchesOutput.matchAll(/^\*? {1,2}(.+)$/mg)).map(([, branch]) => branch)
-    for (const branch of branches) {
-        if (existsSync(repoPath(branch))) {
-            await execa('git', ['fetch'], { stdio: 'inherit', cwd: repoPath(branch) })
-            await execa('git', ['reset', '--hard', `origin/${branch}`], { stdio: 'inherit', cwd: repoPath(branch) })
+    try {
+        await execa(`git`, ['remote', 'update', '--prune'], { cwd: bareRepo, stdio: 'inherit', reject: false })
+        const { stdout: branchesOutput } = await execa('git', ['branch'], { cwd: bareRepo })
+        const branches = Array.from(branchesOutput.matchAll(/^\*? {1,2}(.+)$/mg)).map(([, branch]) => branch)
+        for (const branch of branches) {
+            if (existsSync(repoPath(branch))) {
+                await execa('git', ['fetch'], { stdio: 'inherit', cwd: repoPath(branch) })
+                await execa('git', ['reset', '--hard', `origin/${branch}`], { stdio: 'inherit', cwd: repoPath(branch) })
+            }
+            else {
+                await execa('git', ['clone', '--branch', branch, bareRepo, repoPath(branch)], { stdio: 'inherit' })
+            }
         }
-        else {
-            await execa('git', ['clone', '--branch', branch, bareRepo, repoPath(branch)], { stdio: 'inherit' })
+        for (const branchFolder of await readdir(repoPath())) {
+            if (!branches.includes(branchFolder)) {
+                await rm(repoPath(branchFolder), { recursive: true, force: true })
+            }
         }
+    } catch (e) {
+        console.error(e)
     }
     setTimeout(update, 60 * 1000)
 }
