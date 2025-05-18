ec45d4047421ceca2027fc306f9f1a9f614a75e0
Luke Brody <luke@lukebrody.com>
2025-05-17 18:26:49 -0700

    Use python instead

--- react/ci_proxy/install.sh
+++ react/ci_proxy/install.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
+set -e
 
 apt update
-apt install -y unzip nginx
-
-curl -o- https://fnm.vercel.app/install | bash
-# fnm
-FNM_PATH="/root/.local/share/fnm"
-if [ -d "$FNM_PATH" ]; then
-  export PATH="$FNM_PATH:$PATH"
-  eval "`fnm env --shell bash`"
-fi
-fnm install 22
-
-cd /root/urbanstats/react
-npm ci
+apt install -y nginx
 
 git clone --mirror https://github.com/densitydb/densitydb.github.io.git /root/urbanstats/react/ci_proxy/densitydb/densitydb.github.io
 
