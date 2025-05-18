4bafc0e3a93388365c54e78cec5183938c964ced
Luke Brody <luke@lukebrody.com>
2025-05-17 18:44:18 -0700

    change order

--- ci_proxy/install.sh
+++ ci_proxy/install.sh
@@ -4,8 +4,6 @@ set -e
 apt update
 apt install -y nginx
 
-git clone --mirror https://github.com/densitydb/densitydb.github.io.git /root/urbanstats/ci_proxy/densitydb/densitydb.github.io
-
 cp /root/urbanstats/ci_proxy/default /etc/nginx/sites-available/default
 sed -i -e 's/user www-data/user root/g' /etc/nginx/nginx.conf
 service nginx restart
@@ -14,3 +12,5 @@ cp /root/urbanstats/ci_proxy/urbanstats-ci-proxy.service /etc/systemd/system
 systemctl daemon-reload
 systemctl start urbanstats-ci-proxy.service
 systemctl enable urbanstats-ci-proxy.service
+
+git clone --mirror https://github.com/densitydb/densitydb.github.io.git /root/urbanstats/ci_proxy/densitydb/densitydb.github.io
