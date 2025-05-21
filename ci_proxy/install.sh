5411f8cccd2b7d25f2c5ca0e105d7028f7927ca2
Luke Brody <luke@lukebrody.com>
2025-05-20 23:01:25 -0700

    Improve CI proxy performance (#1103)

--- ci_proxy/install.sh
+++ ci_proxy/install.sh
@@ -6,10 +6,14 @@ apt install -y nginx
 
 cp /root/urbanstats/ci_proxy/default /etc/nginx/sites-available/default
 sed -i -e 's/user www-data/user root/g' /etc/nginx/nginx.conf
-service nginx restart
+sed -z -i -e 's/\[Service\]\n/\[Service\]\nNice=-10\n/g' /usr/lib/systemd/system/nginx.service
 
 cp /root/urbanstats/ci_proxy/urbanstats-ci-proxy.service /etc/systemd/system
+
 systemctl daemon-reload
+
+service nginx restart
+
 systemctl start urbanstats-ci-proxy.service
 systemctl enable urbanstats-ci-proxy.service
 
