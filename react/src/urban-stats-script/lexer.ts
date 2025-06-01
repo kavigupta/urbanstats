439f90d8c4c23c3d0dd2ca87cb1ac2c32366e889
Kavi Gupta <kavi@berkeley.edu>
2025-06-01 12:28:28 -0400

    remove lint warning

--- react/src/urban-stats-script/lexer.ts
+++ react/src/urban-stats-script/lexer.ts
@@ -324,7 +324,6 @@ function lexString(input: string, idx: number, lineNo: number): [number, Annotat
     }
     let result: string
     try {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- we check on the next line
         const resultObj: unknown = JSON.parse(input.slice(start, idx))
         if (typeof resultObj !== 'string') {
             throw new Error(`Invalid string: ${input.slice(start, idx)}`)
