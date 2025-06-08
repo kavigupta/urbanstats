49402564d13f92d4bc1d5740a6e5aab07972890c
Luke Brody <luke@lukebrody.com>
2025-06-07 22:00:51 -0700

    Add more comparison tests (#1144)

--- react/test/comparison_test.ts
+++ react/test/comparison_test.ts
@@ -267,6 +267,10 @@ test('renders transpose comparision', async (t) => {
     await screencap(t)
 })
 
+test('transpose screencap', async (t) => {
+    await downloadImage(t)
+})
+
 urbanstatsFixture('scrolling transpose comparison', `${target}/comparison.html?longnames=%5B"Santa+Clarita+city%2C+California%2C+USA"%2C"Santa+Clara+city%2C+California%2C+USA"%2C"Boston+city%2C+Massachusetts%2C+USA"%2C"San+Francisco+city%2C+California%2C+USA"%2C"Denver+city%2C+Colorado%2C+USA"%5D&s=8gkGqBdgQkNpHJZ`)
 
 test('renders scrolling transpose comparision', async (t) => {
@@ -279,6 +283,10 @@ urbanstatsFixture('mobile transpose', `${target}/comparison.html?longnames=%5B%2
 
 test('renders mobile transpose correctly', async (t) => {
     await screencap(t)
+})
+
+test('mobile transpose screencap', async (t) => {
+    await downloadImage(t)
 })
 
 urbanstatsFixture('transpose with duplicate', `${target}/comparison.html?longnames=%5B%22California%2C+USA%22%2C%22Texas%2C+USA%22%2C%22Florida%2C+USA%22%2C%22Texas%2C+USA%22%5D&s=k32AgBaBU3tCGR`)
@@ -314,6 +322,7 @@ for (const platform of ['desktop', 'mobile']) {
             await waitForSelectedSearchResult(t)
             await t.pressKey('enter')
             await screencap(t)
+            await downloadImage(t)
         }
     })
 }
