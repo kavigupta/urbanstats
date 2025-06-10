1fd8560603e12c573ff6444daaba2a06c8d64084
Kavi Gupta <kavi@berkeley.edu>
2025-06-10 00:17:58 -0400

    improvements to regression function (#1148)

--- react/src/mapper/regression.ts
+++ react/src/mapper/regression.ts
@@ -1,3 +1,5 @@
+import assert from 'assert'
+
 import { MathNumericType, dotMultiply, lusolve, multiply, transpose } from 'mathjs'
 
 import { ColorStat, StatisticsForGeography } from './settings'
@@ -35,7 +37,7 @@ export class Regression {
     }
 }
 
-function calculateRegression(independent: number[], dependent: number[][], w: number[] | undefined): {
+export function calculateRegression(independent: number[], dependent: number[][], w: number[] | undefined, noIntercept: boolean = false): {
     residuals: number[]
     weights: number[]
     intercept: number
@@ -43,12 +45,8 @@ function calculateRegression(independent: number[], dependent: number[][], w: nu
     // independent: (N,)
     // dependent: (K, N)
 
-    if (dependent.length === 0) {
-        throw new Error('Must have at least one dependent variable')
-    }
-    if (dependent.some(row => row.length !== independent.length)) {
-        throw new Error(`Independent and dependent variables must have the same length: instead got independent: ${independent.length}, dependent: ${JSON.stringify(dependent.map(row => row.length))}`)
-    }
+    assert (dependent.length !== 0, 'Must have at least one dependent variable')
+    assert (dependent.every(row => row.length === independent.length), `Independent and dependent variables must have the same length: instead got independent: ${independent.length}, dependent: ${JSON.stringify(dependent.map(row => row.length))}`)
     const y = independent.map(x => [x])
     // transpose dependent
     const x = dependent[0].map((_, i) => dependent.map(row => row[i]))
@@ -65,10 +63,8 @@ function calculateRegression(independent: number[], dependent: number[][], w: nu
         w = w.filter((_, i) => !isNan[i])
     }
 
-    const awoFilt = x.map(row => [1, ...row])
-
     // eslint-disable-next-line no-restricted-syntax -- A is a matrix
-    const A = xfilt.map(row => [1, ...row])
+    const A = noIntercept ? xfilt : xfilt.map(row => [1, ...row])
     // eslint-disable-next-line no-restricted-syntax -- ATW is a matrix
     let ATW = transpose(A)
 
@@ -84,11 +80,38 @@ function calculateRegression(independent: number[], dependent: number[][], w: nu
     const wsCol = lusolve(ata, atb)
     const ws = wsCol.map(col => (col as MathNumericType[])[0])
 
-    const weights = ws.slice(1)
-    const intercept = ws[0]
+    const [weights, intercept] = noIntercept ? [ws, 0] : [ws.slice(1), ws[0]]
 
-    const preds = multiply(awoFilt, wsCol).map(pred => (pred as number[])[0])
+    const preds = multiply(A, wsCol).map(pred => (pred as number[])[0])
 
     const residuals = preds.map((pred, i) => y[i][0] - pred)
     return { residuals, weights: weights as number[], intercept: intercept as number }
+}
+
+export function computePearsonR2(
+    dependent: number[], residuals: number[], w: number[] | undefined,
+): number {
+    if (dependent.length !== residuals.length) {
+        throw new Error(`Dependent and residuals must have the same length: ${dependent.length} vs ${residuals.length}`)
+    }
+    if (w !== undefined && w.length !== dependent.length) {
+        throw new Error(`Weights must have the same length as dependent and residuals: ${w.length} vs ${dependent.length}`)
+    }
+    if (w === undefined) {
+        w = Array(dependent.length).fill(1)
+    }
+    // https://en.wikipedia.org/wiki/Coefficient_of_determination
+    // the most general definition of R^2 is:
+    // R^2 = 1 - SS_res / SS_tot
+    // where SS_res is the sum of squares of residuals and SS_tot is the total sum of squares
+    // SS_res = sum(w_i * (y_i - y_pred_i)^2)
+    // SS_tot = sum(w_i * (y_i - mean(y))^2)
+    const totalWeight = w.reduce((sum, val) => sum + val, 0)
+    const meanDependent = dependent.reduce((sum, val, i) => sum + w[i] * val, 0) / totalWeight
+
+    const ssRes = w.reduce((sum, weight, i) => sum + weight * Math.pow(residuals[i], 2), 0)
+    const ssTot = w.reduce((sum, weight, i) => sum + weight * Math.pow(dependent[i] - meanDependent, 2), 0)
+    const r = 1 - ssRes / ssTot
+
+    return r
 }
