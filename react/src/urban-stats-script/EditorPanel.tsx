b091d50d2e55cbcd6acadce77fe647dd5d22d242
Luke Brody <luke@lukebrody.com>
2025-06-12 21:32:28 -0700

    fix lint

--- react/src/urban-stats-script/EditorPanel.tsx
+++ react/src/urban-stats-script/EditorPanel.tsx
@@ -4,10 +4,8 @@ import { emptyContext } from '../../unit/urban-stats-script-utils'
 import { PageTemplate } from '../page_template/template'
 
 import { Editor } from './Editor'
-import { ValueChecker } from './editor-utils'
 import { execute } from './interpreter'
 import { UrbanStatsASTStatement } from './parser'
-import { renderType, USSValue } from './types-values'
 
 export function EditorPanel(): ReactNode {
     const [script, setScript] = useState(localStorage.getItem('editor-code') ?? '')
@@ -17,19 +15,7 @@ export function EditorPanel(): ReactNode {
         localStorage.setItem('editor-code', newScript)
     }, [])
 
-    const exec = useCallback((expr: UrbanStatsASTStatement) => {
-        const value = execute(expr, emptyContext())
-        return value
-    }, [])
-
-    const checkValue = useCallback<ValueChecker>((result: USSValue) => {
-        if (renderType(result.type) !== '[number]' && renderType(result.type) !== '[boolean]') {
-            return { ok: false, problem: 'result is not a vector of numbers or booleans' }
-        }
-        else {
-            return { ok: true }
-        }
-    }, [])
+    const exec = useCallback((expr: UrbanStatsASTStatement) => execute(expr, emptyContext()), [])
 
     return (
         <PageTemplate>
@@ -38,18 +24,6 @@ export function EditorPanel(): ReactNode {
                 setScript={updateScript}
                 execute={exec}
             />
-            {/* <Editor
-                script={script}
-                setScript={updateScript}
-                execute={exec}
-                checkValue={checkValue}
-            />
-            <Editor
-                script={script}
-                setScript={updateScript}
-                execute={exec}
-                showOutput={false}
-            /> */}
         </PageTemplate>
     )
 }
