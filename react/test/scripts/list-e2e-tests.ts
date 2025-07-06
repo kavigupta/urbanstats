f3132ec6f20e0a25794832537de7b7e90ae09848
Luke Brody <luke@lukebrody.com>
2025-07-06 16:01:15 -0700

    write list script

---
+++ react/test/scripts/list-e2e-tests.ts
@@ -0,0 +1,46 @@
+import { globSync } from 'glob'
+import { z } from 'zod'
+
+const tests = globSync('test/**/*.test.ts').map(testFile => /([^/]+)\.test\.ts$/.exec(testFile)![1]).sort()
+
+const variableValue = process.env.TEST_DURATIONS
+
+if (variableValue === undefined) {
+    // Naively run each test in its own runner
+    process.stdout.write(JSON.stringify(tests))
+    process.exit(0)
+}
+
+const testDurations = z.record(z.number()).parse(JSON.parse(variableValue))
+const groupings: { tests: string[], duration: number }[] = []
+const durationLimit = 4.5 * 60 * 1000
+
+/**
+ * For known tests, fill up 4.5 minute buckets
+ * For unknown tests, just append them
+ */
+
+const leftovers: string[] = []
+
+tests: for (const test of tests) {
+    if (test in testDurations) {
+        const duration = testDurations[test]
+        for (const grouping of groupings) {
+            if (grouping.duration + duration < durationLimit) {
+                grouping.tests.push(test)
+                grouping.duration += duration
+                continue tests
+            }
+        }
+        // If no grouping found
+        groupings.push({
+            tests: [test],
+            duration,
+        })
+    }
+    else {
+        leftovers.push(test)
+    }
+}
+
+process.stdout.write(JSON.stringify(groupings.map(g => `{${g.tests.join(',')}}`).concat(leftovers)))
