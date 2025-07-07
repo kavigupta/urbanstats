9d4986d784f824677dedfdecde91c825923a9fbc
Kavi Gupta <kavi@berkeley.edu>
2025-07-06 22:26:36 -0400

    update

---
+++ react/src/utils/unit.ts
@@ -0,0 +1,59 @@
+export type UnitType = 'percentage' | 'fatalities' | 'fatalitiesPerCapita' | 'density' | 'population'
+    | 'area' | 'distanceInKm' | 'distanceInM' | 'democraticMargin' | 'temperature' | 'time' | 'distancePerYear'
+    | 'contaminantLevel' | 'default'
+
+// Validated list of all unit types - this ensures we have every value from UnitType
+export const allUnitTypes = [
+    'percentage',
+    'fatalities',
+    'fatalitiesPerCapita',
+    'density',
+    'population',
+    'area',
+    'distanceInKm',
+    'distanceInM',
+    'democraticMargin',
+    'temperature',
+    'time',
+    'distancePerYear',
+    'contaminantLevel',
+    'default',
+] as const
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars -- just to check that all unit types are covered
+function checkAllIncluded(unitType: UnitType): (typeof allUnitTypes)[number] {
+    return unitType
+}
+
+export function getUnitName(unitType: UnitType): string {
+    switch (unitType) {
+        case 'percentage':
+            return 'Percentage'
+        case 'fatalities':
+            return 'Fatalities'
+        case 'fatalitiesPerCapita':
+            return 'Fatalities Per Capita'
+        case 'density':
+            return 'Density'
+        case 'population':
+            return 'Population'
+        case 'area':
+            return 'Area'
+        case 'distanceInKm':
+            return 'Distance [km]'
+        case 'distanceInM':
+            return 'Distance [m]'
+        case 'democraticMargin':
+            return 'Democratic Margin'
+        case 'temperature':
+            return 'Temperature'
+        case 'time':
+            return 'Time'
+        case 'distancePerYear':
+            return 'Distance Per Year'
+        case 'contaminantLevel':
+            return 'Contaminant Level'
+        case 'default':
+            return 'Default'
+    }
+}
