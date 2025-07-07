19dd4ec78d83b0995d9e8d2736c6f187aa67af68
Luke Brody <luke@lukebrody.com>
2025-07-06 18:11:12 -0700

    clean up

--- react/test/new.test.ts
+++
@@ -1,9 +0,0 @@
-import { urbanstatsFixture } from './test_utils'
-
-urbanstatsFixture('longer article test', '/article.html?longname=California%2C+USA')
-
-test('too long test', async (t) => {
-    await t.wait(6 * 60 * 1000)
-})
-
-// Some trivial change
