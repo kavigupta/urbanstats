93f251ab8275d9165a068730245c4118e113b543
Kavi Gupta <kavi@berkeley.edu>
2025-07-09 22:08:13 -0700

    update

---
+++ urbanstats/compatibility/pandas_numeric_stub.py
@@ -0,0 +1,18 @@
+from pandas.core.indexes.base import Index
+
+# Based on https://github.com/pandas-dev/pandas/issues/53300#issuecomment-1553939190
+
+class NumericIndex(Index):
+    pass
+
+class IntegerIndex(NumericIndex):
+    pass
+
+class Int64Index(IntegerIndex):
+    pass
+
+class UInt64Index(IntegerIndex):
+    pass
+
+class Float64Index(NumericIndex):
+    pass
