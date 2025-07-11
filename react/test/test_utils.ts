5673ce6f117b4a1cc8eb7d3ab87047f8102f8f8b
Kavi Gupta <kavi@berkeley.edu>
2025-07-11 12:39:24 -0700

    wait for element to exist

--- react/test/test_utils.ts
+++ react/test/test_utils.ts
@@ -286,6 +286,9 @@ export function mapElement(r: RegExp): Selector {
 
 export async function clickMapElement(t: TestController, r: RegExp): Promise<void> {
     const element = mapElement(r)
+    // wait for element to exist
+    await t.expect(element.exists).ok({ timeout: 10000 })
+    console.log('Element exists!')
     const clickablePolygon: string = (await element.getAttribute('clickable-polygon'))!
     await t.eval(() => {
         const cm = (window as unknown as {
