0f6c5037d7d13bf0f9633cc4283d8799d194a862
Kavi Gupta <kavi@berkeley.edu>
2025-07-14 10:45:53 -0700

    undo unecessary

--- urbanstats/consolidated_data/produce_consolidated_data.py
+++ urbanstats/consolidated_data/produce_consolidated_data.py
@@ -168,4 +168,4 @@ def full_consolidated_data(folder):
 
 def output_names(mapper_folder):
     with open(f"{mapper_folder}/used_geographies.ts", "w") as f:
-        output_typescript(use, f)
+        output_typescript(use, f, data_type="string[]")
