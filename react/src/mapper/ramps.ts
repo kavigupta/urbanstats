e7b680ae783d37e2570bb28cb690254042917cdd
Kavi Gupta <kavi@berkeley.edu>
2025-07-22 14:11:54 -0700

    fix

--- react/src/mapper/ramps.ts
+++ react/src/mapper/ramps.ts
@@ -1,6 +1,5 @@
 import hueColors from '../data/hueColors'
 import RAMPS from '../data/mapper/ramps'
-import { RampT } from '../urban-stats-script/constants/ramp'
 
 // eslint-disable-next-line no-restricted-syntax -- Represents encoded data
 export type EncodedColorMap = { type: 'none' } | { type: 'custom', custom_colormap: string } | { type: 'preset', name: string }
@@ -26,7 +25,7 @@ export interface ConstantRampDescriptor extends CommonRampDescriptor {
 /* eslint-enable no-restricted-syntax */
 export type RampDescriptor = ConstantRampDescriptor | CommonRampDescriptor & { type: 'linear' } | CommonRampDescriptor & { type: 'geometric' }
 
-export function getRamps(): Record<string, RampT> {
+export function getRamps(): Record<string, Keypoints> {
     return {
         Uridis: [
             [0, hueColors.purple],
