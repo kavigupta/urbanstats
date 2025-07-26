4c88781bfa62346b487160756fe42e674510cf09
Kavi Gupta <kavi@berkeley.edu>
2025-07-26 12:23:06 -0400

    clean up ramps
    include more ramps; remove diverging ramps and those that are just lists of colors in their name

--- urbanstats/mapper/ramp.py
+++ urbanstats/mapper/ramp.py
@@ -10,17 +10,80 @@ def get_pyplot_ramps():
     for ramp_name in plt.colormaps():
         if ramp_name.endswith("_r"):
             continue
-        if ramp_name in ["flag", "prism"]:
+        if ramp_name in ["flag", "prism", "hsv"]:
             # cyclic colormaps
             continue
-        ramp_obj = mpl.cm.get_cmap(ramp_name)
-        if isinstance(ramp_obj, mpl.colors.ListedColormap):
+        if ramp_name in [
+            "PiYG",
+            "PRGn",
+            "BrBG",
+            "PuOr",
+            "RdGy",
+            "RdBu",
+            "RdYlBu",
+            "RdYlGn",
+            "Spectral",
+            "coolwarm",
+            "bwr",
+            "seismic",
+            "berlin",
+            "managua",
+            "vanimo",
+        ]:
+            # diverging colormaps
             continue
-        assert isinstance(ramp_obj, mpl.colors.LinearSegmentedColormap)
-        ramp_name = ramp_name[0].upper() + ramp_name[1:]
-        pyplot_ramps[ramp_name] = ramp_obj_to_list(ramp_obj)
+        if ramp_name in [
+            "brg",
+            "Blues",
+            "BuGn",
+            "BuPu",
+            "GnBu",
+            "Gray",
+            "Greens",
+            "Greys",
+            "OrRd",
+            "Oranges",
+            "PuBu",
+            "PuBuGn",
+            "PuRd",
+            "Purples",
+            "RdPu",
+            "Reds",
+            "YlGn",
+            "YlGnBu",
+            "YlOrBr",
+            "YlOrRd",
+            "Pink",
+            "Gray",
+            "Rainbow",
+            "Spring",
+            "Summer",
+            "Terrain",
+            "Winter",
+        ]:
+            # ramps that are unnamed (name only composed of color names)
+            continue
+        if ramp_name in [
+            "Pastel1",
+            "Pastel2",
+            "Paired",
+            "Accent",
+            "Dark2",
+            "Set1",
+            "Set2",
+            "Set3",
+            "tab10",
+            "tab20",
+            "tab20b",
+            "tab20c",
+        ]:
+            # qualitative colormaps
+            continue
+        ramp_obj = mpl.cm.get_cmap(ramp_name)
+        ramp_name_disp = ramp_name.replace("_", " ").title()
+        pyplot_ramps[ramp_name_disp] = ramp_obj_to_list(ramp_obj)
 
-    pyplot_ramps = dict(sorted(pyplot_ramps.items(), key=lambda x: x[0].lower()))
+    # pyplot_ramps = dict(sorted(pyplot_ramps.items(), key=lambda x: x[0].lower()))
     return pyplot_ramps
 
 
@@ -39,15 +102,17 @@ def ramp_obj_to_list(ramp_obj):
     at that position.
     """
     # pylint: disable=protected-access
-    assert isinstance(ramp_obj, mpl.colors.LinearSegmentedColormap)
-
-    if callable(ramp_obj._segmentdata["red"]) or len(ramp_obj._segmentdata["red"]) < 10:
-        xs = np.linspace(0, 1, 50).tolist()
+    if isinstance(ramp_obj, mpl.colors.LinearSegmentedColormap):
+        if callable(ramp_obj._segmentdata["red"]):
+            xs = np.linspace(0, 1, 50).tolist()
+        else:
+            xs = sorted(
+                {x for segment in ramp_obj._segmentdata.values() for x, _, _ in segment}
+            )
+        # pylint: disable=consider-using-f-string
     else:
-        xs = sorted(
-            {x for segment in ramp_obj._segmentdata.values() for x, _, _ in segment}
-        )
-    # pylint: disable=consider-using-f-string
+        assert isinstance(ramp_obj, mpl.colors.ListedColormap)
+        xs = np.linspace(0, 1, len(ramp_obj.colors)).tolist()
     return [
         (x, "#%02x%02x%02x" % tuple(int(255 * y) for y in ramp_obj(x))[:-1]) for x in xs
     ]
