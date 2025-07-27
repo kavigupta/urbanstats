395d5a23f349ffb2b3e353accecd866dc008321f
Kavi Gupta <kavi@berkeley.edu>
2025-07-27 16:57:00 -0400

    add downloading image tests

--- react/test/mapper.test.ts
+++ react/test/mapper.test.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 
 import { Selector } from 'testcafe'
 
-import { target, downloadOrCheckString, mostRecentDownloadPath, screencap, urbanstatsFixture } from './test_utils'
+import { target, downloadOrCheckString, mostRecentDownloadPath, screencap, urbanstatsFixture, grabDownload } from './test_utils'
 
 async function checkGeojson(t: TestController, path: string): Promise<void> {
     // download the geojson by clicking the button
@@ -13,11 +13,17 @@ async function checkGeojson(t: TestController, path: string): Promise<void> {
     await downloadOrCheckString(t, mostRecentDownload, path, 'json')
 }
 
+export async function downloadPNG(t: TestController): Promise<void> {
+    const download = Selector('button').withExactText('Export as PNG')
+    await grabDownload(t, download)
+}
+
 urbanstatsFixture('mapping', `${target}/mapper.html?settings=H4sIAAAAAAAAA4WQwUrEMBCGX2UZ2VuRvXjpUcGroEeRZVpn07DTpGQSd8uSd3emiK0nc0ryf%2F83ITdwFF3CaZiPZx8%2BoYW30gXMPgbk3Ss53UADJ8%2BZErQ3oIAdk4InZCFNSuiNtizPE6nh96qBL0zeeIH2%2FcOOXIx4Xgm6TolEFgNArQ30kWM6Ssa8cYoPjglWxSNjf97tQQsJx2mDsg%2BECX5EI24zm0VZs4CjnZ9KRwOxvy6TramDZx2klaWuzN1hWVq6kHeDvupw%2F6B0h0J%2F7VFGpWT9wJeSzSn%2F6Gqt3%2Bx7oSqJAQAA`)
 
 test('state-map', async (t) => {
     await screencap(t)
     await checkGeojson(t, 'state-map-geojson')
+    await downloadPNG(t)
 })
 
 urbanstatsFixture('mapping-more-complex', `${target}/mapper.html?settings=H4sIAAAAAAAAA5WSwW6DMAyGXwV5l3ZqJ8ax10o797DbVCFDDUQLSeSErqjqu88BRpm0wyokhH7bn3%2FbXKEmWzO6ps8%2FlTnBDva2M6GHDVRKB2LYXYEMFpokWKH2JJHOlEFZE2OhdyRVs3S7baC02nLuA4a%2FMjZwRt1F6W0psYpNPOw%2BrmCwjfGLBOjieEHxytSaFoyDdZ3GSEn2DZqaklWWvqbbLM3SdXQz09xjtAETCcexjLwfRgZKtsnqkjwnbi2VE7xR8s1UT2njGLJQciQvE%2F7XFGR5c8nIeMDpcVhjLhbUacgZ8zlXRg5ZkhMXUExaaamqVKmmRtCClH%2BRqpuQF33u7p7Gq9%2BO4o2xdYtJtDKEDNPBW1zG4sIo3Bf0TsyohgmZzsQ%2B%2Fk%2BBOxIhcuR36WVAAQwwqXgq0vjAjy2R0pcsAgr09Lub9a1c6htvmskEzgIAAA%3D%3D`)
@@ -26,6 +32,7 @@ test('mapping-more-complex', async (t) => {
     await t.resizeWindow(1400, 800)
     await screencap(t)
     await checkGeojson(t, 'mapping-more-complex-geojson')
+    await downloadPNG(t)
 })
 
 urbanstatsFixture('mapping-with-regression', `${target}/mapper.html?settings=H4sIAAAAAAAAA52TwW7CMAyGX6XyLiCxCSa4VNqFoe067bLDhKK0dUtEmkRJOlZVvPucttCCdhjrLf7tz3byt4ECdWG52dVsL1QGMTzrSvkaZpAL6dFC3AAqnkgkMefSISmVSr3QKmi%2BNkhV59AMvrisQuhlCOG3sehcWwJtihUB6SD%2B3B6PM0i11JY5z%2F2fmWNGA4qXQd8s%2B24jjBOqkDiCvH1EG1RO%2BDqa2KflvpwCzTAwHlfzmylU02K2V8uicVTTk9%2Bx6JUIrdU20nlEqj2Hu10yNKgyVL473nwb1zNIXUzCUtNhEgiDUg2jLJG1sG7SEBOK3j1F48%2BPxVKNeS5S0Q8FJVsAEQ4oip1nSc2MNpXknSl6l5CdTov861F%2FXWN5scRxS96xvDSjBildo%2BfUs3dVycdq4KEfGGu7fgWCSH1AyxLyfvgH7heUURkzCi3aLKGQXFqTEQjZ4km6m7cfnO4D4vnDirIT7vCyu3YlDf0DcHWWOnUDAAA%3D`)
@@ -34,4 +41,5 @@ test('mapping-with-regression', async (t) => {
     await t.resizeWindow(1400, 800)
     await screencap(t)
     await checkGeojson(t, 'mapping-with-regression-geojson')
+    await downloadPNG(t)
 })
