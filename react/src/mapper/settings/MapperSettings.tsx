cf97dc0575fa84c98dc71e1c422a0b87e190a0a4
Luke Brody <luke@lukebrody.com>
2025-08-02 22:43:22 -0700

    integrate into mapper

--- react/src/mapper/settings/MapperSettings.tsx
+++ react/src/mapper/settings/MapperSettings.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useMemo } from 'react'
+import React, { ReactNode, useEffect, useMemo, useRef } from 'react'
 
 import valid_geographies from '../../data/mapper/used_geographies'
 import universes_ordered from '../../data/universes_ordered'
@@ -11,12 +11,66 @@ import { Selection, SelectionContext } from './SelectionContext'
 import { TopLevelEditor } from './TopLevelEditor'
 import { MapSettings } from './utils'
 
+interface UndoRedoItem { time: number, uss: MapSettings['script']['uss'], selection: Selection | undefined }
+
+const undoChunking = 1000
+const undoHistory = 100
+
 export function MapperSettings({ mapSettings, setMapSettings, errors }: {
     mapSettings: MapSettings
     setMapSettings: (setter: (existing: MapSettings) => MapSettings) => void
     errors: EditorError[]
 }): ReactNode {
+    const uss = mapSettings.script.uss
+    const setUss = (newUss: MapSettings['script']['uss']): void => {
+        setMapSettings(s => ({
+            ...s,
+            script: { uss: newUss },
+        }))
+    }
+
     const selectionContext = useMemo(() => new Property<Selection | undefined>(undefined), [])
+
+    useEffect(() => {
+        const observer = (): void => {
+            undoStack.current[undoStack.current.length - 1].selection = selectionContext.value
+        }
+
+        selectionContext.observers.add(observer)
+        return () => { selectionContext.observers.delete(observer) }
+    }, [selectionContext])
+
+    const undoStack = useRef<UndoRedoItem[]>([{ time: Date.now(), uss, selection: selectionContext.value }])
+    const redoStack = useRef<UndoRedoItem[]>([])
+
+    useEffect(() => {
+        const listener = (e: KeyboardEvent): void => {
+            const isMac = navigator.userAgent.includes('Mac')
+            if (isMac ? e.key === 'z' && e.metaKey && !e.shiftKey : e.key === 'z' && e.ctrlKey) {
+                e.preventDefault()
+                // Undo
+                if (undoStack.current.length >= 2) {
+                    const prevState = undoStack.current[undoStack.current.length - 2]
+                    // Prev state becomes current state, current state becomes redo state
+                    redoStack.current.push(undoStack.current.pop()!)
+                    setUss(prevState.uss)
+                    selectionContext.value = prevState.selection
+                }
+            }
+            else if (isMac ? e.key === 'z' && e.metaKey && e.shiftKey : e.key === 'y' && e.ctrlKey) {
+                e.preventDefault()
+                // Redo
+                const futureState = redoStack.current.pop()
+                if (futureState !== undefined) {
+                    undoStack.current.push(futureState)
+                    setUss(futureState.uss)
+                    selectionContext.value = futureState.selection
+                }
+            }
+        }
+        window.addEventListener('keydown', listener)
+        return () => { window.removeEventListener('keydown', listener) }
+    })
 
     return (
         <SelectionContext.Provider value={selectionContext}>
@@ -47,12 +101,23 @@ export function MapperSettings({ mapSettings, setMapSettings, errors }: {
                 }
             />
             <TopLevelEditor
-                uss={mapSettings.script.uss}
-                setUss={(uss) => {
-                    setMapSettings(s => ({
-                        ...s,
-                        script: { uss },
-                    }))
+                uss={uss}
+                setUss={(newUss) => {
+                    setUss(newUss)
+
+                    const currentUndoState = undoStack.current[undoStack.current.length - 1]
+                    if (currentUndoState.time + undoChunking > Date.now()) {
+                        // ammend current item rather than making a new one
+                        currentUndoState.uss = newUss
+                        currentUndoState.selection = selectionContext.value
+                    }
+                    else {
+                        undoStack.current.push({ time: Date.now(), uss: newUss, selection: selectionContext.value })
+                        while (undoStack.current.length > undoHistory) {
+                            undoStack.current.shift()
+                        }
+                    }
+                    redoStack.current = []
                 }}
                 typeEnvironment={defaultTypeEnvironment(mapSettings.universe)}
                 errors={errors}
