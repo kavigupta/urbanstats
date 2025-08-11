ff37f2ea8417bc8f38b023aa5da7db78424ef165
Luke Brody <luke@lukebrody.com>
2025-08-10 17:54:27 -0700

    venv

--- ci_api/run.sh
+++ ci_api/run.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 rm -r venv
-virtualenv -p python3 venv
+python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 uvicorn --host 0.0.0.0 --port 8080 src.main:app
