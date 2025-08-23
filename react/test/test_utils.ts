5fd374ae7ccb1af191a90a6b26b270cbd43cb5f5
Kavi Gupta <kavi@berkeley.edu>
2025-08-23 10:53:47 -0400

    add a mkdir

--- react/test/test_utils.ts
+++ react/test/test_utils.ts
@@ -184,6 +184,7 @@ async function copyMostRecentFile(t: TestController, laterThan: number): Promise
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access -- TestCafe doesn't have a public API for the screenshots folder
     const screenshotsFolder: string = t.testRun.opts.screenshots.path ?? (() => { throw new Error() })()
     const mrdp = await waitForDownload(t, laterThan)
+    fs.mkdirSync(screenshotsFolder, { recursive: true })
     fs.copyFileSync(mrdp, path.join(screenshotsFolder, screenshotPath(t)))
 }
 
