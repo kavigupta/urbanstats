b4de0c1a0cc5304d2c7e755b56d2564bcd254210
Kavi Gupta <kavi@berkeley.edu>
2025-09-03 20:25:09 -0400

    add suounties

---
+++ urbanstats/geometry/historical_counties/all_counties.py
@@ -0,0 +1,211 @@
+from datetime import datetime, timedelta
+
+import geopandas as gpd
+import pandas as pd
+import us
+from permacache import permacache
+
+end_of_time = "9999-12-31"
+
+
+@permacache("urbanstats/geometry/historical_counties/all_counties/get_all_counties")
+def get_all_counties():
+    data = gpd.read_file(
+        "named_region_shapefiles/historical-counties/US_AtlasHCB_Counties/US_HistCounties_Shapefile/US_HistCounties.shp"
+    )
+    data = data[
+        [
+            "STATE_TERR",
+            "FIPS",
+            "START_DATE",
+            "END_DATE",
+            "CNTY_TYPE",
+            "FULL_NAME",
+            "geometry",
+        ]
+    ]
+    data.loc[data.END_DATE == "2000-12-31", "END_DATE"] = end_of_time
+    county_2010 = gpd.read_file(
+        "named_region_shapefiles/gz_2010_us_050_00_500k.zip"
+    ).rename(columns={"STATE": "STATEFP", "COUNTY": "COUNTYFP"})
+    # county_2015 = gpd.read_file("named_region_shapefiles/cb_2015_us_county_500k.zip")
+    county_2022 = gpd.read_file("named_region_shapefiles/cb_2022_us_county_500k.zip")
+    data = handle_recent_changes(data, county_2010=county_2010, county_2022=county_2022)
+    return data
+
+
+def date_minus_one(date):
+    dt = datetime.strptime(date, "%Y-%m-%d")
+    dt = dt - timedelta(days=1)
+    return dt.strftime("%Y-%m-%d")
+
+
+def terminate(frame, date, fips):
+    mask = (frame.FIPS == fips) & (frame.END_DATE == end_of_time)
+    assert mask.sum() == 1, f"expected exactly one match for {fips}, got {mask.sum()}"
+    frame.loc[mask, "END_DATE"] = date
+
+
+def introduce(frame, date, fips, county_source, *, alter=False):
+    if alter:
+        # just terminate the old one
+        terminate(frame, date_minus_one(date), fips)
+    mask = county_source.STATEFP + county_source.COUNTYFP == fips
+    assert mask.sum() == 1, f"expected exactly one match for {fips}, got {mask.sum()}"
+    row = county_source.loc[mask].iloc[0]
+    county_name = row.NAME
+    # add as needed. should crash if something unexpected
+    county_type = {
+        "Muny": "Municipality",
+        "CA": "Census Area",
+        "Cty&Bor": "County and Borough",
+        "Borough": "Borough",
+        "County": "County",
+        "city": "City",
+        "PL": "Planning Region",
+        "04": "Borough",
+        "05": "Census Area",
+        "06": "County",
+    }[row.LSAD]
+
+    row = pd.Series(
+        {
+            "FIPS": fips,
+            "START_DATE": date,
+            "END_DATE": end_of_time,
+            "CNTY_TYPE": county_type,
+            "FULL_NAME": county_name,
+            "geometry": row.geometry,
+            "STATE_TERR": us.states.lookup(row.STATEFP).name,
+        }
+    )
+    frame = frame.append(row, ignore_index=True)
+    return frame
+
+
+def handle_recent_changes(frame, *, county_2010, county_2022):
+    """
+    handle recent changes as listed in https://www.census.gov/programs-surveys/geography/technical-documentation/county-changes.html
+    """
+    frame = frame.copy()
+    # AK
+    """
+    2007-06-20: split
+        deleted Skagway-Hoonah-Angoon Census Area, Alaska (02-232)
+        created Skagway Municipality (02-230)
+        created Hoonah-Angoon Census Area (02-105)
+    """
+
+    terminate(frame, "2007-06-19", "02232")
+    frame = introduce(frame, "2007-06-20", "02230", county_2010)
+    frame = introduce(frame, "2007-06-20", "02105", county_2010)
+
+    """
+    2008-05-19/2008-06-01: something complicated
+        deleted Prince of Wales-Outer Ketchikan Census Area, Alaska (02-201)
+        deleted Wrangell-Petersburg Census Area, Alaska (02-280)
+        created Petersburg Census Area, Alaska (02-195)
+        created Prince of Wales-Hyder Census Area, Alaska (02-198)
+        created Wrangell City and Borough, Alaska (02-275)
+        altered Ketchikan Gateway Borough, Alaska (02-130)
+    """
+    terminate(frame, "2008-05-31", "02201")
+    terminate(frame, "2008-05-31", "02280")
+    frame = introduce(frame, "2008-06-01", "02195", county_2010)
+    frame = introduce(frame, "2008-06-01", "02198", county_2010)
+    frame = introduce(frame, "2008-06-01", "02275", county_2010)
+    frame = introduce(frame, "2008-06-01", "02130", county_2010, alter=True)
+    """
+    2013-01-03: split
+        altered Petersburg Census Area, Alaska and renamed it to Petersburg Borough, Alaska (02-195)
+        altered Hoonah-Angoon Census Area, Alaska (02-105)
+        altered Prince of Wales-Hyder Census Area, Alaska (02-198)
+    """
+    frame = introduce(frame, "2013-01-03", "02195", county_2022, alter=True)
+    frame = introduce(frame, "2013-01-03", "02105", county_2022, alter=True)
+    frame = introduce(frame, "2013-01-03", "02198", county_2022, alter=True)
+
+    """
+    2015-07-01: renamed
+        renamed Wade Hampton Census Area, Alaska (02-270) to Kusilvak Census Area, Alaska (02-158). No boundary changes.
+    """
+    terminate(frame, "2015-06-30", "02270")
+    frame = introduce(frame, "2015-07-01", "02158", county_2022)
+
+    """
+    2019-02-02: split
+        deleted Valdez-Cordova Census Area, Alaska (02-261)
+        created Chugach Census Area, Alaska (02-063)
+        created Copper River Census Area, Alaska (02-066)
+    """
+
+    terminate(frame, "2019-02-01", "02261")
+    frame = introduce(frame, "2019-02-02", "02063", county_2022)
+    frame = introduce(frame, "2019-02-02", "02066", county_2022)
+
+    # CO
+    """
+    2001-11-15: carved new city from parts of 4 counties
+        altered Adams County, Colorado (08-001)
+        altered Boulder County, Colorado (08-013)
+        altered Jefferson County, Colorado (08-059)
+        altered Weld County, Colorado (08-123)
+
+        created Broomfield County, Colorado (08-014)
+    """
+
+    frame = introduce(frame, "2001-11-15", "08001", county_2010, alter=True)
+    frame = introduce(frame, "2001-11-15", "08013", county_2010, alter=True)
+    frame = introduce(frame, "2001-11-15", "08059", county_2010, alter=True)
+    frame = introduce(frame, "2001-11-15", "08123", county_2010, alter=True)
+    frame = introduce(frame, "2001-11-15", "08014", county_2010)
+
+    # VA
+    """
+    2001-07-01: deleted independent city, added to county
+        deleted Clifton Forge (independent) city, Virginia (51-560)
+        altered Alleghany County, Virginia (51-005)
+    """
+    terminate(frame, "2001-06-30", "51560")
+    frame = introduce(frame, "2001-07-01", "51005", county_2010, alter=True)
+
+    """
+    2003-07-01: exchange of territory
+        altered York County, Virginia (51-199)
+        altered Newport News (independent) city, Virginia (51-700)
+    """
+    frame = introduce(frame, "2003-07-01", "51199", county_2010, alter=True)
+    frame = introduce(frame, "2003-07-01", "51700", county_2010, alter=True)
+
+    """
+    2013-07-01: deleted independent city, added to county
+        deleted Bedford (independent) city, Virginia (51-515)
+        altered Bedford County, Virginia (51-019)
+    """
+    terminate(frame, "2013-06-30", "51515")
+    frame = introduce(frame, "2013-07-01", "51019", county_2022, alter=True)
+
+    # ND
+    """
+    2015-05-01: renamed
+        renamed Shannon County, South Dakota (46-113) to Oglala Lakota County, South Dakota (46-102). No boundary changes.
+    """
+    terminate(frame, "2015-04-30", "46113")
+    frame = introduce(frame, "2015-05-01", "46102", county_2022)
+
+    # CT
+    """
+    2022-06-02: completely new county system
+    """
+
+    frame.loc[
+        (frame.STATE_TERR == "Connecticut") & (frame.END_DATE == end_of_time),
+        "END_DATE",
+    ] = "2022-06-01"
+
+    ct22 = county_2022[county_2022.STATE_NAME == "Connecticut"]
+    for _, row in ct22.iterrows():
+        fips = row.STATEFP + row.COUNTYFP
+        frame = introduce(frame, "2022-06-02", fips, county_2022)
+
+    return frame
---
+++ urbanstats/geometry/historical_counties/compute_suos.py
@@ -0,0 +1,118 @@
+import geopandas as gpd
+import numpy as np
+import shapely
+import tqdm.auto as tqdm
+from permacache import permacache
+
+from urbanstats.data.census_blocks import load_raw_census
+
+from .all_counties import get_all_counties
+
+
+@permacache("urbanstats/geometry/historical_counties/compute_suos/current_suos_2")
+def current_suos():
+    data = get_all_counties()
+    suos_per_datum, representative_point, suo_idx_to_subset = compute_suos(data)
+    return suos_per_datum, representative_point, suo_idx_to_subset
+
+
+def coordinates_blocks(extra_geographies):
+    *_, coordinates = load_raw_census()
+    *_, coordinates = load_raw_census()
+    pt = extra_geographies.representative_point()
+    coordinates = np.concatenate([coordinates, np.array([pt.y, pt.x]).T])
+    blocks = gpd.GeoDataFrame(
+        dict(geometry=gpd.points_from_xy(coordinates[:, 1], coordinates[:, 0])),
+        crs="epsg:4326",
+    )
+    return coordinates, blocks
+
+
+def compute_suos(data):
+    coordinates, blocks = coordinates_blocks(data)
+    joined = gpd.sjoin(data[["geometry"]], blocks)
+    missing = set(data.index) - set(joined.index)
+    assert not missing, missing
+    idx_data = np.array(joined.index)
+    idx_blocks = np.array(joined.index_right)
+    set_per_block = [[] for _ in tqdm.trange(len(blocks))]
+    for idxd, idxb in zip(idx_data, tqdm.tqdm(idx_blocks)):
+        set_per_block[idxb].append(idxd)
+    set_per_block = [tuple(sorted(x)) for x in tqdm.tqdm(set_per_block) if x]
+    suo_idx_to_subset = sorted(set(set_per_block))
+    representative_point = compute_suo_representative(
+        coordinates, set_per_block, suo_idx_to_subset
+    )
+    suos_per_datum = [[] for _ in range(len(data))]
+    for i, suo in enumerate(suo_idx_to_subset):
+        for county in suo:
+            suos_per_datum[county].append(i)
+
+    return suos_per_datum, np.array(representative_point), suo_idx_to_subset
+
+
+def compute_suo_representative(coordinates, set_per_block, suo_idx_to_subset):
+    subset_to_suo_idx = {k: i for i, k in enumerate(suo_idx_to_subset)}
+    coordinates_each = [[] for _ in suo_idx_to_subset]
+    for i, subset in enumerate(tqdm.tqdm(set_per_block)):
+        coordinates_each[subset_to_suo_idx[subset]].append(i)
+    representative_point_each = []
+    for ce in coordinates_each:
+        ce = np.array(ce)
+        c = coordinates[ce]
+        c = c[np.abs(c - c.mean(0)).sum(1).argmin()]
+        representative_point_each.append(c)
+    return np.array(representative_point_each)
+
+
+def compute_intersection(geometries, precomputed, elements):
+    if elements in precomputed:
+        return precomputed[elements]
+    precomputed[elements] = compute_intersection_direct(
+        geometries, precomputed, elements
+    )
+    return precomputed[elements]
+
+
+def compute_intersection_direct(geometries, precomputed, elements):
+    if len(elements) == 1:
+        return geometries[elements[0]]
+    relevant_subsets = [x for x in precomputed if all(y in elements for y in x)]
+    subset = elements[:-1] if not relevant_subsets else max(relevant_subsets, key=len)
+    remaining = tuple(x for x in elements if x not in subset)
+    return shapely.intersection(
+        compute_intersection(geometries, precomputed, subset),
+        compute_intersection(geometries, precomputed, remaining),
+    )
+
+
+@permacache("urbanstats/geometry/historical_counties/compute_suos/compute_suo_geometry")
+def compute_suo_geometry():
+    data = get_all_counties().copy()
+    _, _, suos = current_suos()
+    fixed_geo = data.geometry.buffer(0)
+    by_subset = {}
+    geometry_per_suo = [
+        compute_intersection(fixed_geo, by_subset, tuple(suo))
+        for suo in tqdm.tqdm(suos)
+    ]
+    geometry_per_suo = [clean_up_geometry(g) for g in geometry_per_suo]
+    return geometry_per_suo
+
+
+def clean_up_geometry(geom):
+    if isinstance(geom, (shapely.geometry.MultiPolygon, shapely.geometry.Polygon)):
+        return geom
+    if isinstance(geom, shapely.geometry.GeometryCollection):
+        polygons = []
+        for g in geom.geoms:
+            if isinstance(g, shapely.geometry.Polygon):
+                polygons.append(g)
+            elif isinstance(g, shapely.geometry.MultiPolygon):
+                polygons.extend(list(g.geoms))
+            else:
+                assert isinstance(
+                    g, (shapely.geometry.LineString, shapely.geometry.Point)
+                ), type(g)
+        assert polygons, "no polygons found"
+        return shapely.geometry.MultiPolygon(polygons)
