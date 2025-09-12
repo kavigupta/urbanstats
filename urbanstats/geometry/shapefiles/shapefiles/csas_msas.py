b266c2f2052c6e18b5fb11549f2be283b886ad01
Kavi Gupta <kavi@berkeley.edu>
2025-09-12 11:53:21 -0700

    unify csa / msa

---
+++ urbanstats/geometry/shapefiles/shapefiles/csas_msas.py
@@ -0,0 +1,34 @@
+from urbanstats.geometry.shapefiles.shapefile import Shapefile
+from urbanstats.geometry.shapefiles.shapefile_subset import SelfSubset
+from urbanstats.geometry.shapefiles.utils import name_components
+from urbanstats.universe.universe_provider.constants import us_domestic_provider
+
+
+def csa_or_msa(hash_key, typ, path):
+    return Shapefile(
+        hash_key=hash_key,
+        path=path,
+        shortname_extractor=lambda x: name_components(typ, x)[0],
+        longname_extractor=lambda x: ", ".join(
+            name_components(typ, x, abbreviate=True)
+        ),
+        filter=lambda x: True,
+        meta=dict(type=typ, source="Census", type_category="Census"),
+        does_overlap_self=False,
+        universe_provider=us_domestic_provider(),
+        subset_masks={"USA": SelfSubset()},
+        abbreviation=typ,
+        data_credit=dict(
+            linkText="US Census",
+            link="https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html",
+        ),
+        include_in_syau=True,
+    )
+
+
+CSAs = csa_or_msa(
+    "census_csas_4", "CSA", "named_region_shapefiles/cb_2018_us_csa_500k.zip"
+)
+MSAs = csa_or_msa(
+    "census_msas_4", "MSA", "named_region_shapefiles/cb_2018_us_cbsa_500k.zip"
+)
