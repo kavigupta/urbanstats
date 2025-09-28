7a1c86174b834ffd1e744f3845d18cfdddfed9bb
Kavi Gupta <kavi@berkeley.edu>
2025-09-28 13:00:25 -0700

    move

--- urbanstats-persistent-data/all_routes.py
+++ urbanstats-persistent-data/all_routes.py
@@ -21,14 +21,6 @@ from urbanstats_persistent_data.routes.stats import (
     retrostat_store_user_stats_request,
 )
 
-retrostat_latest_week_request
-retrostat_get_per_question_stats_request
-juxtastat_infinite_has_infinite_stats_request
-juxtastat_get_per_question_stats_request
-juxtastat_register_user_request
-juxtastat_latest_day_request
-juxtastat_store_user_stats_request
-retrostat_store_user_stats_request
 
 juxtastat_friend_request
 juxtastat_todays_score_for
@@ -37,3 +29,12 @@ juxtastat_infinite_results
 lengthen_request
 shorten_request
 route_s
+
+retrostat_latest_week_request
+retrostat_get_per_question_stats_request
+juxtastat_infinite_has_infinite_stats_request
+juxtastat_get_per_question_stats_request
+juxtastat_register_user_request
+juxtastat_latest_day_request
+juxtastat_store_user_stats_request
+retrostat_store_user_stats_request
