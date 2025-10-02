0aa5388220c97a8294a901611d3eea855e883405
Luke Brody <luke@lukebrody.com>
2025-10-01 23:26:12 -0700

    Add more tests

--- react/unit/urban-stats-script-literal-parser.test.ts
+++ react/unit/urban-stats-script-literal-parser.test.ts
@@ -22,3 +22,89 @@ void test('edit', () => {
         '{a: 1, b: 3}',
     )
 })
+
+void test('string', () => {
+    assert.equal(
+        l.string().parse(parseExpr('"hello"'), defaultConstants),
+        'hello',
+    )
+    assert.equal(
+        l.string().parse(parseExpr('123'), defaultConstants),
+        undefined,
+    )
+})
+
+void test('boolean', () => {
+    assert.equal(
+        l.boolean().parse(parseExpr('true'), defaultConstants),
+        true,
+    )
+    assert.equal(
+        l.boolean().parse(parseExpr('false'), defaultConstants),
+        false,
+    )
+    assert.equal(
+        l.boolean().parse(parseExpr('1'), defaultConstants),
+        undefined,
+    )
+})
+
+void test('number', () => {
+    assert.equal(
+        l.number().parse(parseExpr('42'), defaultConstants),
+        42,
+    )
+    assert.equal(
+        l.number().parse(parseExpr('-7'), defaultConstants),
+        -7,
+    )
+    assert.equal(
+        l.number().parse(parseExpr('"not a number"'), defaultConstants),
+        undefined,
+    )
+})
+
+void test('optional', () => {
+    const optNum = l.optional(l.number())
+    assert.equal(optNum.parse(undefined, defaultConstants), null)
+    assert.equal(optNum.parse(parseExpr('5'), defaultConstants), 5)
+    assert.equal(optNum.parse(parseExpr('"no"'), defaultConstants), undefined)
+})
+
+void test('vector', () => {
+    assert.deepEqual(
+        l.vector(l.number()).parse(parseExpr('[1, 2, 3]'), defaultConstants),
+        [1, 2, 3],
+    )
+    assert.equal(
+        l.vector(l.number()).parse(parseExpr('[1, "a"]'), defaultConstants),
+        undefined,
+    )
+})
+
+void test('call', () => {
+    const parser = l.call({
+        namedArgs: { foo: l.number() },
+        unnamedArgs: [l.string(), l.boolean()],
+    })
+    assert.deepEqual(
+        parser.parse(parseExpr('bar("hi", true, foo=7)'), defaultConstants),
+        { namedArgs: { foo: 7 }, unnamedArgs: ['hi', true] },
+    )
+    assert.equal(
+        parser.parse(parseExpr('bar("hi", foo="no", true)'), defaultConstants),
+        undefined,
+    )
+})
+
+void test('deconstruct', () => {
+    // This test assumes a constant with equivalentExpressions is present in defaultConstants
+    // If not, this will just return undefined
+    assert.deepEqual(
+        l.deconstruct(l.call({ namedArgs: {}, unnamedArgs: [l.number(), l.number(), l.number()] })).parse(parseExpr('colorBlue'), defaultConstants),
+        {
+            namedArgs: {},
+            unnamedArgs: [0.353, 0.49, 0.765],
+        },
+    )
+})
