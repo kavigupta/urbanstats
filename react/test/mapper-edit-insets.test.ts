a0ef24af323646c8c22676012ff4656d3a24641c
Luke Brody <luke@lukebrody.com>
2025-10-05 00:48:26 -0700

    Finish tests

--- react/test/mapper-edit-insets.test.ts
+++ react/test/mapper-edit-insets.test.ts
@@ -1,10 +1,15 @@
+import stableStringify from 'json-stable-stringify'
 import { ClientFunction, Selector } from 'testcafe'
 
 import { TestWindow } from '../src/utils/TestUtils'
 
-import { urbanstatsFixture, waitForLoading } from './test_utils'
+import { screencap, urbanstatsFixture, waitForLoading } from './test_utils'
 
 urbanstatsFixture(`default map`, '/mapper.html')
+
+function numMaps(): Promise<number> {
+    return Selector('[id^="map-"]').count
+}
 
 function map(n: number): string {
     return `[id^="map-${n}"]`
@@ -38,7 +43,8 @@ function frame(selector: string): Promise<Rect> {
     return ClientFunction(() => {
         const map0Rect = document.querySelector(map0)!.getBoundingClientRect()
         const domRect = document.querySelector(selector)!.getBoundingClientRect()
-        return { x: Math.round(domRect.x - map0Rect.x), y: Math.round(domRect.y - map0Rect.y), width: Math.round(domRect.width), height: Math.round(domRect.height) }
+        // Fudge Y since it's unstable
+        return { x: Math.round(domRect.x - map0Rect.x), y: Math.round((domRect.y - map0Rect.y) / 5) * 5, width: Math.round(domRect.width), height: Math.round(domRect.height / 5) * 5 }
     }, { dependencies: { selector, map0 } })()
 }
 
@@ -49,36 +55,136 @@ async function drag(t: TestController, selector: string, deltaX: number, deltaY:
     await t.dispatchEvent(selector, 'pointerdown', downEvent).dispatchEvent(selector, 'pointermove', upEvent).dispatchEvent(selector, 'pointerup', upEvent)
 }
 
-test('move and accept', async (t) => {
-    const beforeMoveFrame = { x: 0, y: 365, width: 163, height: 167 }
-    const move = { x: 0, y: -200 }
-    const afterMoveFrame = { ...beforeMoveFrame, x: beforeMoveFrame.x + move.x, y: beforeMoveFrame.y + move.y }
+async function wheel(t: TestController, selector: string, deltaY: number): Promise<void> {
+    return ClientFunction(() => {
+        const element = document.querySelector(`${selector} .maplibregl-canvas-container`)!
+        const elementRect = element.getBoundingClientRect()
+        const elementCenter = { clientX: (elementRect.left + elementRect.right) / 2, clientY: (elementRect.bottom + elementRect.top) / 2 }
+        element.dispatchEvent(new WheelEvent('wheel', { deltaY, ...elementCenter }))
+    }, { dependencies: { selector, deltaY } })()
+}
 
-    await waitForLoading(t)
-    await t.expect(frame(map(4))).eql(beforeMoveFrame)
+type MapPositions = { frame: Rect, bounds: Bounds }[]
+
+function insetsEditTest(testFn: () => TestFn, { description, action, before, after }: {
+    description: string
+    action: (t: TestController) => Promise<void>
+    before: MapPositions
+    after: MapPositions
+}): void {
+    for (const confirmation of ['Accept', 'Cancel'] as const) {
+        testFn()(`${description} then ${confirmation}`, async (t) => {
+            const check = async (positions: MapPositions): Promise<void> => {
+                let currentPositions
+                for (let iter = 0; iter < 3; iter++) {
+                    currentPositions = await Promise.all(Array.from({ length: await numMaps() }).map(async (_, i) => ({ frame: await frame(map(i)), bounds: await bounds(i) })))
+                    if (stableStringify(currentPositions) !== stableStringify(positions)) {
+                        console.warn('.')
+                        await t.wait(1000)
+                    }
+                    else {
+                        break
+                    }
+                }
+                if (stableStringify(currentPositions) !== stableStringify(positions)) {
+                    console.warn(currentPositions)
+                }
+                await t.expect(currentPositions).eql(positions)
+            }
+
+            await waitForLoading(t)
+            await check(before)
+            await t.click(Selector('button').withExactText('Edit Insets'))
+            await waitForLoading(t)
+            await check(before)
+            await action(t)
+            await check(after)
+            await t.expect(Selector('button:not(:disabled)').withExactText('Accept').exists).ok()
+            await t.wait(1000) // These waits are connected with the nonUserPanZoomOcurring hack
+            await t.pressKey('ctrl+z')
+            await check(before)
+            await t.expect(Selector('button:disabled').withExactText('Accept').exists).ok()
+            await t.wait(1000)
+            await t.pressKey('ctrl+y')
+            await check(after)
+            await t.wait(1000)
+            await t.click(Selector('button:not(:disabled)').withExactText(confirmation))
+
+            await waitForLoading(t)
+            if (confirmation === 'Accept') {
+                await check(after)
+            }
+            else {
+                await check(before)
+            }
+        })
+    }
+}
+
+const defaultUSA = [
+    {
+        frame: { x: 0, y: 0, width: 992, height: 530 },
+        bounds: { n: 50, e: -65, s: 23, w: -130 },
+    },
+    {
+        frame: { x: 914, y: 355, width: 78, height: 105 },
+        bounds: { n: 14, e: 145, s: 13, w: 144 },
+    },
+    {
+        frame: { x: 837, y: 460, width: 155, height: 70 },
+        bounds: { n: 19, e: -64, s: 17, w: -68 },
+    },
+    {
+        frame: { x: 163, y: 440, width: 161, height: 90 },
+        bounds: { n: 23, e: -153, s: 18, w: -162 },
+    },
+    {
+        frame: { x: 0, y: 365, width: 163, height: 165 },
+        bounds: { n: 72, e: -127, s: 51, w: -172 },
+    },
+]
+
+const move = { x: 200, y: -200 }
+const resize = 50
+
+insetsEditTest(() => test, {
+    description: 'move frame',
+    action: t => drag(t, handle(4, 'move'), move.x, move.y),
+    before: defaultUSA,
+    after: [...defaultUSA.slice(0, 4), { frame: { ...defaultUSA[4].frame, x: defaultUSA[4].frame.x + move.x, y: defaultUSA[4].frame.y + move.y }, bounds: defaultUSA[4].bounds }],
+})
+
+insetsEditTest(() => test, {
+    description: 'resize frame',
+    action: t => drag(t, handle(4, 'topRight'), resize, -resize),
+    before: defaultUSA,
+    after: [...defaultUSA.slice(0, 4), {
+        frame: {
+            x: defaultUSA[4].frame.x,
+            y: defaultUSA[4].frame.y - resize,
+            width: defaultUSA[4].frame.width + resize,
+            height: defaultUSA[4].frame.height + resize,
+        },
+        bounds: { n: 72, e: -126, s: 51, w: -172 },
+    }],
+})
+
+insetsEditTest(() => test, {
+    description: 'move bounds',
+    action: t => t.drag(map(0), 50, 50, { speed: 0.1 }),
+    before: defaultUSA,
+    after: [{ ...defaultUSA[0], bounds: { n: 52, e: -68, s: 26, w: -133 } }, ...defaultUSA.slice(1)],
+})
+
+insetsEditTest(() => test, {
+    description: 'zoom bounds',
+    action: t => wheel(t, map(3), -1000),
+    before: defaultUSA,
+    after: [...defaultUSA.slice(0, 3), { ...defaultUSA[3], bounds: { n: 23, e: -155, s: 21, w: -160 } }, ...defaultUSA.slice(4)],
+})
+
+test('edit interface', async (t) => {
     await t.click(Selector('button').withExactText('Edit Insets'))
     await waitForLoading(t)
-    await t.expect(frame(map(4))).eql(beforeMoveFrame)
-    await drag(t, handle(4, 'move'), move.x, move.y)
-    await t.expect(frame(map(4))).eql(afterMoveFrame)
-    await t.expect(Selector('button:not(:disabled)').withExactText('Accept').exists).ok()
-    await t.wait(1000) // These waits are connected with the nonUserPanZoomOcurring hack
-    await t.pressKey('ctrl+z')
-    await t.expect(frame(map(4))).eql(beforeMoveFrame)
-    await t.expect(Selector('button:disabled').withExactText('Accept').exists).ok()
-    await t.wait(1000)
-    await t.pressKey('ctrl+y')
-    await t.expect(frame(map(4))).eql(afterMoveFrame)
-    await t.click(Selector('button:not(:disabled)').withExactText('Accept'))
-
-    await waitForLoading(t)
-    await t.expect(frame(map(4))).eql(afterMoveFrame)
+    await screencap(t)
 })
-
-test('move bounds', async (t) => {
-    await waitForLoading(t)
-    await t.expect(bounds(0)).eql({ n: 50, e: -65, s: 23, w: -130 })
-    await t.drag(map(0), 100, 100)
-    await t.expect(bounds(0)).notEql({ n: 50, e: -65, s: 23, w: -130 })
-})
-// How to test bounds?
