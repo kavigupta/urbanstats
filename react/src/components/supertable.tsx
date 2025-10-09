f7d4856e50010a56a319493fccf53a6c62f295eb
Luke Brody <luke@lukebrody.com>
2025-10-08 22:04:06 -0700

    Fix errors

--- react/src/components/supertable.tsx
+++ react/src/components/supertable.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, ReactNode } from 'react'
+import React, { CSSProperties, Fragment, ReactNode } from 'react'
 
 import { Article } from '../utils/protos'
 
@@ -127,10 +127,10 @@ export function SuperTableRow(props: {
             <TableRowContainer index={props.rowIndex} minHeight={props.rowMinHeight}>
                 <Cell {...props.leftHeaderSpec} width={props.widthLeftHeader} />
                 {props.cellSpecs.map((spec, colIndex) => (
-                    <>
-                        <Cell key={`rowCells_${colIndex}_${props.rowIndex}`} {...spec} width={props.columnWidth} />
-                        <div key={`spacer_${colIndex}_${props.rowIndex}`} style={{ width: `${props.extraSpaceRight[colIndex]}%` }}></div>
-                    </>
+                    <Fragment key={`cells_${colIndex}_${props.rowIndex}`}>
+                        <Cell {...spec} width={props.columnWidth} />
+                        <div style={{ width: `${props.extraSpaceRight[colIndex]}%` }}></div>
+                    </Fragment>
                 ))}
             </TableRowContainer>
             {props.plotSpec && (
