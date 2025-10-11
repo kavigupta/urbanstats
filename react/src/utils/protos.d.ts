8b45a865b8c5a68a582285320bedf584d661fe07
Kavi Gupta <kavi@berkeley.edu>
2025-10-11 15:24:17 -0700

    add metadata columns to the model

--- react/src/utils/protos.d.ts
+++ react/src/utils/protos.d.ts
@@ -851,6 +851,112 @@ export class ExtraStatistic implements IExtraStatistic {
     public static getTypeUrl(typeUrlPrefix?: string): string;
 }
 
+/** Properties of a Metadata. */
+export interface IMetadata {
+
+    /** Metadata metadataIndex */
+    metadataIndex?: (number|null);
+
+    /** Metadata stringValue */
+    stringValue?: (string|null);
+}
+
+/** Represents a Metadata. */
+export class Metadata implements IMetadata {
+
+    /**
+     * Constructs a new Metadata.
+     * @param [properties] Properties to set
+     */
+    constructor(properties?: IMetadata);
+
+    /** Metadata metadataIndex. */
+    public metadataIndex: number;
+
+    /** Metadata stringValue. */
+    public stringValue?: (string|null);
+
+    /** Metadata _stringValue. */
+    public _stringValue?: "stringValue";
+
+    /**
+     * Creates a new Metadata instance using the specified properties.
+     * @param [properties] Properties to set
+     * @returns Metadata instance
+     */
+    public static create(properties?: IMetadata): Metadata;
+
+    /**
+     * Encodes the specified Metadata message. Does not implicitly {@link Metadata.verify|verify} messages.
+     * @param message Metadata message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encode(message: IMetadata, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Encodes the specified Metadata message, length delimited. Does not implicitly {@link Metadata.verify|verify} messages.
+     * @param message Metadata message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encodeDelimited(message: IMetadata, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Decodes a Metadata message from the specified reader or buffer.
+     * @param reader Reader or buffer to decode from
+     * @param [length] Message length if known beforehand
+     * @returns Metadata
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): Metadata;
+
+    /**
+     * Decodes a Metadata message from the specified reader or buffer, length delimited.
+     * @param reader Reader or buffer to decode from
+     * @returns Metadata
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): Metadata;
+
+    /**
+     * Verifies a Metadata message.
+     * @param message Plain object to verify
+     * @returns `null` if valid, otherwise the reason why it is not
+     */
+    public static verify(message: { [k: string]: any }): (string|null);
+
+    /**
+     * Creates a Metadata message from a plain object. Also converts values to their respective internal types.
+     * @param object Plain object
+     * @returns Metadata
+     */
+    public static fromObject(object: { [k: string]: any }): Metadata;
+
+    /**
+     * Creates a plain object from a Metadata message. Also converts values to other types if specified.
+     * @param message Metadata
+     * @param [options] Conversion options
+     * @returns Plain object
+     */
+    public static toObject(message: Metadata, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+    /**
+     * Converts this Metadata to JSON.
+     * @returns JSON object
+     */
+    public toJSON(): { [k: string]: any };
+
+    /**
+     * Gets the default type url for Metadata
+     * @param [typeUrlPrefix] your custom typeUrlPrefix(default "type.googleapis.com")
+     * @returns The default type url
+     */
+    public static getTypeUrl(typeUrlPrefix?: string): string;
+}
+
 /** Properties of an Article. */
 export interface IArticle {
 
@@ -883,6 +989,9 @@ export interface IArticle {
 
     /** Article extraStats */
     extraStats?: (IExtraStatistic[]|null);
+
+    /** Article metadata */
+    metadata?: (IMetadata[]|null);
 }
 
 /** Represents an Article. */
@@ -923,6 +1032,9 @@ export class Article implements IArticle {
 
     /** Article extraStats. */
     public extraStats: IExtraStatistic[];
+
+    /** Article metadata. */
+    public metadata: IMetadata[];
 
     /**
      * Creates a new Article instance using the specified properties.
