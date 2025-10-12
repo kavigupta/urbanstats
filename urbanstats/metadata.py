db12e08dd9b9405bd16e717db659d19497a66e67
Kavi Gupta <kavi@berkeley.edu>
2025-10-12 16:55:04 -0700

    fix

--- urbanstats/metadata.py
+++ urbanstats/metadata.py
@@ -1,7 +1,31 @@
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+from urbanstats.protobuf import data_files_pb2
+
+class MetadataColumn(ABC):
+    @abstractmethod
+    def create(self, value):
+        pass
+
+@dataclass
+class DisplayedMetadata:
+    typ: type
+
+    def create(self, idx, value):
+        assert isinstance(value, self.typ), f"Expected {self.typ}, got {type(value)} for value {value}"
+        assert self.typ == str
+        return data_files_pb2.Metadata(metadata_index=idx, string_value=value)
+
+@dataclass
+class ExternalLinkMetadata:
+    def create(self, idx, value):
+        return data_files_pb2.Metadata(metadata_index=idx, string_value=value)
+
 metadata_types = {
-    "geoid": "str",
-    "scgc": "str",
-    "wikidata": "str",
-    "wikipedia_page": "str",
-    "iso": "str",
+    "geoid": DisplayedMetadata(str),
+    "scgc": DisplayedMetadata(str),
+    "wikidata": ExternalLinkMetadata(),
+    "wikipedia_page": ExternalLinkMetadata(),
+    "iso": DisplayedMetadata(str),
 }
