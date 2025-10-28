21d6974b42c83ae334501e8418aa9b1c30d1ad20
Luke Brody <luke@lukebrody.com>
2025-10-28 03:40:22 -0700

    hacking

--- react/src/components/QuillEditor.tsx
+++ react/src/components/QuillEditor.tsx
@@ -1,66 +1,23 @@
 import katex from 'katex'
 // eslint-disable-next-line import/no-named-as-default, import/default -- These don't like the import
-import Quill, { Delta, EmitterSource, Parchment, Range } from 'quill'
+import Quill, { Delta, EmitterSource, Range } from 'quill'
 import Block from 'quill/blots/block'
-import React, { CSSProperties, ReactNode, useEffect, useLayoutEffect, useRef, useState } from 'react'
+import React, { CSSProperties, ReactNode, useEffect, useLayoutEffect, useRef } from 'react'
 
 import 'katex/dist/katex.css'
 import 'quill/dist/quill.snow.css'
-import { createPortal } from 'react-dom'
 
 // Needed for formula module
 (window as { katex: unknown }).katex = katex
 
-const fonts = [
-    { family: 'jost' },
-    { family: 'times new roman', displaySize: 10 },
-]
-
-const fontAttributor = Quill.import('attributors/style/font') as Parchment.Attributor
-fontAttributor.whitelist = fonts.map(f => f.family)
-Quill.register(fontAttributor, true)
-
-function FontStyles(): ReactNode {
-    return (
-        <style>
-            {fonts.map(({ family, displaySize }) => {
-                const capitalizedFont = family.replaceAll(/\b\w/g, char => char.toUpperCase())
-                return `/* Set dropdown font-families */
-.ql-snow .ql-picker.ql-font .ql-picker-label[data-value="${family}"]::before,
-.ql-snow .ql-picker.ql-font .ql-picker-item[data-value="${family}"]::before{
-  font-family: "${family}";
-  content: "${capitalizedFont}";
-  ${displaySize !== undefined ? `font-size: ${displaySize}px;` : ''}
-}
-/* Set effect font-families */
-.ql-font-${family} {
-  font-family: "${family}";
-}`
-            })}
-        </style>
-    )
-}
-
-const sizeAttributor = Quill.import('attributors/style/size') as Parchment.Attributor
-sizeAttributor.whitelist = ['10px', '12px', '14px', '16px', '18px', '20px', '24px', '30px', '36px', '48px', '72px', '96px']
-Quill.register(sizeAttributor, true)
-
-function SizeStyles(): ReactNode {
-    return (
-        <style>
-            {['size', 'borderWidth'].map(control => `.ql-snow .ql-picker.ql-${control} .ql-picker-label[data-value]::before,
-.ql-snow .ql-picker.ql-${control} .ql-picker-item[data-value]::before {
-  content: attr(data-value) !important;
-}
-.ql-picker.ql-${control} { width: 60px !important }`)}
-        </style>
-    )
-}
-
 function DefaultStyle(): ReactNode {
     return (
         <style>
-            {` .ql-editor { font-family: jost; font-size: 16px; }`}
+            {`
+.ql-editor { font-family: jost; font-size: 16px; }
+.ql-container.ql-snow { border: none; }
+.ql-toolbar.ql-snow { border: none; }
+`}
         </style>
     )
 }
@@ -82,19 +39,14 @@ export interface QuillEditorProps {
     onTextChange: (delta: Delta) => void
     onSelectionChange: (range: Range | null) => void
     containerStyle?: CSSProperties
-    backgroundColor?: string
-    border?: { color: string, width: number }
-    customControls?: ReactNode
+    toolbar: string | false
+    quillRef: React.MutableRefObject<Quill | undefined>
 }
 
-export function QuillEditor({ editable, content, selection, onTextChange, onSelectionChange, containerStyle, backgroundColor, border, customControls }: QuillEditorProps): ReactNode {
-    const quillRef = useRef<Quill>()
-
+export function QuillEditor({ editable, content, selection, onTextChange, onSelectionChange, containerStyle, toolbar, quillRef }: QuillEditorProps): ReactNode {
     const containerRef = useRef<HTMLDivElement>(null)
     const onTextChangeRef = useRef(onTextChange)
     const onSelectionChangeRef = useRef(onSelectionChange)
-
-    const [customControlsContainer, setCustomControlsContainer] = useState<Element | null>(null)
 
     useLayoutEffect(() => {
         onTextChangeRef.current = onTextChange
@@ -112,26 +64,7 @@ export function QuillEditor({ editable, content, selection, onTextChange, onSele
                 history: {
                     maxStack: 0, // We use our own undo manager
                 },
-                toolbar: editable && {
-                    container: [
-                        [{ font: fontAttributor.whitelist }, { size: sizeAttributor.whitelist }],
-
-                        ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-
-                        [{ align: [] }],
-                        [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-
-                        ['link', 'image', 'formula'],
-
-                        [], // This is a reference point to put custom controls
-                        [{ borderWidth: ['0px', '1px', '2px', '3px', '4px', '5px'] }],
-                    ],
-                    handlers: {
-                        borderWidth: (width: string) => {
-                            console.log(Number(width.slice(0, width.length - 2)))
-                        },
-                    },
-                },
+                toolbar,
             },
         })
 
@@ -157,48 +90,28 @@ export function QuillEditor({ editable, content, selection, onTextChange, onSele
                     break
             }
         })
-
-        quill.enable(editable)
-
-        setCustomControlsContainer(container.querySelector('.ql-formats:not(:has(*))'))
-
         return () => {
             quillRef.current = undefined
             container.innerHTML = ''
-            setCustomControlsContainer(null)
         }
-    }, [editable])
+    }, [toolbar, quillRef])
+
+    useEffect(() => {
+        quillRef.current?.enable(editable)
+    }, [editable, quillRef])
 
     useEffect(() => {
         quillRef.current?.updateContents(quillRef.current.getContents().diff(content), 'api')
-    }, [content])
+    }, [content, quillRef])
 
     useEffect(() => {
         quillRef.current?.setSelection(selection, 'api')
-    }, [selection])
-
-    useEffect(() => {
-        const editor = containerRef.current?.querySelector<HTMLDivElement>('.ql-editor')
-        if (editor) {
-            editor.style.backgroundColor = backgroundColor ?? ''
-        }
-        const toolbar = containerRef.current?.querySelector<HTMLDivElement>('.ql-toolbar')
-        if (toolbar) {
-            toolbar.style.backgroundColor = backgroundColor ? `${backgroundColor}aa` : ''
-        }
-        const container = containerRef.current?.querySelector<HTMLDivElement>('.ql-container')
-        if (container) {
-            container.style.border = border ? `${border.width}px solid ${border.color}` : ''
-        }
-    })
+    }, [selection, quillRef])
 
     return (
         <>
             <div ref={containerRef} style={containerStyle}></div>
-            <FontStyles />
-            <SizeStyles />
             <DefaultStyle />
-            {customControlsContainer ? createPortal(customControls, customControlsContainer) : null}
         </>
     )
 }
--- react/src/mapper/components/MapLabel.tsx
+++ react/src/mapper/components/MapLabel.tsx
@@ -1,10 +1,12 @@
-import { Range } from 'quill'
-import React, { createContext, ReactNode, RefObject, useContext, useEffect, useRef, useState } from 'react'
-import { flushSync } from 'react-dom'
+// eslint-disable-next-line import/no-named-as-default, import/default -- These don't like the import
+import Color from 'color'
+import Quill, { Parchment, Range } from 'quill'
+import React, { createContext, ReactNode, RefObject, useContext, useEffect, useId, useRef, useState } from 'react'
 
 import { QuillEditor } from '../../components/QuillEditor'
 import { fromQuillDelta, Label, toQuillDelta } from '../../urban-stats-script/constants/label'
 import { Property } from '../../utils/Property'
+import { BetterSelector } from '../settings/BetterSelector'
 
 import { EditInsetsHandles } from './InsetMap'
 
@@ -27,27 +29,27 @@ export function MapLabel({ label, container, editLabel, i, numLabels }: {
 
     const divRef = useRef<HTMLDivElement>(null)
 
-    const [toolbarHeight, setToolbarHeight] = useState(0)
+    const toolbarId = useId()
+
+    const quillRef = useRef<Quill>()
+
+    const [format, setFormat] = useState<Record<string, unknown>>({})
 
     useEffect(() => {
-        const toolbar = divRef.current!.querySelector<HTMLDivElement>('.ql-toolbar')
-
-        if (toolbar === null) {
-            return // not editing
+        const quill = quillRef.current!
+        const listener = (): void => {
+            const currentSelection = quill.getSelection(false)
+            if (currentSelection !== null) {
+                const currentFormat = quill.getFormat(currentSelection.index, currentSelection.length)
+                setFormat(currentFormat)
+            }
         }
-
-        const observer = new ResizeObserver(() => {
-            flushSync(() => {
-                setToolbarHeight(toolbar.offsetHeight)
-            })
-        })
-        setToolbarHeight(toolbar.offsetHeight)
-        observer.observe(toolbar)
+        listener()
+        quill.on('editor-change', listener)
         return () => {
-            observer.unobserve(toolbar)
+            quill.off('editor-change', listener)
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- Only needs to change when the toolbar appears/disappears
-    }, [!!editLabel])
+    }, [quillRef])
 
     return (
         <div
@@ -59,6 +61,7 @@ export function MapLabel({ label, container, editLabel, i, numLabels }: {
             ref={divRef}
         >
             <QuillEditor
+                quillRef={quillRef}
                 editable={!!editLabel}
                 content={toQuillDelta(label.text)}
                 onTextChange={(delta) => {
@@ -73,16 +76,65 @@ export function MapLabel({ label, container, editLabel, i, numLabels }: {
                         selectionProperty.value = undefined
                     }
                 }}
-                containerStyle={{ transform: editLabel ? `translateY(-${toolbarHeight}px)` : undefined, width: '100%', height: '100%' }}
-                backgroundColor={label.backgroundColor}
-                border={{ width: label.borderWidth, color: label.borderColor }}
-                customControls={(
-                    <>
-                        <QuillColorPicker label="bg" color={label.backgroundColor} setColor={newColor => editLabel?.modify({ backgroundColor: newColor })} />
-                        <QuillColorPicker label="border" color={label.borderColor} setColor={newColor => editLabel?.modify({ borderColor: newColor })} />
-                    </>
-                )}
+                containerStyle={{ width: '100%', height: '100%', backgroundColor: label.backgroundColor, border: `${label.borderWidth}px solid ${label.borderColor}` }}
+                toolbar={`#${CSS.escape(toolbarId)}`}
             />
+            <div
+                id={toolbarId}
+                style={{
+                    position: 'absolute',
+                    bottom: '100%',
+                    backgroundColor: `${label.backgroundColor}aa`,
+                    border: `${label.borderWidth}px solid ${label.borderColor}`,
+                    borderRadius: '5px 5px 0 0',
+                }}
+            >
+                <span className="ql-formats">
+                    <select className="ql-font">
+                        {fonts.map(({ family }) => <option key={family} value={family} />)}
+                    </select>
+                    <FontStyles />
+                    <select className="ql-size">
+                        {sizes.map(size => <option key={size} value={size} />)}
+                    </select>
+                    <SizeStyles />
+                </span>
+                <span className="ql-formats">
+                    <button className="ql-bold"></button>
+                    <button className="ql-italic"></button>
+                    <button className="ql-underline"></button>
+                    <button className="ql-strike"></button>
+                </span>
+                <span className="ql-formats">
+                    <select className="ql-align"></select>
+                </span>
+                <span className="ql-formats">
+                    <ColorButton label={<span style={{ fontSize: '20px' }}>𝑨</span>} color={format.color as (string | undefined) ?? '#000000'} setColor={c => quillRef.current?.format('color', c, 'user')} />
+                </span>
+                <span className="ql-formats">
+                    <button className="ql-link"></button>
+                    <button className="ql-image"></button>
+                    <button className="ql-formula"></button>
+                </span>
+                <span className="ql-formats">
+                    <ColorButton label={<span style={{ fontSize: '12px' }}>bg</span>} color={label.backgroundColor} setColor={c => editLabel?.modify({ backgroundColor: c })} />
+                </span>
+                <span className="ql-formats">
+                    <ColorButton label={<span style={{ fontSize: '12px' }}>border</span>} color={label.borderColor} setColor={c => editLabel?.modify({ borderColor: c })} />
+                    <select
+                        className="borderWidth"
+                        value={label.borderWidth}
+                        onChange={(e) => {
+                            editLabel?.modify({ borderWidth: Number(e.target.value.slice(0, e.target.value.length - 2)) })
+                        }}
+                    >
+                        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(n => (
+                            <option key={n} value={`${n}px`} />
+                        ))}
+                    </select>
+                </span>
+            </div>
+
             { editLabel && (
                 <EditInsetsHandles
                     frame={[...label.bottomLeft, ...label.topRight]}
@@ -114,20 +166,77 @@ export interface Selection {
 // eslint-disable-next-line no-restricted-syntax -- React context
 export const SelectionContext = createContext(new Property<Selection | undefined>(undefined))
 
-function QuillColorPicker({ color, setColor, label }: { color: string, setColor: (c: string) => void, label: string }): ReactNode {
+const fonts = [
+    { family: 'jost' },
+    { family: 'times new roman', displaySize: 10 },
+]
+
+const fontAttributor = Quill.import('attributors/style/font') as Parchment.Attributor
+fontAttributor.whitelist = fonts.map(f => f.family)
+Quill.register(fontAttributor, true)
+
+function FontStyles(): ReactNode {
     return (
-        <span className="ql-picker">
-            <span className="ql-picker-label serif" onClick={(e) => { (e.target as HTMLSpanElement).querySelector('input')!.click() }}>
-                {`${label} `}
-                <input
-                    type="color"
-                    value={color}
-                    onChange={(e) => {
-                        setColor(e.target.value)
-                    }}
-                    style={{ width: '20px', height: '20px' }}
-                />
-            </span>
-        </span>
+        <style>
+            {fonts.map(({ family, displaySize }) => {
+                const capitalizedFont = family.replaceAll(/\b\w/g, char => char.toUpperCase())
+                return `/* Set dropdown font-families */
+.ql-snow .ql-picker.ql-font .ql-picker-label[data-value="${family}"]::before,
+.ql-snow .ql-picker.ql-font .ql-picker-item[data-value="${family}"]::before{
+  font-family: "${family}";
+  content: "${capitalizedFont}";
+  ${displaySize !== undefined ? `font-size: ${displaySize}px;` : ''}
+}
+/* Set effect font-families */
+.ql-font-${family} {
+  font-family: "${family}";
+}`
+            })}
+        </style>
+    )
+}
+
+const sizes = ['10px', '12px', '14px', '16px', '18px', '20px', '24px', '30px', '36px', '48px', '72px', '96px']
+
+const sizeAttributor = Quill.import('attributors/style/size') as Parchment.Attributor
+sizeAttributor.whitelist = sizes
+Quill.register(sizeAttributor, true)
+
+function SizeStyles(): ReactNode {
+    return (
+        <style>
+            {['ql-size', 'borderWidth'].map(control => `.ql-snow .ql-picker.${control} .ql-picker-label[data-value]::before,
+.ql-snow .ql-picker.${control} .ql-picker-item[data-value]::before {
+  content: attr(data-value) !important;
+}
+.ql-picker.ql-${control} { width: 60px !important }`)}
+        </style>
+    )
+}
+
+function ColorButton({ label, color, setColor }: { label: ReactNode, color: string, setColor: (c: string) => void }): ReactNode {
+    return (
+        <button
+            style={{ display: 'flex', gap: '2px', alignItems: 'center', width: 'fit-content' }}
+            onClick={(e) => {
+                (e.target as Element).querySelector('input')?.click()
+            }}
+            tabIndex={-1}
+        >
+            <span style={{ pointerEvents: 'none' }}>{label}</span>
+            <input
+                type="color"
+                value={color}
+                onChange={(e) => {
+                    setColor(e.target.value)
+                }}
+                style={{
+                    border: 0,
+                    padding: 0,
+                    height: '125%',
+                    width: '25px',
+                }}
+            />
+        </button>
     )
 }
