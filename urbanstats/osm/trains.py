edf26dbe62b625a164968f6b2deda448a0ac2440
Kavi Gupta <kavi@berkeley.edu>
2025-11-01 17:43:28 -0700

    updates

--- urbanstats/osm/trains.py
+++ urbanstats/osm/trains.py
@@ -13,6 +13,7 @@ import requests
 import pandas as pd
 import tqdm.auto as tqdm
 
+from urbanstats.features.within_distance import haversine
 from urbanstats.geometry.ellipse import Ellipse
 
 from .query import query_to_geopandas
@@ -582,26 +583,100 @@ def stops_covered_by_valid_trips(gtfs, invalid_route_types) -> Set[str]:
     return covered_stops
 
 
-def pull_stops_for_gtfs(gtfs, invalid_route_types) -> Tuple[pd.DataFrame, Dict[str, str]]:
+def clean_up_parents(stops: pd.DataFrame) -> pd.DataFrame:
+    if "parent_station" not in stops.columns:
+        return stops
+    assert stops.stop_id.apply(
+        lambda x: str(x).strip() != ""
+    ).all(), "Empty stop_id found"
+    stops["parent_station"] = stops["parent_station"].apply(
+        lambda x: x.strip() if isinstance(x, str) and x.strip() != "" else np.nan
+    )
+    return stops
+
+
+def pull_stops_for_gtfs(
+    gtfs, invalid_route_types
+) -> Tuple[pd.DataFrame, Dict[str, str]]:
     stops = pull_file_from_gtfs(gtfs, "stops.txt")
+    stops = clean_up_parents(stops)
     assert stops is not None, "stops.txt is missing"
     covered_stops = stops_covered_by_valid_trips(gtfs, invalid_route_types)
     referenced_stops = stops[stops["stop_id"].isin(covered_stops)].copy()
     remap = {}
     if "parent_station" in referenced_stops.columns:
+        mask = referenced_stops["parent_station"].apply(
+            lambda p: p == p and p in referenced_stops["stop_id"].values
+        )
         with_parent, without_parent = (
-            referenced_stops[~referenced_stops.parent_station.isna()].copy(),
-            referenced_stops[referenced_stops.parent_station.isna()].copy(),
+            referenced_stops.loc[mask].copy(),
+            referenced_stops.loc[~mask].copy(),
         )
         original, parent = with_parent["stop_id"], with_parent["parent_station"]
         remap = dict(zip(original, parent))
-        referenced_stops = pd.concat([
-            without_parent,
-            stops[stops.stop_id.isin(parent.unique())],
-        ])
+        referenced_stops = pd.concat(
+            [
+                without_parent,
+                stops[stops.stop_id.isin(parent.unique())],
+            ]
+        )
+    referenced_stops, remap = deduplicate_stops(referenced_stops, remap)
     return referenced_stops, remap
 
-def pull_stops_for_gtfs_arrays(gtfs, invalid_route_types, start_idx) -> Tuple[np.ndarray, np.ndarray, np.ndarray, Dict[str, int]]:
+
+def shatter_clusters_by_distance(
+    lons: pd.Series,
+    lats: pd.Series,
+    clustered_indices: List[List[int]],
+    max_distance_m,
+) -> List[List[int]]:
+    lons, lats = np.array(lons), np.array(lats)
+    shattered = []
+    for cluster in clustered_indices:
+        edges = compute_stop_graph_within_radius(
+            max_distance_m / 1000, lons[cluster], lats[cluster]
+        )
+        components = connected_components(edges, len(lons[cluster]))
+        components = [x for x in components if len(x) > 1]
+        components = [[cluster[i] for i in comp] for comp in components]
+        shattered.extend(components)
+    return shattered
+
+
+def deduplicate_stops(
+    stops: pd.DataFrame, remap: Dict[str, str]
+) -> Tuple[pd.DataFrame, Dict[str, str]]:
+    stops = stops.reset_index(drop=True)
+    clustered_indices = defaultdict(list)
+    for idx, stop_name in zip(stops.index, stops["stop_name"]):
+        clustered_indices[stop_name].append(idx)
+    clustered_indices = [sorted(v) for k, v in clustered_indices.items() if len(v) > 1]
+    clustered_indices = shatter_clusters_by_distance(
+        stops.stop_lon, stops.stop_lat, clustered_indices, max_distance_m=250
+    )
+    new_rows = []
+    remove_indices = []
+    for indices in clustered_indices:
+        cluster = stops.loc[indices]
+        mean_lat = cluster["stop_lat"].mean()
+        mean_lon = cluster["stop_lon"].mean()
+        row_exemplar = cluster.loc[indices[0]].copy()
+        row_exemplar["stop_lat"] = mean_lat
+        row_exemplar["stop_lon"] = mean_lon
+        new_rows.append(row_exemplar)
+        exemplar_id = row_exemplar["stop_id"]
+        for idx in indices:
+            if stops.at[idx, "stop_id"] != exemplar_id:
+                remap[stops.at[idx, "stop_id"]] = exemplar_id
+        remove_indices.extend(indices)
+    stops = stops.drop(index=remove_indices)
+    stops = pd.concat([stops, pd.DataFrame(new_rows)], ignore_index=True)
+    return stops, remap
+
+
+def pull_stops_for_gtfs_arrays(
+    gtfs, invalid_route_types, start_idx
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, Dict[str, int]]:
     stops, remap_parent = pull_stops_for_gtfs(gtfs, invalid_route_types)
     names, lats, lons = [], [], []
     remap = {}
@@ -629,48 +704,37 @@ def pull_stops_for_gtfs_arrays(gtfs, invalid_route_types, start_idx) -> Tuple[np
         remap,
     )
 
+
 def standardized_stops():
-    all_stops = []
-    remap_by_agency = {}
-    for res in all_gtfs_info():
-        r = res["gtfs_result"]()
+    names, feed_ids, lats, lons = [], [], [], []
+    remaps = {}
+    start_idx = 0
+    for gtfs_info in all_gtfs_info():
+        r = gtfs_info["gtfs_result"]()
         if r["status"] != "success":
             continue
-        gtfs = r["content"]
-        stops = pull_file_from_gtfs(gtfs, "stops.txt")
-        if stops is None:
-            continue
-        if "parent_station" in stops.columns:
-            without_parent = stops[stops.parent_station.isna()].copy()
-            with_parent = stops[~stops.parent_station.isna()].copy()
-        else:
-            without_parent = stops
-            with_parent = {"stop_id": [], "parent_station": []}
-        remap = {}
-        for stop_id, stop_name, stop_lat, stop_lon in zip(
-            without_parent["stop_id"],
-            without_parent["stop_name"],
-            without_parent["stop_lat"],
-            without_parent["stop_lon"],
-        ):
-            stop_lat, stop_lon = parse_float(stop_lat), parse_float(stop_lon)
-            if (
-                stop_lat is None
-                or stop_lon is None
-                or np.isnan(stop_lat)
-                or np.isnan(stop_lon)
-            ):
-                continue
-            all_stops.append((stop_name, stop_lat, stop_lon))
-            remap[stop_id] = len(all_stops) - 1
-        for stop_id, parent_station in zip(
-            with_parent["stop_id"], with_parent["parent_station"]
-        ):
-            remap[stop_id] = remap[parent_station]
-        remap_by_agency[res["feed"]["id"]] = remap
-    names, lats, lons = zip(*all_stops)
-    names, lats, lons = np.array(names), np.array(lats), np.array(lons)
-    return names, lats, lons, remap_by_agency
+        (
+            names_array,
+            lats_array,
+            lons_array,
+            remap,
+        ) = pull_stops_for_gtfs_arrays(
+            r["content"], is_bus_or_ferry_route_type, start_idx
+        )
+        names.append(names_array)
+        feed_ids.extend([gtfs_info["feed"]["id"]] * len(names_array))
+        lats.append(lats_array)
+        lons.append(lons_array)
+        for k, v in remap.items():
+            remaps[gtfs_info["feed"]["id"], k] = v
+        start_idx += len(names_array)
+    return (
+        np.concatenate(names),
+        np.array(feed_ids),
+        np.concatenate(lats),
+        np.concatenate(lons),
+        remaps,
+    )
 
 
 def compute_stop_graph_within_radius(
@@ -680,7 +744,7 @@ def compute_stop_graph_within_radius(
     lats_in_order = lat[indices]
     lons_in_order = lon[indices]
     edges = []
-    for i in tqdm.trange(len(lat), desc="Computing stop graph"):
+    for i in tqdm.trange(len(lat), desc="Computing stop graph", delay=10):
         ellipse = Ellipse(radius_in_km, lats_in_order[i], lons_in_order[i])
         # only look behind, since the graph is undirected
         start_i = np.searchsorted(
