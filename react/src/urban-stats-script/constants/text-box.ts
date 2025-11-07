f705ff5c3a760fe6035691839125e287dc97fc89
Luke Brody <luke@lukebrody.com>
2025-11-07 00:57:23 -0800

    Revert "Remove text box uss"
    This reverts commit 860a394eb82921067408f924fa03d8429faae8aa.

---
+++ react/src/urban-stats-script/constants/text-box.ts
@@ -0,0 +1,141 @@
+import { colorThemes } from '../../page_template/color-themes'
+import { UrbanStatsASTExpression } from '../ast'
+import { Context } from '../context'
+import { parseNoErrorAsExpression } from '../parser'
+import { USSRawValue, USSType, USSValue } from '../types-values'
+
+import { colorType } from './color'
+import { Color, deconstructColor, hexToColor } from './color-utils'
+import { boundsType } from './insets'
+import { alignValueToIdentifer, listValueToIdentifier, RichTextDocument, richTextDocumentType, RichTextSegment } from './rich-text'
+
+export interface TextBox {
+    bottomLeft: [number, number]
+    topRight: [number, number]
+    text: RichTextDocument
+    backgroundColor: Color
+    borderColor: Color
+    borderWidth: number
+}
+
+export const defaults = {
+    backgroundColor: hexToColor(colorThemes['Light Mode'].background),
+    borderColor: hexToColor(colorThemes['Light Mode'].borderShadow),
+    borderWidth: 1,
+}
+
+export function deconstruct(textBox: TextBox): UrbanStatsASTExpression {
+    const uss = `textBox(
+        screenBounds={
+            north: ${textBox.topRight[1]},
+            east: ${textBox.topRight[0]},
+            south: ${textBox.bottomLeft[1]},
+            west: ${textBox.bottomLeft[0]}
+        },
+        text=${deconstructRichTextDocument(textBox.text)}
+    )`
+    return parseNoErrorAsExpression(uss, '')
+}
+
+function deconstructRichTextDocument(doc: RichTextDocument): string {
+    return `rtfDocument([${doc.map(deconstructRichTextSegment).join(', ')}])`
+}
+
+function deconstructRichTextSegment(segment: RichTextSegment): string {
+    if (typeof segment.insert === 'string') {
+        return `rtfString(${JSON.stringify(segment.insert)}${deconstructRichTextAttributes(segment.attributes)})`
+    }
+    if ('formula' in segment.insert) {
+        return `rtfFormula("${segment.insert.formula}"${deconstructRichTextAttributes(segment.attributes)})`
+    }
+    if ('image' in segment.insert) {
+        return `rtfImage("${segment.insert.image}"${deconstructRichTextAttributes(segment.attributes)})`
+    }
+    throw new Error()
+}
+
+function deconstructRichTextAttributes(attributes: RichTextSegment['attributes']): string {
+    if (attributes === undefined) {
+        return ''
+    }
+    const list = Object.entries(attributes).filter(([, value]) => value !== undefined)
+    if (list.length === 0) {
+        return ''
+    }
+    return `, ${list.flatMap((pair) => {
+        if (pair[1] === undefined) {
+            return []
+        }
+
+        const value = (() => {
+            switch (pair[0]) {
+                case 'align':
+                    return [alignValueToIdentifer[pair[1]]]
+                case 'list':
+                    return [listValueToIdentifier[pair[1]]]
+                case 'color':
+                    return deconstructColor(pair[1])
+                case 'bold':
+                case 'italic':
+                case 'underline':
+                    return pair[1] ? 'true' : 'false'
+                case 'size':
+                case 'indent':
+                    return pair[1].toString()
+                case 'font':
+                    return `"${pair[1]}"`
+            }
+        })()
+
+        return `${pair[0]}=${value}`
+    }).join(', ')}`
+}
+
+export const textBoxType = {
+    type: 'opaque',
+    name: 'textBox',
+} satisfies USSType
+
+export const constructTextBoxValue: USSValue = {
+    type: {
+        type: 'function',
+        posArgs: [],
+        namedArgs: {
+            screenBounds: { type: { type: 'concrete', value: boundsType } },
+            text: { type: { type: 'concrete', value: richTextDocumentType } },
+            backgroundColor: { type: { type: 'concrete', value: colorType }, defaultValue: parseNoErrorAsExpression(deconstructColor(defaults.backgroundColor), '') },
+            borderColor: { type: { type: 'concrete', value: colorType }, defaultValue: parseNoErrorAsExpression(deconstructColor(defaults.borderColor), '') },
+            borderWidth: { type: { type: 'concrete', value: { type: 'number' } }, defaultValue: parseNoErrorAsExpression(`${defaults.borderWidth}`, '') },
+        },
+        returnType: { type: 'concrete', value: textBoxType },
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars -- needed for USSValue interface
+    value: (ctx: Context, posArgs: USSRawValue[], namedArgs: Record<string, USSRawValue>): USSRawValue => {
+        const screenBounds = namedArgs.screenBounds as Map<string, USSRawValue>
+
+        const text = (namedArgs.text as { type: 'opaque', opaqueType: 'richTextDocument', value: RichTextDocument }).value
+        const backgroundColor = (namedArgs.backgroundColor as { type: 'opaque', opaqueType: 'color', value: Color }).value
+        const borderColor = (namedArgs.borderColor as { type: 'opaque', opaqueType: 'color', value: Color }).value
+        const borderWidth = namedArgs.borderWidth as number
+
+        return {
+            type: 'opaque',
+            opaqueType: 'textBox',
+            value: {
+                bottomLeft: [screenBounds.get('west') as number, screenBounds.get('south') as number],
+                topRight: [screenBounds.get('east') as number, screenBounds.get('north') as number],
+                text,
+                backgroundColor,
+                borderColor,
+                borderWidth,
+            },
+        }
+    },
+    documentation: {
+        humanReadableName: 'Map Text Box',
+        category: 'map',
+        longDescription: 'Create a text box on the map with specified screen bounds (bounding box of the inset on the screen, where bottom left corner has (0, 0) and top right corner has (1, 1))',
+        selectorRendering: { kind: 'subtitleLongDescription' },
+        customConstructor: true,
+    },
+} satisfies USSValue
