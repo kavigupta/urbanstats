3b7d8bcb6ad51934fa4e3e45e92e5ee17aef699e
Luke Brody <luke@lukebrody.com>
2025-11-07 00:51:37 -0800

    Revert "revert literal parser changes"
    This reverts commit 4b59c7751dca054606c41eab31c269d67392cada.

--- react/src/mapper/settings/edit-insets.ts
+++ react/src/mapper/settings/edit-insets.ts
@@ -32,7 +32,7 @@ const insetSchema = l.transformExpr(l.deconstruct(l.call({ fn: l.identifier('con
 
 const constructInsetsSchema = l.transformExpr(l.call({ fn: l.identifier('constructInsets'), namedArgs: {}, unnamedArgs: [l.editableVector(insetSchema)] }), call => call.unnamedArgs[0])
 
-const mapInsetsSchema = l.transformStmt(l.statements([
+const mapSchema = l.transformStmt(l.statements([
     l.ignore(),
     l.condition({
         condition: l.ignore(),
@@ -47,26 +47,19 @@ const mapInsetsSchema = l.transformStmt(l.statements([
                 }))),
         ],
     }),
-]), (uss) => {
-    const insetArg = uss[1].rest[0].namedArgs.insets
-    const literalInsets = insetArg.currentValue ?? null
-    return {
-        ...insetArg,
-        currentValue: literalInsets,
-    }
-})
+]), uss => uss[1].rest[0].namedArgs.insets)
 
 export function getInsets(settings: MapSettings, typeEnvironment: TypeEnvironment): Inset[] | undefined {
     if (settings.script.uss.type === 'statements') {
-        const parseResult = mapInsetsSchema.parse(settings.script.uss, typeEnvironment)
-        if (parseResult === undefined) {
+        try {
+            const parseResult = mapSchema.parse(settings.script.uss, typeEnvironment)
+            if (parseResult.currentValue === undefined && settings.universe !== undefined) {
+                return loadInsets(settings.universe)
+            }
+            return parseResult.currentValue?.currentValue
+        }
+        catch {
             return undefined
-        }
-        if (parseResult.currentValue !== null) {
-            return parseResult.currentValue.currentValue
-        }
-        if (settings.universe !== undefined) {
-            return loadInsets(settings.universe)
         }
     }
     return undefined
@@ -93,18 +86,18 @@ export function swapInsets(edits: InsetEdits, indexA: number, indexB: number): I
 
 export function doEditInsets(settings: MapSettings, edits: InsetEdits, typeEnvironment: TypeEnvironment): MapUSS {
     assert(settings.script.uss.type === 'statements', 'Trying to do an inset edit on USS that is not inset editable')
-    const mapInsets = mapInsetsSchema.parse(settings.script.uss, typeEnvironment)
-    assert(mapInsets !== undefined && (mapInsets.currentValue !== null || settings.universe !== undefined), 'Trying to do an inset edit on USS that is not inset editable')
+    const mapInsets = mapSchema.parse(settings.script.uss, typeEnvironment)
+    assert(settings.universe !== undefined, 'Trying to do an inset edit on USS that is not inset editable')
 
     let currentInsetsAst: UrbanStatsASTExpression
-    if (mapInsets.currentValue !== null) {
+    if (mapInsets.currentValue !== undefined) {
         currentInsetsAst = mapInsets.expr!
     }
     else {
-        currentInsetsAst = loadInsetExpression(settings.universe!)
+        currentInsetsAst = loadInsetExpression(settings.universe)
     }
 
-    const newConstructInsets = constructInsetsSchema.parse(currentInsetsAst, typeEnvironment)!.edit(edits.ast) as UrbanStatsASTExpression
+    const newConstructInsets = constructInsetsSchema.parse(currentInsetsAst, typeEnvironment).edit(edits.ast) as UrbanStatsASTExpression
 
     const result = mapInsets.edit(newConstructInsets)
     return result as MapUSS
