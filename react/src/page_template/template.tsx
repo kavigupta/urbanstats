d49a7745308aee59f540cffa3d5252ae2d5a984c
Kavi Gupta <kavi@berkeley.edu>
2025-11-16 16:33:10 -0800

    bump version

--- react/src/page_template/template.tsx
+++ react/src/page_template/template.tsx
@@ -158,7 +158,7 @@ function TemplateFooter(): ReactNode {
 function Version(): ReactNode {
     return (
         <span id="current-version">
-            {TestUtils.shared.isTesting ? '<VERSION>' : '30.3.7'}
+            {TestUtils.shared.isTesting ? '<VERSION>' : '30.3.8'}
         </span>
     )
 }
@@ -166,7 +166,7 @@ function Version(): ReactNode {
 function LastUpdated(): ReactNode {
     return (
         <span id="last-updated">
-            {TestUtils.shared.isTesting ? '<LAST UPDATED>' : '2025-11-08'}
+            {TestUtils.shared.isTesting ? '<LAST UPDATED>' : '2025-11-16'}
         </span>
     )
 }
