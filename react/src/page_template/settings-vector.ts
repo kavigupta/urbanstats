e778f9e6ed98554e7dfe4756bd54f0dc1b7e9f57
Kavi Gupta <kavi@berkeley.edu>
2026-02-08 10:37:17 -0800

    add settings to the end, always

--- react/src/page_template/settings-vector.ts
+++ react/src/page_template/settings-vector.ts
@@ -412,13 +412,6 @@ const settingsVector = [
     new ActiveSetting({ key: 'expanded__gpw_pw_density_32', coder: booleanSettingCoder }),
     new ActiveSetting({ key: 'expanded__gpw_pw_density_64', coder: booleanSettingCoder }),
     new ActiveSetting({ key: 'show_stat_group_us_presidential_election', coder: booleanSettingCoder }),
-    new ActiveSetting({ key: 'show_stat_group_canada_general_election_lib', coder: booleanSettingCoder }),
-    new ActiveSetting({ key: 'show_stat_group_canada_general_election_con', coder: booleanSettingCoder }),
-    new ActiveSetting({ key: 'show_stat_group_canada_general_election_ndp', coder: booleanSettingCoder }),
-    new ActiveSetting({ key: 'show_stat_group_canada_general_election_bq', coder: booleanSettingCoder }),
-    new ActiveSetting({ key: 'show_stat_group_canada_general_election_grn', coder: booleanSettingCoder }),
-    new ActiveSetting({ key: 'show_stat_group_canada_general_election_ppc', coder: booleanSettingCoder }),
-    new ActiveSetting({ key: 'show_stat_group_canada_general_election_coalition_margin', coder: booleanSettingCoder }),
     new ActiveSetting({ key: 'show_stat_group_mean_high_temp_djf', coder: booleanSettingCoder }),
     new ActiveSetting({ key: 'show_stat_group_mean_high_temp_mam', coder: booleanSettingCoder }),
     new ActiveSetting({ key: 'show_stat_group_mean_high_temp_jja', coder: booleanSettingCoder }),
@@ -437,6 +430,13 @@ const settingsVector = [
     new ActiveSetting({ key: 'show_stat_group_performance_score_adj_2019', coder: booleanSettingCoder }),
     new ActiveSetting({ key: 'show_stat_group_transportation_commute_time_median', coder: booleanSettingCoder }),
     new ActiveSetting({ key: 'show_person_circles', coder: booleanSettingCoder }),
+    new ActiveSetting({ key: 'show_stat_group_canada_general_election_lib', coder: booleanSettingCoder }),
+    new ActiveSetting({ key: 'show_stat_group_canada_general_election_con', coder: booleanSettingCoder }),
+    new ActiveSetting({ key: 'show_stat_group_canada_general_election_ndp', coder: booleanSettingCoder }),
+    new ActiveSetting({ key: 'show_stat_group_canada_general_election_bq', coder: booleanSettingCoder }),
+    new ActiveSetting({ key: 'show_stat_group_canada_general_election_grn', coder: booleanSettingCoder }),
+    new ActiveSetting({ key: 'show_stat_group_canada_general_election_ppc', coder: booleanSettingCoder }),
+    new ActiveSetting({ key: 'show_stat_group_canada_general_election_coalition_margin', coder: booleanSettingCoder }),
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Necessary use of any
 ] satisfies (ActiveSetting<any> | DeprecatedSetting<string>)[]
 
