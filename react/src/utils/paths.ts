95ad1b5090321080e8060c9a97314b72a68ee2d5
Kavi Gupta <kavi@berkeley.edu>
2026-02-23 00:15:44 -0500

    simplify slash sanitization
    there were never any % symbols in longnames, so removing it should be fine. this makes it consistent with the python

--- react/src/utils/paths.ts
+++ react/src/utils/paths.ts
@@ -1,11 +1,3 @@
-export function sanitize(longname: string, spaces_around_slash = true): string {
-    let x = longname
-    if (spaces_around_slash) {
-        x = x.replaceAll('/', ' slash ')
-    }
-    else {
-        x = x.replaceAll('/', 'slash')
-    }
-    x = x.replaceAll('%', '%25')
-    return x
+export function sanitize(longname: string): string {
+    return longname.replaceAll('/', ' slash ')
 }
