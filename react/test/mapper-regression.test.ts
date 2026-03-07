0faeafb263e227fed2ab625900dac606cccabd6e
Luke Brody <luke@lukebrody.com>
2026-03-07 15:16:31 -0800

    add regression tests

--- react/test/mapper-regression.test.ts
+++ react/test/mapper-regression.test.ts
@@ -7,7 +7,8 @@ const urls = {
     'rgbMap': '/mapper.html?settings=H4sIAAAAAAAAA42OsWrDMBCGX0UcFKTiIV4TNFQZPJR2cBO6CMrZFqmIJRnp1CSEvHuVhhqyZbv7fr7%2F7gw7E3YRp%2B%2FTq%2FUDLGEdsqcTVJC9%2FTExmcK2Hy8FpD7aiWB5hpxSoX1OFNx7GAzXoEGstO%2BDHyzZ4BmnmI0o5A2ntlF8QMJW3inOeuuy4%2FWCPbM%2BOJfJfFFEnyxVrBalsmJXr3nA6%2Bze3EnqAemA436WRuzMKDW0cn2L2eb%2Fl2ZG6u%2BMmvfP0rBiDo%2FXclkvnjRUrMNkHE7SB3WbuBBwufwCmMvBE2wBAAA%3D',
     'textBoxesMap': '/mapper.html?settings=H4sIAAAAAAAAA51UbWvbMBD%2BK0IwsIfnl9CkTYYHbUdhjI2yrJ%2FqNij2xdZiS0KSm4SQ%2Fz7J9tK8bmWHLd%2Fdc3fW6R57jXPguSSiWH2lLMMjPK6njGjKGSnRD8iNgj1cM%2FoCUoHBH8bXxqFSSYXGozWulTLetFaaV995Bk6CGWd3tNQgIbvnoi6bcihGqq4csXW4CXY%2FJizlLKNNgLNX5DUQfUJR2IpJcU3KNyKchCEjGdEkzoApqlcTsZhE88prEZWSEuKSMiBybHXH7RBJKhHb5UHSjKrOq2Gpb%2FgSVPzYOnaczqunLS0B2A2vWabi9T5mhXGpixGKvGMIiNIjFPq9q2G%2FP7joDaJoOLzoRf0TsYrXtkzoXxkZGInCftSLesMTsQto6u4Dm4NA200s9ewzT%2BsKmHYejwsZeKwlZbkZwc%2FCjBARcyfYQzPOdJzga0lJaW2qSUnTWMsa3BM72il0hFnZG7bmXawj7ak6zbzRe%2BQcUMZFATrJrgYxWZZUJzZjZcrLrNntGbzr75bXkoJM8HHUP9rE7xCfIQFclIAkCAnKHDJkiDKkC6pQRYQ9OGXi59Ae3H7Bpx1792VvoWFHutDve1uWDY2%2BZVHk%2FWGJf3nIjCmXGchbXnIZF%2BrF6YeeoS%2BKDjueknSeNyNqY2U%2BdcxrPNQtl4cJb6TcHZeVmeQZqiR45iRGJJRkuV5uXPMvsTZlerL%2B0Gozvdo8b9UGLohGbeKSuvbhwfO610CCIopaBC03DZa15v%2FMPUmYvcxozReRs7hZb%2B3s5ZnULxXJ4Wy3hdZCjYKgFiUnmb%2Bgc1pBRonPZR5YS1grSHlVcaaCWTAbBOZXXZeGtwso5OTecCGMJuGV%2F0vkJzv6C%2B9a9anb%2BJQoMLyNGb9pNccEuHiz%2BQ1fsC6OOgYAAA%3D%3D',
     'textBoxesMap-corsError': '/mapper.html?settings=H4sIAAAAAAAAA51UbWvbMBD%2BK0IwkIeX2KFpmwwP2o7CGCtjWT%2FVbVHsi6PVloQktykh%2F30n20vz1q3ssOXTPc%2BdXu5JlrQAVRiu589fhczpmE7qqeROKMlL8gMKdGhIaykewVhA%2FHpyhgGbGaEdHS9pbS1Gs9o6VV2pHFhKpZKXonRgIP%2BudF025UhCbF0xvQ4EKQ0%2BpjJTMhcNgW0VeSGSTySOWsOUAFO%2Bcc1SSdBy7niSg7TCPd%2Frp%2Fv4oQpbxGa8hKQUEriZeJ8FHWJ4pRM%2FXBuRC9tFHSzcuVqATW7awEaQvUTa0gZAnqta5jZZbmPepDJuPiZxuA8Bt25Mot7gdDQcHh8NjuN4NDoaxMMDXKtqXybqnaIdo8XRMB7Eg9EB7hM0dbeB1Q7RnyYxbvZZZXUF0rGb%2FUIIT5wRssAW%2FJxjCwnHN6UhmSnpkpSeGcFLPxeOlyJLnKkhOLCjjUJ7mLetZjvVcZnxt8qafpP3hO1IJiB9clBdDYJZXlQHNuNtqsq82e0reHe%2BC1UbASal%2B6x%2FHJO%2BI2pGNChdAjGgDVi8ZMiJkMTNhSUV1%2F7iLPIfoL247YK3G%2FPNxd4iw050UW8YrlU2Qn%2Btojj8o5Leya4ypsrkYC5UqUwyt49sGIUoXxLvnnjKs4eiaVHLNcWU4TIh6YaT3YQ3Su5SmQo7%2BYpUUjpjKZqBki%2BWi1WA%2FyV%2BLqS7X35ovZl7Xt2t3Qaec0faxIUI%2FCeEu%2BWggbQggrQIWawaLG%2Bn%2F9P3NJX%2BwdbiL6KQSTNe%2BN6bV1K%2FVLzwup87p%2B243%2FffXsZdX3jA9o%2BiUe%2BXLlDMfxFI6952K0y5BRRYItV56zEkBHS1%2Bg0Q5%2Bo14wUAAA%3D%3D',
-
+    'translucencyRamp': '/mapper.html?settings=H4sIAAAAAAAAA3VOS0vEMBD%2BKyGnBHLYXis5uPUmKrh4sh6maewG00zIY0VK%2F7vJdosnwxBmvvkes9BJ4xTAn38ejRtpS7vugQqanbnoEHUFwJpPDM6AIG%2Bn%2B7KMKhifaLvQHGNhqBwTzs84atbTnvK73il0o0kGHWEpZM0L8gSe9Y6UN0IC6dFnC5UiNjQqsFpa4zSEU%2B0Zv20CzF4Wx1isVHotE3tfLmCzbslBEIUWQ0vCNLBGVKAUyANfBdlZzX%2Bshq8fe8wAUc%2FgpcPj1rEB1NcUMLuxq2p5E29VI3Yp5lQP%2FzvyZQPYNVVe%2F6MtboJ8azOdUwnuHafr%2BgsUPXOtgQEAAA%3D%3D',
+    'translucentRamp': '/mapper.html?settings=H4sIAAAAAAAAA4WSXWvDIBSG%2F4p4ZUBKkuajzfBi7eXYBiu7WkaxRlJposGPllL632eSrrDANhGVc87znqPHC6y5qjXt9ucnIStYwLVy0p4hhk6KI9eGDzYpObOCOYvB%2B%2BbRew3TorOwuEBnjA9hzljVvqiKoxKWMHgoJVOyElYoCZDVjgfe8kw7VErgR0UtJRWXRtjztjtto0OLR49htOGkEZJTvenPKLh5NG078iORT2G8NrNv3oU%2ByjHucqSN4wUIMWCqUboAut6hcJYuYwzC2Tyd91uepRhQ4s3BFU%2FJWZxO4BuVLP9np2gY5z2UJeO2yP5g8ymcJtE9462KX%2BBoQubxcN1sPpQcLZI76cHPwDfp9q47anhLOyLVajyhHWWHWvufUK17RdILevlxepHgG1XO9p0i90a8jgY0VEKGddV4NQxOXNR7SyL%2FDwJ4vX4BmQRSIXoCAAA%3D',
 }
 
 function regressionTest(testFn: () => TestFn, name: keyof typeof urls, code: string): void {
@@ -152,5 +153,29 @@ cMap(
         )
     ],
     basemap=noBasemap()
+)
+`)
+
+regressionTest(() => test, 'translucencyRamp', `cMap(
+    data=population,
+    scale=linearScale(),
+    ramp=constructRamp([{value: 0, color: rgb(1, 0, 0, a=0)}, {value: 1, color: rgb(1, 0, 0, a=1)}]),
+    basemap=noBasemap(backgroundColor=rgb(1, 1, 1, a=0)),
+    outline=constructOutline(color=colorBlack, weight=1)
+)
+`)
+
+regressionTest(() => test, 'translucentRamp', `cMap(
+    data=density_pw_1km,
+    scale=linearScale(),
+    ramp=constructRamp([
+    {value: 0, color: rgb(0.592, 0.353, 0.765, a=0.5)},
+    {value: 0.25, color: rgb(0.353, 0.49, 0.765, a=0.5)},
+    {value: 0.5, color: rgb(0.027, 0.647, 0.686, a=0.5)},
+    {value: 0.75, color: rgb(0.541, 0.765, 0.353, a=0.5)},
+    {value: 1, color: rgb(0.722, 0.639, 0.184, a=0.5)}
+]),
+    basemap=noBasemap(backgroundColor=rgb(1, 1, 1, a=0)),
+    outline=constructOutline(color=colorBlack, weight=1)
 )
 `)
