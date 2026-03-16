56b40bb8375650d8093842f2b01d2f5c8819fa24
Kavi Gupta <kavi@berkeley.edu>
2026-03-15 23:36:16 -0400

    set line opacity to the fill opacity

--- react/src/components/map-common.tsx
+++ react/src/components/map-common.tsx
@@ -128,6 +128,7 @@ export function PolygonFeatureCollection({ features, clickable }: { features: Ge
                 source={polygonsId(id, 'source')}
                 paint={{
                     'line-color': ['get', 'color'],
+                    'line-opacity': ['get', 'fillOpacity'],
                     'line-width': ['get', 'weight'],
                 }}
                 beforeId={labelId}
