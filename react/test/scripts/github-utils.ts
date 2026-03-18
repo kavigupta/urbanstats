e7f22ad75ad6729e7376d65f79140a7735ac9207
Luke Brody <luke@lukebrody.com>
2026-03-17 23:02:56 -0700

    remove new stuff

--- react/test/scripts/github-utils.ts
+++ react/test/scripts/github-utils.ts
@@ -5,27 +5,6 @@ export async function github(token = z.string().parse(process.env.GITHUB_TOKEN))
 
     const octokit = getOctokit(token)
 
-    function currentJobId(): number {
-        return z.coerce.number().parse(process.env.CHECK_RUN_ID)
-    }
-
-    async function currentStepNumber(): Promise<number> {
-        const { data: currentJob } = await octokit.rest.actions.getJobForWorkflowRun({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            run_id: context.runId,
-            job_id: currentJobId(),
-        })
-        if (currentJob.steps === undefined) {
-            throw new Error('Current job has no steps')
-        }
-        const inProgressStep = currentJob.steps.find(step => step.status === 'in_progress')
-        if (inProgressStep === undefined) {
-            throw new Error(`Current job has no in_progress steps. Current job: ${JSON.stringify(currentJob, null, 2)}`)
-        }
-        return inProgressStep.number
-    }
-
-    const result = { context, currentStepNumber, octokit, currentJobId }
+    const result = { context, octokit }
     return result
 }
--- react/test/scripts/run-e2e-tests.ts
+++ react/test/scripts/run-e2e-tests.ts
@@ -8,7 +8,6 @@ import { z } from 'zod'
 import { argumentParser } from 'zodcli'
 
 import { startProxy } from './ci_proxy'
-import { github } from './github-utils'
 import { booleanArgument, getTOTPWait, setTOTPWait, testFile, TestHistory, TestResult } from './util'
 
 const options = argumentParser({
@@ -49,18 +48,14 @@ const testcafe = await createTestCafe('localhost', 1337, 1338)
 
 const testHistory: TestHistory = []
 
-const gh = process.env.GITHUB_ACTION ? await github() : undefined
-
 for (const test of tests) {
+    console.warn(chalkTemplate`{cyan ${testFile(test)} running...}`)
+
     const numTries = options.tries * (await testFileDidChange(test) ? 1 : 2)
     let retries = 0
     let result: TestResult
 
     retry: while (true) {
-        if (gh) {
-            console.warn(`::group::${testFile(test)} attempt ${retries + 1}`)
-        }
-        console.warn(chalkTemplate`{cyan ${testFile(test)} attempt ${(retries + 1)} running...}`)
         result = await runTest(test)
         printResult({ test, result, retries })
         switch (result.status) {
@@ -73,27 +68,11 @@ for (const test of tests) {
                     break retry
                 }
                 console.warn(chalkTemplate`{red ${testFile(test)} failed... trying again}`)
-                if (process.env.GITHUB_ACTIONS) {
-                    console.warn(`::endgroup::`)
-                }
                 retries++
         }
     }
 
-    if (gh) {
-        console.warn(`::endgroup::`)
-        console.warn(result.status === 'success' ? '✅' : '❌')
-    }
-
-    testHistory.push({
-        test,
-        result,
-        retries,
-        github: gh && {
-            jobId: gh.currentJobId(),
-            stepNumber: await gh.currentStepNumber(),
-        },
-    })
+    testHistory.push({ test, result, retries })
 }
 
 testHistory.forEach(printResult)
