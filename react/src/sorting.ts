bb22130991d856a226bfe11bcac3a0c2b36f8778
Kavi Gupta <kavi@berkeley.edu>
2026-03-21 18:52:40 -0700

    slightly refactor sorting

--- react/src/sorting.ts
+++ react/src/sorting.ts
@@ -1,15 +1,19 @@
-import { ArticleRow } from './components/load-article'
+import { ArticleRow, isNoValue } from './components/load-article'
 
 export function compareArticleRows(a: ArticleRow, b: ArticleRow, direction: 'up' | 'down'): number {
-    if (!isNaN(a.statval) && !isNaN(b.statval)) {
-        return direction === 'up' ? a.statval - b.statval : b.statval - a.statval
+    const nameCompared = direction === 'up' ? a.renderedStatname.localeCompare(b.renderedStatname) : b.renderedStatname.localeCompare(a.renderedStatname)
+    if (isNoValue(a.statval) && isNoValue(b.statval)) {
+        return nameCompared
     }
-    // always put NaN values at the end
-    if (isNaN(a.statval)) {
+    if (isNoValue(a.statval)) {
         return 1
     }
-    if (isNaN(b.statval)) {
+    if (isNoValue(b.statval)) {
         return -1
     }
-    return direction === 'up' ? a.renderedStatname.localeCompare(b.renderedStatname) : b.renderedStatname.localeCompare(a.renderedStatname)
+    const directComparison = a.statval - b.statval
+    if (directComparison !== 0) {
+        return direction === 'up' ? directComparison : -directComparison
+    }
+    return nameCompared
 }
