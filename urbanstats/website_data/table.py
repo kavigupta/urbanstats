a7fa8188747af9f0afbd69380ae37dd7a0c0fcc0
Kavi Gupta <kavi@berkeley.edu>
2026-03-29 14:40:01 -0400

    fix things

--- urbanstats/website_data/table.py
+++ urbanstats/website_data/table.py
@@ -18,7 +18,7 @@ from urbanstats.universe.universe_provider.compute_universes import (
 
 
 @permacache_with_remapping_pickle(
-    "population_density/stats_for_shapefile/compute_statistics_for_shapefile_41",
+    "population_density/stats_for_shapefile/compute_statistics_for_shapefile_42",
     key_function=dict(
         sf=lambda x: x.hash_key,
         shapefiles=lambda x: {
