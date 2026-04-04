0985a85243e86dae01bc24fe06d27e3bea2aeecf
Kavi Gupta <kavi@berkeley.edu>
2026-04-04 12:02:01 -0700

    update

---
+++ urbanstats/metadata/metadata_columns.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import Any, List
+
+from abc import ABC, abstractmethod
+
+
+@dataclass(frozen=True)
+class MetadataColumnResult:
+    key: str
+    values: List[Any]
+
+
+class MetadataColumnProvider(ABC):
+    @abstractmethod
+    def compute_metadata_columns(self, *, shapefile, shapefile_table):
+        pass
+
+    def __permacache_hash__(self):
+        return (self.__class__.__name__, getattr(self, "version", None))
---
+++ urbanstats/metadata/metadata_columns_list.py
@@ -0,0 +1,5 @@
+from urbanstats.metadata.congressional_representatives import (
+    CongressionalRepresentativesMetadataProvider,
+)
+
+metadata_column_providers = (CongressionalRepresentativesMetadataProvider(),)
