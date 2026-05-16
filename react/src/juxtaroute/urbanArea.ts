8e8efa9b1898bfde17bc3923f9da5ba59806c939
Kavi Gupta <kavi@berkeley.edu>
2026-05-16 12:47:31 -0400

    defensive

--- react/src/juxtaroute/urbanArea.ts
+++ react/src/juxtaroute/urbanArea.ts
@@ -1,8 +1,13 @@
+import { assert } from '../utils/defensive'
 import { loadArticleFromPossibleSymlink } from '../utils/symlinks'
 
 export async function fetchUrbanAreaZips(urbanAreaLongname: string): Promise<string[]> {
     const article = await loadArticleFromPossibleSymlink(urbanAreaLongname)
     // Find contained ZIP codes
-    const contained = article.related.find(r => r.relationshipType === 'contains')
-    return contained?.buttons ? contained.buttons.filter(b => b.rowType === 'ZIP').map(b => b.longname ?? '') : []
+    const containedButtons = article.related.find(r => r.relationshipType === 'contains')?.buttons || undefined
+    assert(containedButtons !== undefined, `Expected 'contains' relationship with buttons for urban area ${urbanAreaLongname}`)
+    return containedButtons.filter(b => b.rowType === 'ZIP').map((b) => {
+        assert(b.longname !== undefined && b.longname !== null, 'expected a longname for ZIP button')
+        return b.longname
+    })
 }
