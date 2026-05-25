f526e1fcba81b84a6e7503395347140a5f35aa45
Kavi Gupta <kavi@berkeley.edu>
2026-05-25 09:38:39 -0700

    update

--- urbanstats/ordinals/ordering_info_outputter.py
+++ urbanstats/ordinals/ordering_info_outputter.py
@@ -1,59 +1,13 @@
-import numpy as np
 import tqdm.auto as tqdm
 
 from urbanstats.ordinals.compress_counts import compress_counts, mapify
+from urbanstats.ordinals.ensure_ordering import ensure_correct_ordering
 from urbanstats.protobuf import data_files_pb2
 from urbanstats.protobuf.utils import save_article_ordering_list, write_gzip
 from urbanstats.statistics.output_statistics_metadata import internal_statistic_names
 from urbanstats.statistics.stat_path import get_statistic_column_path
 from urbanstats.universe.universe_list import all_universes
 from urbanstats.utils import output_typescript
-
-
-def check_ordering_pair(prev_val, val_unmodified, val_modified, tol=1e-3):
-    assert isinstance(prev_val, np.float32)
-    assert isinstance(val_unmodified, np.float32)
-    assert isinstance(val_modified, np.float32)
-    if not np.isfinite(prev_val) or not np.isfinite(val_unmodified):
-        return
-    if not (val_modified < prev_val):
-        raise ValueError(
-            f"Modified value {val_modified} is not less than previous value {prev_val}"
-        )
-    if not np.isclose(val_unmodified, val_modified, rtol=tol, atol=tol):
-        raise ValueError(
-            f"Modified value {val_modified} is not close enough to original value {val_unmodified}"
-        )
-
-
-def check_ordering(values, ordering):
-    for i in range(1, len(values)):
-        index = ordering[i]
-        prev_index = ordering[i - 1]
-        check_ordering_pair(values[prev_index], values[index], values[index])
-
-
-def ensure_correct_ordering(values, ordering):
-    """
-    We want to ensure that the values are strictly decreasing according to the ordering.
-    We break ties by using np.nextafter to get the next representable float value that is less than the previous value.
-
-    The resulting list guarantees that for all initially finite values, if ordering[i] > ordering[j], then values[i] < values[j],
-    and the values are within a small tolerance of the original values
-    """
-    values = np.array(values, dtype=np.float32, copy=True)
-    ordering = np.array(ordering, dtype=np.int64, copy=False)
-    assert len(values) == len(ordering)
-    ordering = np.argsort(np.argsort(ordering))
-    for i in reversed(range(0, len(values) - 1)):
-        index = ordering[i]
-        next_index = ordering[i + 1]
-        if values[index] <= values[next_index]:
-            next_value = values[next_index]
-            new_value = np.nextafter(next_value, np.float32(np.inf))
-            values[index] = new_value
-    check_ordering(values, ordering)
-    return values.tolist()
 
 
 class ProtobufOutputter:
