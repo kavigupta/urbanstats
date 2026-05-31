70fcc110d5c7eaed3f4de8a2ec87e10c486a208e
Luke Brody <luke@lukebrody.com>
2026-05-30 17:56:42 -0700

    better test

--- react/test/memory_mapper_leak.test.ts
+++ react/test/memory_mapper_leak.test.ts
@@ -1,14 +1,24 @@
 import { replaceInput } from './mapper-utils'
 import { memoryUsage } from './memory_test_utils'
-import { urbanstatsFixture } from './test_utils'
+import { urbanstatsFixture, waitForLoading } from './test_utils'
 
 urbanstatsFixture('mapper', '/mapper.html')
 
-test('counties USA', async (t) => {
-    await replaceInput(t, 'USA', 'California, USA')
-    await replaceInput(t, 'Subnational Region', 'Metropolitan Cluster')
-    await replaceInput(t, 'California, USA', 'Asia')
-    await replaceInput(t, 'Asia', 'USA')
-    await replaceInput(t, 'Metropolitan Cluster', 'County')
-    await t.expect(await memoryUsage(t)).lt(185_000_000)
+test('USA zip and back', async (t) => {
+    await waitForLoading()
+
+    for (let n = 0; n < 10; n++) {
+        console.warn(`Iteration ${n}`)
+        const mapTypes = ['Choropleth Map', 'RGB Choropleth Map', 'Point Map', 'Clustered Point Map']
+        for (let i = 0; i < mapTypes.length; i++) {
+            await replaceInput(t, mapTypes[i], mapTypes[(i + 1) % mapTypes.length])
+            await waitForLoading()
+            await replaceInput(t, 'Subnational Region', 'ZIP')
+            await waitForLoading()
+            await replaceInput(t, 'ZIP', 'Subnational Region')
+            await waitForLoading()
+        }
+    }
+
+    await t.expect(await memoryUsage(t, target => target.type === 'page')).lt(86_000_000)
 })
