4dc8981e93e504832d23a25ef9731de6c8529b11
Luke Brody <luke@lukebrody.com>
2026-05-30 18:46:43 -0700

    simpler test

--- react/test/memory_mapper_leak.test.ts
+++ react/test/memory_mapper_leak.test.ts
@@ -6,19 +6,10 @@ urbanstatsFixture('mapper', '/mapper.html')
 
 test('USA zip and back', async (t) => {
     await waitForLoading()
+    await replaceInput(t, 'Subnational Region', 'ZIP')
+    await waitForLoading()
+    await replaceInput(t, 'ZIP', 'Subnational Region')
+    await waitForLoading()
 
-    for (let n = 0; n < 10; n++) {
-        console.warn(`Iteration ${n}`)
-        const mapTypes = ['Choropleth Map', 'RGB Choropleth Map', 'Point Map', 'Clustered Point Map']
-        for (let i = 0; i < mapTypes.length; i++) {
-            await replaceInput(t, mapTypes[i], mapTypes[(i + 1) % mapTypes.length])
-            await waitForLoading()
-            await replaceInput(t, 'Subnational Region', 'ZIP')
-            await waitForLoading()
-            await replaceInput(t, 'ZIP', 'Subnational Region')
-            await waitForLoading()
-        }
-    }
-
-    await t.expect(await memoryUsage(t, target => target.type === 'page')).lt(86_000_000)
+    await t.expect(await memoryUsage(t, target => target.type === 'page')).lt(79_000_000)
 })
