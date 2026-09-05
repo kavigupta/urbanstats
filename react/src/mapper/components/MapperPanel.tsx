a98f2872c5a4c8425224bdf56bd40101ce3852ba
Luke Brody <luke@lukebrody.com>
2026-09-04 21:51:46 -0700

    add feature

--- react/src/mapper/components/MapperPanel.tsx
+++ react/src/mapper/components/MapperPanel.tsx
@@ -1,6 +1,8 @@
-import React, { ReactNode, useMemo } from 'react'
+import React, { ReactNode, useContext, useMemo } from 'react'
 
 import { CountsByUT } from '../../components/countsByArticleType'
+import { Navigator } from '../../navigation/Navigator'
+import { PageDescriptor } from '../../navigation/PageDescriptor'
 import { defaultTypeEnvironment } from '../context'
 import { useMapGenerator } from '../map-generator'
 import { EditMapperPanel } from '../settings/EditMapperPanel'
@@ -17,9 +19,27 @@ export function MapperPanel(props: { mapSettings: MapSettings, view: boolean, co
 function DisplayMap({ mapSettings }: { mapSettings: MapSettings }): ReactNode {
     const typeEnvironment = useMemo(() => defaultTypeEnvironment(mapSettings.universe), [mapSettings.universe])
     const mapGenerator = useMapGenerator({ mapSettings, typeEnvironment })
+
     return (
-        <>
+        <div style={{ position: 'absolute', inset: 0 }}>
             {mapGenerator.ui({ mode: 'view' }).node}
-        </>
+            <EditButton />
+        </div>
+    )
+}
+
+function EditButton(): ReactNode {
+    const navigator = useContext(Navigator.Context)
+
+    const currentDescriptor = navigator.usePageState().current.descriptor
+
+    if (currentDescriptor.kind !== 'mapper') {
+        return null
+    }
+
+    return (
+        <button style={{ position: 'absolute', left: '1em', top: '1em' }} {...navigator.link({ ...currentDescriptor, view: false }, { scroll: { kind: 'position', top: 0 } })}>
+            Edit
+        </button>
     )
 }
