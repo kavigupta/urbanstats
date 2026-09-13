b4471afcaa67e097e2d179b215fe7248eb0e18e0
Kavi Gupta <kavi@berkeley.edu>
2026-09-12 23:01:00 -0700

    clean

--- react/src/urban-stats-script/constants/map.ts
+++ react/src/urban-stats-script/constants/map.ts
@@ -241,8 +241,8 @@ function computeCommonMap(
     const labelPassedIn = namedArgs.label as string | null
     const basemap = (namedArgs.basemap as { type: 'opaque', opaqueType: 'basemap', value: Basemap }).value
     const insets = (namedArgs.insets as { type: 'opaque', opaqueType: 'insets', value: Inset[] }).value
-    const unitArg = namedArgs.unit as { type: 'opaque', opaqueType: 'unit', value: { unit: string } } | null
-    const unit = unitArg === null ? undefined : unitArg.value.unit as UnitType
+    const unitArg = namedArgs.unit as { type: 'opaque', opaqueType: 'unit', value: { unit: UnitType } } | null
+    const unit = unitArg === null ? undefined : unitArg.value.unit
     const textBoxes = (namedArgs.textBoxes as { value: TextBox }[] | null ?? []).map(({ value }) => value)
     const opacity = Math.max(0, Math.min(1, namedArgs.opacity as number))
     const missingData = (namedArgs.missingData as { type: 'opaque', opaqueType: 'missingData', value: MissingData } | null)?.value
